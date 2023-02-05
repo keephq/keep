217eabed19a67e2bdcb049eae5dd8fee615987a6
shahargl <shaharglazner@gmail.com>
2023-02-04 17:00:28 -0800

    feat: improve factory interface

---
+++ keep/providers/providers_factory.py
@@ -0,0 +1,32 @@
+"""
+The providers factory module.
+"""
+import importlib
+import logging
+
+from keep.providers.base.base_provider import BaseProvider
+from keep.providers.models.provider_config import ProviderConfig
+
+
+class ProvidersFactory:
+    @staticmethod
+    def get_provider(
+        provider_type: str, provider_config: dict, **kwargs
+    ) -> BaseProvider:
+        """
+        Get the provider class from the provider type.
+
+        Args:
+            provider (dict): The provider configuration.
+
+        Returns:
+            BaseProvider: The provider class.
+        """
+        provider_config = ProviderConfig(**provider_config)
+        module = importlib.import_module(
+            f"keep.providers.{provider_type}_provider.{provider_type}_provider"
+        )
+        provider_class = getattr(
+            module, provider_type.title().replace("_", "") + "Provider"
+        )
+        return provider_class(config=provider_config, **kwargs)
