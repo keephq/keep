034d3c6fe572806cb3708a2052a28bb8d201d2a6
talboren <tal@keephq.dev>
2023-02-20 10:52:26 +0200

    feat(cli): allow directory as alerts file (#54)

--- keep/alertmanager/alertmanager.py
+++ keep/alertmanager/alertmanager.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from keep.parser.parser import Parser
 
@@ -8,8 +9,38 @@ class AlertManager:
         self.parser = Parser()
         self.logger = logging.getLogger(__name__)
 
-    def run(self, alert_file: str, providers_file: str = None):
-        self.logger.info(f"Running alert {alert_file}")
-        alert = self.parser.parse(alert_file, providers_file)
-        alert.run()
-        self.logger.info(f"Alert {alert_file} ran successfully")
+    def run(self, alert: str, providers_file: str = None):
+        """
+        Run alerts from a file or directory.
+
+        Args:
+            alert (str): Either a an alert yaml or a directory containing alert yamls.
+            providers_file (str, optional): The path to the providers yaml. Defaults to None.
+        """
+        self.logger.info(f"Running alert(s) from {alert}")
+        if os.path.isdir(alert):
+            self.run_from_directory(alert, providers_file)
+        else:
+            alert = self.parser.parse(alert, providers_file)
+            alert.run()
+        self.logger.info(f"Alert(s) from {alert} ran successfully")
+
+    def run_from_directory(self, alerts_dir: str, providers_file: str = None):
+        """
+        Run alerts from a directory.
+
+        Args:
+            alerts_dir (str): A directory containing alert yamls.
+            providers_file (str, optional): The path to the providers yaml. Defaults to None.
+        """
+        for file in os.listdir(alerts_dir):
+            if file.endswith(".yaml") or file.endswith(".yml"):
+                self.logger.info(f"Running alert from {file}")
+                try:
+                    alert = self.parser.parse(os.path.join(alert, file), providers_file)
+                    alert.run()
+                    self.logger.info(f"Alert from {file} ran successfully")
+                except Exception as e:
+                    self.logger.error(
+                        f"Error running alert from {file}", extra={"exception": e}
+                    )
