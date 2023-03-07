4a78f0be66f809133490130e292e41a970db2aaf
shahargl <shaharglazner@gmail.com>
2023-03-07 12:42:40 -0800

    feat: wip adding alertsfiles

--- keep/api/routes/alertsfiles.py
+++ keep/api/routes/alertsfiles.py
@@ -47,3 +47,39 @@ def get_alert(
     if not alerts:
         raise HTTPException(status_code=404, detail="Alert file not found")
     return alerts
+
+
+@router.post(
+    "/{alerts_file_id}/alert/{alert_id}/step/{step_id}",
+    description="Run step",
+)
+def run_step(
+    alerts_file_id: str,
+    alert_id: str,
+    step_id: str,
+    context: click.Context = Depends(click.get_current_context),
+) -> list[Alert]:
+    alert_manager = AlertManager()
+    alerts_file = context.params.get("alerts_file")
+    alerts_url = context.params.get("alert_url")
+    providers_file = context.params.get("providers_file")
+    alerts = alert_manager.get_alerts(alerts_file or alerts_url, providers_file)
+    alert = [
+        alert
+        for alert in alerts
+        if alert.alert_id == alert_id and alert.alert_file == alerts_file_id
+    ]
+    if len(alert) != 1:
+        raise HTTPException(
+            status_code=502,
+            detail="Multiple alerts with the same id within the same file",
+        )
+    alert = alert[0]
+
+    step = [step for step in alert.alert_steps if step.step_id == step_id]
+    if len(step) != 1:
+        raise HTTPException(status_code=502, detail="Multiple steps with the same id")
+
+    step = step[0]
+    step_output = alert.run_step(step)
+    return JSONResponse(content={"step_output": step_output})
