9152e34143ec7e0d4656547b9a507ab916eb590b
shahargl <shaharglazner@gmail.com>
2023-05-01 14:58:28 +0300

    feat: add test to throttling

--- tests/test_throttling.py
+++ tests/test_throttling.py
@@ -2,14 +2,22 @@ import io
 import logging
 import os
 import sys
+import uuid
+from unittest import mock
 
 import pytest
 from click.testing import CliRunner
 
+from keep.action.action import ActionStatus
+from keep.alertmanager.alertmanager import AlertManager
 from keep.cli.cli import cli
 
 
-def test_throttle_sanity(caplog, capsys):
+@mock.patch.dict(
+    os.environ, {"KEEP_STATE_FILE": f"tmpstate{uuid.uuid4()}.json"}, clear=True
+)
+def test_throttle_sanity():
+    """Tests end-to-end (cli -> alertmanager -> action) for a single alert"""
     # trigger first alert and check it triggered
     runner = CliRunner()
     result = runner.invoke(
@@ -35,9 +43,40 @@ def test_throttle_sanity(caplog, capsys):
     assert result.exit_code == 0
     throttled = False
     logs = result.output.splitlines()
+    # verify that the action is throttled
     for log in logs:
         if "is throttled" in log:
             throttled = True
             break
 
     assert throttled == True
+
+
+@mock.patch.dict(
+    os.environ, {"KEEP_STATE_FILE": f"tmpstate{uuid.uuid4()}.json"}, clear=True
+)
+def test_throttle_resolve():
+    alert_manager = AlertManager()
+    alerts = alert_manager.get_alerts(alert_path="tests/alerts/one_until_resolved.yaml")
+    alert = alerts[0]
+    # trigger first alert and check it triggered
+    alert.run()
+    # Validate the mock action is evaluated to run
+    assert alert.alert_actions[0].status == ActionStatus.FIRING
+    # Run it again
+    alert.run()
+    # Validate the mock action is evaluated to run
+    assert alert.alert_actions[0].status == ActionStatus.THROTTLED
+
+    # Resolve
+    alert.alert_steps[0].step_config["provider"]["with"]["command"] = "true"
+    alert.run()
+    assert alert.alert_actions[0].status == ActionStatus.SKIPPED
+
+    # Now rerun
+    alert.alert_steps[0].step_config["provider"]["with"]["command"] = "stat notfound"
+    alert.run()
+    assert alert.alert_actions[0].status == ActionStatus.FIRING
+
+    # Remove the tmpstate file
+    os.remove(os.environ["KEEP_STATE_FILE"])
