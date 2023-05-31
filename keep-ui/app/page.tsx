633bf32b6ba1cd5cc0b2320033f95fe727a14465
Shahar Glazner <shaharglazner@gmail.com>
2023-05-31 16:12:47 +0300

    fix: bugfix/callback (#153)

--- keep-ui/app/page.tsx
+++ keep-ui/app/page.tsx
@@ -23,7 +23,7 @@ export default async function IndexPage() {
     const apiUrl = getApiURL();
     isGitHubPluginInstalled = await fetch(`${apiUrl}/tenant/onboarded`, {
       headers: {
-        Authorization: `Bearer ${accessToken}`,
+        Authorization: `Bearer ${accessToken?.accessToken}`,
       },
       cache: "no-store",
     })
