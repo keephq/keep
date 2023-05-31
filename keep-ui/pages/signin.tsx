dc11504d3451721d46ca48159136cd24245ecc7c
Shahar Glazner <shaharglazner@gmail.com>
2023-05-31 14:39:06 +0300

    fix: add callback (#151)

--- keep-ui/pages/signin.tsx
+++ keep-ui/pages/signin.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 export default function Signin() {
 
   useEffect(() => {
-    void signIn("auth0");
+    void signIn("auth0", { callbackUrl: "/" });
   });
 
   return <div></div>;
