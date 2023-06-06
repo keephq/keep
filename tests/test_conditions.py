58d4be674897a4dc36d8c6227cd76e35e3ea466c
Tal Borenstein <tal@keephq.dev>
2023-06-06 09:53:12 +0300

    test: conditions factory

--- tests/test_conditions.py
+++ tests/test_conditions.py
@@ -1,4 +1,22 @@
+import pytest
+
 from keep.conditions.assert_condition import AssertCondition
+from keep.conditions.condition_factory import ConditionFactory
+
+
+def test_condition_factory():
+    condition_type = "assert"
+    condition_name = "mock"
+    condition_config = {"assert": "mock"}
+    condition = ConditionFactory.get_condition(
+        condition_type, condition_name, condition_config
+    )
+    assert isinstance(condition, AssertCondition)
+    condition_type = "unknown"
+    with pytest.raises(ModuleNotFoundError):
+        condition = ConditionFactory.get_condition(
+            condition_type, condition_name, condition_config
+        )
 
 
 def test_assert_condition():
@@ -14,3 +32,7 @@ def test_assert_condition():
 
     compare_value = assert_condtion.get_compare_value()
     assert compare_value == "mock"
+
+
+def test_threshold_condition():
+    assert True
