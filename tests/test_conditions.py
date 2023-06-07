c983e096ceb4e9dc000d74aaffe25e9cd7964f80
Tal Borenstein <tal@keephq.dev>
2023-06-07 11:13:13 +0300

    test: threshold condition

--- tests/test_conditions.py
+++ tests/test_conditions.py
@@ -2,6 +2,7 @@ import pytest
 
 from keep.conditions.assert_condition import AssertCondition
 from keep.conditions.condition_factory import ConditionFactory
+from keep.conditions.threshold_condition import ThresholdCondition
 
 
 def test_condition_factory():
@@ -34,5 +35,85 @@ def test_assert_condition():
     assert compare_value == "mock"
 
 
-def test_threshold_condition():
-    assert True
+def test_threshold_condition_single_threshold_gt():
+    threshold_condition = ThresholdCondition(
+        condition_type="threshold",
+        condition_name="mock",
+        condition_config={"compare_type": "gt"},
+    )
+    # 200 < 100
+    result = threshold_condition.apply(200, 100)
+    assert result is False
+
+
+def test_threshold_condition_single_threshold_lt():
+    threshold_condition = ThresholdCondition(
+        condition_type="threshold",
+        condition_name="mock",
+        condition_config={"compare_type": "lt"},
+    )
+    # 200 > 100
+    result = threshold_condition.apply(200, 100)
+    assert result is True
+
+
+def test_threshold_condition_invalid_threshold_type():
+    threshold_condition = ThresholdCondition(
+        condition_type="threshold",
+        condition_name="mock",
+        condition_config={"compare_type": "invalid"},
+    )
+    with pytest.raises(Exception):
+        threshold_condition.apply(200, 100)
+
+
+def test_threshold_condition_invalid_threshold_value():
+    threshold_condition = ThresholdCondition(
+        condition_type="threshold",
+        condition_name="mock",
+        condition_config={"compare_type": "gt"},
+    )
+    with pytest.raises(Exception, match="Invalid values for threshold") as e:
+        threshold_condition.apply("200000000000x", 100)
+
+
+def test_threshold_condition_different_threshold_types():
+    threshold_condition = ThresholdCondition(
+        condition_type="threshold",
+        condition_name="mock",
+        condition_config={"compare_type": "gt"},
+    )
+    with pytest.raises(Exception, match="Invalid threshold value"):
+        threshold_condition.apply(200000000000, "x100")
+
+
+def test_threshold_condition_one_value_is_precentage():
+    threshold_condition = ThresholdCondition(
+        condition_type="threshold",
+        condition_name="mock",
+        condition_config={"compare_type": "gt"},
+    )
+    with pytest.raises(Exception, match="Invalid threshold value"):
+        threshold_condition.apply("90", "80%")
+
+
+def test_threshold_condition_multithreshold():
+    threshold_condition = ThresholdCondition(
+        condition_type="threshold",
+        condition_name="mock",
+        condition_config={"level": "1, 2 ,3"},
+    )
+    result = threshold_condition.apply("1,2,3", "4,5,6")
+    assert result is True
+
+
+def test_threshold_condition_multithreshold_not_equals():
+    threshold_condition = ThresholdCondition(
+        condition_type="threshold",
+        condition_name="mock",
+        condition_config={"level": "1, 2 ,3"},
+    )
+    with pytest.raises(
+        Exception, match="Number of levels and number of thresholds do not match"
+    ):
+        threshold_condition.apply("1,2,3,4", "4,5,6")
