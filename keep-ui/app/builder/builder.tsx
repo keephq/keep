42d3be03370a612925c8749bb438b2fb2df01a1d
Tal Borenstein <tal@keephq.dev>
2023-06-11 15:42:07 +0300

    feat: wip

--- keep-ui/app/builder/builder.tsx
+++ keep-ui/app/builder/builder.tsx
@@ -4,108 +4,171 @@ import "sequential-workflow-designer/css/designer-dark.css";
 import "./page.css";
 import {
   Definition,
-  ToolboxConfiguration,
   StepsConfiguration,
   ValidatorConfiguration,
   Uid,
+  Step,
+  Sequence,
 } from "sequential-workflow-designer";
 import {
   SequentialWorkflowDesigner,
   wrapDefinition,
 } from "sequential-workflow-designer-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import StepEditor from "./editors";
+import { load, JSON_SCHEMA } from "js-yaml";
+import { Title } from "@tremor/react";
+import { KeepStep } from "./types";
 
 function IconUrlProvider(componentType: string, type: string): string | null {
-  if (componentType === "task") {
+  if (componentType === "task" && type) {
     return `${type}-icon.png`;
   }
   return null;
 }
 
-function stepValidator(step: any, parentSequence: any, definition: any) {
-  if (step.type === "query" || step.type === "notify") {
-    return step.providerType ? true : false;
-  }
+function stepValidator(
+  step: Step,
+  parentSequence: Sequence,
+  definition: Definition
+): boolean {
   return true;
 }
 
-function CanDeleteStep(step: any, parentSequence: any) {
+function CanDeleteStep(step: Step, parentSequence: Sequence): boolean {
   return !step.properties["isLocked"];
 }
 
 function CanMoveStep(
   sourceSequence: any,
   step: any,
-  targetSequence: any,
-  targetIndex: any
-) {
+  targetSequence: Sequence,
+  targetIndex: number
+): boolean {
   return CanDeleteStep(step, sourceSequence);
 }
 
-const alert = [
-  {
+// TODO: load dynamically
+const toolboxConfiguration = {
+  groups: [
+    {
+      name: "Misc",
+      steps: [
+        {
+          type: "for",
+          componentType: "container",
+          name: "Foreach",
+          properties: {},
+          sequence: [],
+        },
+      ],
+    },
+    {
+      name: "Conditions",
+      steps: [
+        {
+          type: "condition",
+          componentType: "switch",
+          name: "Threshold",
+          properties: {
+            value: "",
+            compare_to: "",
+          },
+          branches: {
+            true: [],
+            false: [],
+          },
+        },
+      ],
+    },
+  ],
+};
+
+function getActionOrStepObj(action: any): KeepStep {
+  return {
     id: Uid.next(),
-    name: "alert",
+    name: action.name,
+    componentType: "task",
+    type: action.provider.type,
+    properties: {
+      config: action.provider.config,
+      with: action.provider.with,
+    },
+  };
+}
+
+function parseAlert(alertToParse: string): Definition {
+  const parsedAlertFile = load(alertToParse, { schema: JSON_SCHEMA }) as any;
+  const steps = parsedAlertFile.alert.steps.map((step: any) => {
+    return getActionOrStepObj(step);
+  });
+  const conditions = [] as any;
+  parsedAlertFile.alert.actions.forEach((action: any) => {
+    // This means this action always runs, there's no condition and no alias
+    if (!action.condition && !action.if) {
+      steps.push(getActionOrStepObj(action));
+    }
+    // If this is an alias, we need to find the existing condition and add this action to it
+    else if (action.if) {
+      const cleanIf = action.if.replace("{{", "").replace("}}", "").trim();
+      const existingCondition = conditions.find(
+        (a: any) => a.alias === cleanIf
+      );
+      existingCondition.branches.true.push(getActionOrStepObj(action));
+    } else {
+      action.condition.forEach((condition: any) => {
+        conditions.push({
+          id: Uid.next(),
+          name: condition.name,
+          type: condition.type,
+          componentType: "switch",
+          alias: condition.alias,
+          properties: {
+            value: condition.value,
+            compare_to: condition.compare_to,
+          },
+          branches: {
+            true: [getActionOrStepObj(action)],
+            false: [],
+          },
+        });
+      });
+    }
+  });
+  const alert = {
+    id: Uid.next(),
+    name: "Workflow",
     componentType: "container",
     type: "alert",
     properties: {
-      id: "alert-id",
-      description: "alert description",
-      owners: [],
-      services: [],
-      isLocked: true,
+      id: parsedAlertFile.alert.id,
+      description: parsedAlertFile.alert.description,
     },
-    sequence: [
-      {
-        id: Uid.next(),
-        name: "db-no-space",
-        type: "mock",
-        componentType: "task",
-        properties: {
-          command: "df -h | grep /dev/disk3s1s1 | awk '{ print $5}'",
-          command_output: "80%",
-        },
-        providerId: "db-server-mock",
-      },
-      {
-        id: Uid.next(),
-        type: "condition",
-        componentType: "switch",
-        name: "Threshold",
-        properties: {
-          value: "{{ steps.db-no-space.results }}",
-          compare_to: "90%",
-        },
-        branches: {
-          true: [
-            {
-              id: Uid.next(),
-              name: "trigger-slack",
-              type: "slack",
-              componentType: "task",
-              properties: {
-                message:
-                  "The disk space of {{ providers.db-server-mock.description }} is about to finish\nDisk space left: {{ steps.db-no-space.results }}",
-              },
-              providerId: "slack-demo",
-            },
-          ],
-          false: [],
-        },
-      },
-    ],
-  },
-];
-
-function Builder() {
-  const startDefinition: Definition = {
-    sequence: alert,
-    properties: {},
+    sequence: [...steps, ...conditions],
   };
+  return { sequence: [alert], properties: {} };
+}
+
+function Builder({
+  loadedAlertFile,
+  fileName,
+}: {
+  loadedAlertFile: string | null;
+  fileName: string;
+}) {
   const [definition, setDefinition] = useState(() =>
-    wrapDefinition(startDefinition)
+    wrapDefinition({ sequence: [], properties: {} } as Definition)
   );
+
+  useEffect(() => {
+    if (loadedAlertFile == null) {
+      setDefinition(
+        wrapDefinition({ sequence: [], properties: {} } as Definition)
+      );
+    } else {
+      setDefinition(wrapDefinition(parseAlert(loadedAlertFile!)));
+    }
+  }, [loadedAlertFile]);
 
   const stepsConfiguration: StepsConfiguration = {
     iconUrlProvider: IconUrlProvider,
@@ -115,74 +178,21 @@ function Builder() {
   const validatorConfiguration: ValidatorConfiguration = {
     step: stepValidator,
   };
-  const toolboxConfiguration = {
-    groups: [
-      // {
-      //   name: "Steps",
-      //   steps: [
-      //     {
-      //       type: "query",
-      //       componentType: "task",
-      //       name: "Query",
-      //       properties: {},
-      //     },
-      //   ],
-      // },
-      // {
-      //   name: "Actions",
-      //   steps: [
-      //     {
-      //       type: "notify",
-      //       componentType: "task",
-      //       name: "Notify",
-      //       properties: {},
-      //     },
-      //   ],
-      // },
-      {
-        name: "Misc",
-        steps: [
-          {
-            type: "for",
-            componentType: "container",
-            name: "Foreach",
-            properties: {},
-            sequence: [],
-          },
-        ],
-      },
-      {
-        name: "Conditions",
-        steps: [
-          {
-            type: "condition",
-            componentType: "switch",
-            name: "Threshold",
-            properties: {
-              value: "",
-              compare_to: "",
-            },
-            branches: {
-              true: [],
-              false: [],
-            },
-          },
-        ],
-      },
-    ],
-  };
   return (
-    <SequentialWorkflowDesigner
-      definition={definition}
-      onDefinitionChange={setDefinition}
-      stepsConfiguration={stepsConfiguration}
-      validatorConfiguration={validatorConfiguration}
-      toolboxConfiguration={toolboxConfiguration}
-      undoStackSize={10}
-      controlBar={true}
-      globalEditor={<></>}
-      stepEditor={<StepEditor />}
-    />
+    <>
+      {fileName ? <Title>Current loaded file: {fileName}</Title> : null}
+      <SequentialWorkflowDesigner
+        definition={definition}
+        onDefinitionChange={setDefinition}
+        stepsConfiguration={stepsConfiguration}
+        validatorConfiguration={validatorConfiguration}
+        toolboxConfiguration={toolboxConfiguration}
+        undoStackSize={10}
+        controlBar={true}
+        globalEditor={<></>}
+        stepEditor={<StepEditor />}
+      />
+    </>
   );
 }
 
--- keep-ui/app/builder/page.tsx
+++ keep-ui/app/builder/page.tsx
@@ -1,19 +1,70 @@
 "use client";
 
-import { Card, Title, Text } from "@tremor/react";
+import { Card, Title, Text, Button } from "@tremor/react";
 import dynamic from "next/dynamic";
+import { useState } from "react";
+import Loader from "./loader";
 
 const Builder = dynamic(() => import("./builder"), {
   ssr: false, // Prevents server-side rendering
 });
 
 export default function Page() {
+  const [fileContents, setFileContents] = useState<string | null>("");
+  const [fileName, setFileName] = useState("");
+
+  function loadAlert() {
+    document.getElementById("alertFile")?.click();
+  }
+
+  function newAlert() {
+    setFileContents(null);
+    setFileName("");
+  }
+
+  function handleFileChange(event: any) {
+    const file = event.target.files[0];
+    const fName = event.target.files[0].name;
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      setFileName(fName);
+      const contents = event.target!.result as string;
+      setFileContents(contents);
+    };
+    reader.readAsText(file);
+  }
+
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl h-full">
-      <Title>Builder</Title>
-      <Text>Alert building kit</Text>
+      <div className="flex justify-between">
+        <div className="flex flex-col">
+          <Title>Builder</Title>
+          <Text>Alert building kit</Text>
+        </div>
+        <div>
+          <Button color="orange" size="md" className="mr-2" onClick={newAlert}>
+            +
+          </Button>
+          <Button color="orange" size="md" className="mr-2" onClick={loadAlert}>
+            Load
+          </Button>
+          <input
+            type="file"
+            id="alertFile"
+            style={{ display: "none" }}
+            onChange={handleFileChange}
+          />
+          <Button disabled={true} color="orange" size="md">
+            Generate 🚧
+          </Button>
+        </div>
+      </div>
       <Card className="p-4 md:p-10 h-5/6 mx-auto max-w-7xl mt-6">
-        <Builder />
+        {fileContents == "" ? (
+          <Loader />
+        ) : (
+          <Builder loadedAlertFile={fileContents} fileName={fileName} />
+        )}
       </Card>
     </main>
   );
