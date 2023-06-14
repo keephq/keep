ceb6257c9d74052e736c3694594d5e39242afbf5
talboren <tal@keephq.dev>
2023-06-14 17:10:46 +0200

    fix: dont append main to api url (#170)

--- keep-ui/next_build.sh
+++ keep-ui/next_build.sh
@@ -2,9 +2,11 @@
 
 if [ -n "$VERCEL_GIT_COMMIT_REF" ]; then
     branch_name_sanitized=$(echo $VERCEL_GIT_COMMIT_REF | sed 's/\//-/g' | cut -c 1-63)
-    # Here we replace 'keep-api' in the URL with 'keep-api-{sanitizedBranchName}'
-    export NEXT_PUBLIC_API_URL=$(echo $NEXT_PUBLIC_API_URL | sed "s|keep-api|keep-api-${branch_name_sanitized}|")
-    export API_URL=$(echo $API_URL | sed "s|keep-api|keep-api-${branch_name_sanitized}|")
+    # Here we replace 'keep-api' in the URL with 'keep-api-{sanitizedBranchName}' if the branch is not main
+    if [ "$branch_name_sanitized" != "main" ]; then
+        export NEXT_PUBLIC_API_URL=$(echo $NEXT_PUBLIC_API_URL | sed "s|keep-api|keep-api-${branch_name_sanitized}|")
+        export API_URL=$(echo $API_URL | sed "s|keep-api|keep-api-${branch_name_sanitized}|")
+    fi
 fi
 
 # Then run the build
