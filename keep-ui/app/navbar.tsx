65494fabf3bfca467aab3bb18de68ff372a2b2a9
Shahar Glazner <shaharglazner@gmail.com>
2023-06-22 16:44:13 +0300

    feat: add control k (#188)

--- keep-ui/app/navbar.tsx
+++ keep-ui/app/navbar.tsx
@@ -3,12 +3,15 @@
 import PHProvider from "./posthog-client";
 import NavbarInner from "./navbar-inner";
 import { useSession } from "../utils/customAuth";
+import { CMDK } from "./command-menu";
+
 
 export default function Navbar() {
   const { data: session } = useSession();
   return (
     <PHProvider>
       <NavbarInner user={session?.user} />
+      <CMDK />
     </PHProvider>
   );
 }
