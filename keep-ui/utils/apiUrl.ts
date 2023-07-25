52a66fbb3cb53066b4d6c6b14a07c060babd55c9
Shahar Glazner <shaharglazner@gmail.com>
2023-07-25 15:10:16 +0300

    feat: add support for reverse proxy (#217)

--- keep-ui/utils/apiUrl.ts
+++ keep-ui/utils/apiUrl.ts
@@ -11,7 +11,16 @@ export function getApiURL(): string {
   if (componentType === "server") {
     apiUrl = process.env.API_URL!;
   } else {
-    apiUrl =  process.env.NEXT_PUBLIC_API_URL!;
+    // if the NEXT_PUBLIC_API_URL is set, use it
+    // and fetch the data from the browser
+    if(process.env.NEXT_PUBLIC_API_URL){
+      apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    }
+    // otherwise, use the "/backend" which will be handled in next.config.ts rewrites
+    // this is for cases, such as k8s, where we will have only access to the frontend (port 3000)
+    else{
+      apiUrl = "/backend";
+    }
   }
   return apiUrl;
 }
