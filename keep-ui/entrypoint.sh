f5c3ab9b5eb8673eb6595f13885df7103ba25b73
shahargl <shaharglazner@gmail.com>
2023-07-25 21:30:05 +0300

    fix: .

--- keep-ui/entrypoint.sh
+++ keep-ui/entrypoint.sh
@@ -3,7 +3,7 @@
 if [ -n "$API_URL" ]; then
   echo "Patching API_URL - ${API_URL}"
   # In case it's deployed on Kubernetes using the helm chart, we need to replace the NEXT_PUBLIC_API_URL
-  find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_API_URL-keep-api#$API_URL#g"
+  find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#http://APP_API_URL-keep-api#$API_URL#g"
 
 fi
 
