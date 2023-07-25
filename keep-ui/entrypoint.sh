73c6b91b05adf470cef133a3e52ebdf33b7696fe
shahargl <shaharglazner@gmail.com>
2023-07-25 21:24:10 +0300

    fix: .

--- keep-ui/entrypoint.sh
+++ keep-ui/entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-if [ -n "$NEXT_PUBLIC_API_URL" ]; then
-  echo "Patching NEXT_PUBLIC_API_URL - ${NEXT_PUBLIC_API_URL}"
+if [ -n "$API_URL" ]; then
+  echo "Patching API_URL - ${API_URL}"
   # In case it's deployed on Kubernetes using the helm chart, we need to replace the NEXT_PUBLIC_API_URL
-  find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_API_URL-keep-api#$NEXT_PUBLIC_API_URL#g"
+  find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_API_URL-keep-api#$API_URL#g"
+
 fi
 
 echo "Starting Nextjs"
