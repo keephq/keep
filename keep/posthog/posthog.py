32329825dbefa3eccfa5ba1407f6f2dc7257197d
talboren <tal@keephq.dev>
2023-08-02 12:12:08 +0300

    chore: add a few more posthog events (#230)

--- keep/posthog/posthog.py
+++ keep/posthog/posthog.py
@@ -7,6 +7,19 @@ if not os.getenv("DISABLE_POSTHOG"):
     posthog.disabled = True
 
 
+def get_random_user_id():
+    if os.path.exists("RANDOM_USER_ID"):
+        with open("RANDOM_USER_ID") as f:
+            return f.read()
+    else:
+        import uuid
+
+        random_user_id = str(uuid.uuid4())
+        with open("RANDOM_USER_ID", "w") as f:
+            f.write(random_user_id)
+        return random_user_id
+
+
 def get_posthog_client():
     posthog_api_key = (
         os.getenv("POSTHOG_API_KEY")
