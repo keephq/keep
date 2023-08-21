e8da4906d4b9eb31bccb684f25ae6d36a1579ce7
Tal Borenstein <tal@keephq.dev>
2023-08-21 10:17:01 +0300

    fix: webhook settings page and todo

--- keep-ui/app/settings/settings.client.tsx
+++ keep-ui/app/settings/settings.client.tsx
@@ -5,6 +5,12 @@ import { UsersSettings } from "./users-settings";
 import { WebhookSettings } from "./webhook-settings";
 
 export default function SettingsPage() {
+  /**
+   * TODO: Refactor this page to use pages
+   * Right now we load all components at once when we load the main settings page.
+   * It should be /settings/users and /settings/webhook, etc.
+   * Think about a proper way to implement it.
+   */
   return (
     <TabGroup>
       <TabList color="orange">
