3debeb7a895ddf5d2dc7266eb6cd12d36866c905
Shahar Glazner <shaharglazner@gmail.com>
2023-08-29 17:32:25 +0300

    feat: workflow tremor (#270)

--- keep-ui/app/workflows/dragndrop.tsx
+++ keep-ui/app/workflows/dragndrop.tsx
@@ -2,17 +2,13 @@ import React, { useRef, useState } from "react";
 import { getApiURL } from "../../utils/apiUrl";
 import { useSession } from "../../utils/customAuth";
 
-
-const DragAndDrop: React.FC = () => {
-  const [isDragActive, setIsDragActive] = useState(false);
+const FileUpload: React.FC = () => {
   const apiUrl = getApiURL();
   const { data: session } = useSession();
   const [error, setError] = useState<string | null>(null);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-
   const onDrop = async (files: any) => {
-    setIsDragActive(false);
     const formData = new FormData();
     formData.append("file", files[0]);
 
@@ -26,7 +22,6 @@ const DragAndDrop: React.FC = () => {
       });
 
       if (response.ok) {
-        // managed to upload the workflow
         setError(null);
         if(fileInputRef.current) {
           fileInputRef.current.value = "";
@@ -34,75 +29,40 @@ const DragAndDrop: React.FC = () => {
         window.location.reload();
       } else {
         const errorMessage = await response.text();
-        setError(errorMessage); // Set the error message
+        setError(errorMessage);
         if(fileInputRef.current) {
           fileInputRef.current.value = "";
         }
-        console.error("Failed to upload file");
       }
     } catch (error) {
       setError("An error occurred during file upload");
       if(fileInputRef.current) {
         fileInputRef.current.value = "";
       }
-      console.error("An error occurred during file upload", error);
     }
-  };
-
-  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setIsDragActive(true);
-  };
-
-  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setIsDragActive(false);
-  };
-
-  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
   };
 
   return (
     <div
-      className={`flex flex-col items-center justify-center h-full w-1/3 mx-auto p-4 border border-gray-300 rounded-lg ${
-        isDragActive ? "border-blue-500" : ""
-      } my-4`}
-      onDragEnter={handleDragEnter}
-      onDragLeave={handleDragLeave}
-      onDragOver={(e) => {
-        e.preventDefault(); // Prevent the default behavior
-        handleDragOver(e);
-      }}
-      onDrop={(e) => {
-        e.preventDefault();
-        setIsDragActive(false);
-        onDrop(e.dataTransfer.files);
-      }}
+      className="absolute top-0 left-0 mt-4 ml-4"
     >
-        <>
-          <p className="mb-2">Drag and drop a workflow here</p>
-          <p className="mb-2">or</p>
-          <label className="cursor-pointer">
-            <span className="text-blue-500">Upload a workflow</span>
-            <input
-              ref={fileInputRef} // Set the ref for the file input
-              type="file"
-              className="hidden"
-              onChange={(e) => {
-                onDrop(e.target.files);
-              }}
-            />
-          </label>
-          <div className="mb-4 mt-4">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <label className="cursor-pointer flex items-center">
+        <span className="text-gray-500 mr-2">Click to upload a workflow</span>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline-block">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-          </svg>
-          </div>
-        </>
-        {error && <p className="text-red-500 mt-4">Failed to upload the file: {error}<br></br>Please try again with another file.</p>}
+        </svg>
+        <input
+          ref={fileInputRef}
+          type="file"
+          className="hidden"
+          onChange={(e) => {
+            onDrop(e.target.files);
+          }}
+        />
+      </label>
+      {error && <p className="text-red-500 mt-4">Failed to upload the file: {error}<br></br>Please try again with another file.</p>}
     </div>
   );
 };
 
-export default DragAndDrop;
+export default FileUpload;
