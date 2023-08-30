582a7cc1604b399d3a740277119d9a0e481c1fe4
talboren <tal@keephq.dev>
2023-08-30 16:43:01 +0300

    chore: touch ups and bug fixes (#272)

--- keep-ui/app/workflows/noworfklows.tsx
+++ keep-ui/app/workflows/noworfklows.tsx
@@ -1,35 +1,31 @@
 import React from "react";
-import { ClipboardIcon } from "@heroicons/react/24/outline";
-import DragAndDrop from "./dragndrop";
+import { CircleStackIcon } from "@heroicons/react/24/outline";
+import { Callout, Italic } from "@tremor/react";
+import Link from "next/link";
 
-interface NoWorkflowsProps {
-  copyCurlCommand: () => void;
-}
+const WorkflowsEmptyState = () => {
+  const loadAlert = () => document.getElementById("workflowFile")?.click();
 
-const NoWorkflows: React.FC<NoWorkflowsProps> = ({ copyCurlCommand }) => {
   return (
     <div className="text-center mt-4">
-      <p>No workflows available. </p>
-      <p>1. Use the Workflow Builder</p>
-      <p>2. Drop the Workflow file here:</p>
-      <DragAndDrop/>
-      <p>3. Use Keep API to push your YAML files:</p>
-      <div className="relative w-1/2 mx-auto">
-        <button
-          className="absolute top-0 right-0 mt-1 mr-1 p-1 rounded hover:bg-gray-200"
-          onClick={copyCurlCommand}
-          title="Copy to Clipboard"
-        >
-          <ClipboardIcon className="h-5 w-5 text-gray-500" />
-        </button>
-        <pre className="bg-gray-100 p-2 rounded">
-          <code className="whitespace-pre-wrap">
-            {`curl -X POST ...\n...`}
-          </code>
-        </pre>
-      </div>
+      <Callout
+        title="No Workflows"
+        icon={CircleStackIcon}
+        color="yellow"
+        className="mt-5"
+      >
+        You can start by uploading a workflow file using the{" "}
+        <Italic onClick={loadAlert} className="underline cursor-pointer">
+          Load a Workflow
+        </Italic>{" "}
+        button above or by using the{" "}
+        <Italic className="underline">
+          <Link href="/builder">Workflow Builder</Link>
+        </Italic>{" "}
+        to create a new workflow.
+      </Callout>
     </div>
   );
 };
 
-export default NoWorkflows;
+export default WorkflowsEmptyState;
