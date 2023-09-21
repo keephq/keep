b8634d01f4ad0162c245bedbb5732a57638a656b
Tal Borenstein <tal@keephq.dev>
2023-09-21 18:40:11 +0300

    fix: grafana provider webhook name

--- keep/providers/grafana_provider/grafana_provider.py
+++ keep/providers/grafana_provider/grafana_provider.py
@@ -135,7 +135,8 @@ class GrafanaProvider(BaseProvider):
     ):
         self.logger.info("Setting up webhook")
         webhook_name = (
-            GrafanaProvider.KEEP_GRAFANA_WEBHOOK_INTEGRATION_NAME + f"-{tenant_id}"
+            GrafanaProvider.KEEP_GRAFANA_WEBHOOK_INTEGRATION_NAME
+            + f"-{tenant_id.split('-')[0]}"
         )
         headers = {"Authorization": f"Bearer {self.authentication_config.token}"}
         contacts_api = f"{self.authentication_config.host}{APIEndpoints.ALERTING_PROVISIONING.value}/contact-points"
@@ -167,7 +168,10 @@ class GrafanaProvider(BaseProvider):
                     "authorization_credentials": api_key,
                 },
             }
-            requests.post(contacts_api, json=webhook, headers=headers)
+            response = requests.post(contacts_api, json=webhook, headers=headers)
+            if not response.ok:
+                raise Exception(response.json())
+            self.logger.info(f"Created webhook {webhook_name}")
         if setup_alerts:
             self.logger.info("Setting up alerts")
             policies_api = f"{self.authentication_config.host}{APIEndpoints.ALERTING_PROVISIONING.value}/policies"
