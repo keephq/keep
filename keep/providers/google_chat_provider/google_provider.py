1b223e503454078d440a07197b0abb81386cc453
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-09-21 23:49:45 +0530

    Create google_provider.py
    Signed-off-by: Rishi Raj Jain <rishi18304@iiitd.ac.in>

---
+++ keep/providers/google_chat_provider/google_provider.py
@@ -0,0 +1,75 @@
+import os
+import requests
+from keep.providers.base.base_provider import BaseProvider
+from keep.providers.models.provider_config import ProviderConfig
+from keep.contextmanager.contextmanager import ContextManager
+from keep.exceptions.provider_exception import ProviderException
+
+class GoogleChatProvider(BaseProvider):
+    GOOGLE_CHAT_API = "https://chat.googleapis.com/v1/spaces/"
+
+    def __init__(self, context_manager: ContextManager, provider_id: str, config: ProviderConfig):
+        super().__init__(context_manager, provider_id, config)
+
+    def validate_config(self):
+        if not self.config.authentication.get("webhook_url"):
+            raise Exception("Google Chat webhook URL is required")
+
+    def dispose(self):
+        """
+        No need to dispose of anything, so just do nothing.
+        """
+        pass
+
+    def notify(self, message="", cards=[], space="", **kwargs: dict):
+        """
+        Notify a message to a Google Chat room using a webhook URL.
+
+        Args:
+            message (str): The text message to send.
+            cards (list): A list of card objects to attach to the message.
+            space (str): The Google Chat space or room ID.
+
+        Raises:
+            ProviderException: If the message could not be sent successfully.
+        """
+        self.logger.debug("Notifying message to Google Chat")
+        webhook_url = self.config.authentication.get("webhook_url")
+
+        if not message and not cards:
+            raise ProviderException("Message or cards are required")
+
+        if not space:
+            raise ProviderException("Google Chat space (room) is required")
+
+        payload = {
+            "text": message,
+            "cards": cards,
+        }
+
+        response = requests.post(f"{GoogleChatProvider.GOOGLE_CHAT_API}{space}/messages", json=payload)
+
+        if not response.ok:
+            raise ProviderException(f"Failed to notify message to Google Chat: {response.text}")
+
+if __name__ == "__main__":
+    # Output debug messages
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG, handlers=[logging.StreamHandler()])
+    context_manager = ContextManager(
+        tenant_id="singletenant",
+        workflow_id="test",
+    )
+
+    # Load environment variables
+    google_chat_webhook_url = os.environ.get("GOOGLE_CHAT_WEBHOOK_URL")
+
+    # Initialize the provider and provider config
+    config = ProviderConfig(
+        id="google-chat-test",
+        description="Google Chat Output Provider",
+        authentication={"webhook_url": google_chat_webhook_url},
+    )
+    provider = GoogleChatProvider(context_manager, provider_id="google-chat", config=config)
+    provider.notify(message="Simple alert showing context with name: John Doe", space="YOUR_GOOGLE_CHAT_SPACE_ID")
