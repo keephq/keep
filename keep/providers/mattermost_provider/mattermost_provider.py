19ed7de6c9fd03c1041c25b9e0d2770e810ddc9e
Vishal-Sikka <145092393+Vishal-Sikka@users.noreply.github.com>
2023-09-27 18:23:07 +0530

    fix: #323 Implement Mattermost Provider (#358)
    Signed-off-by: Vishal-Sikka <145092393+Vishal-Sikka@users.noreply.github.com>
    Co-authored-by: Shahar Glazner <shaharglazner@gmail.com>

---
+++ keep/providers/mattermost_provider/mattermost_provider.py
@@ -0,0 +1,91 @@
+
+import dataclasses
+import pydantic
+import requests
+
+from keep.contextmanager.contextmanager import ContextManager
+from keep.exceptions.provider_exception import ProviderException
+from keep.providers.base.base_provider import BaseProvider
+from keep.providers.models.provider_config import ProviderConfig
+
+@pydantic.dataclasses.dataclass
+class MattermostProviderAuthConfig:
+    """Mattermost authentication configuration."""
+
+    webhook_url: str = dataclasses.field(
+        metadata={
+            "required": True,
+            "description": "Mattermost Webhook Url",
+            "sensitive": True,
+        }
+    )
+
+class MattermostProvider(BaseProvider):
+    def __init__(
+        self, context_manager: ContextManager, provider_id: str, config: ProviderConfig
+    ):
+        super().__init__(context_manager, provider_id, config)
+
+    def validate_config(self):
+        self.authentication_config = MattermostProviderAuthConfig(
+            **self.config.authentication
+        )
+        if not self.authentication_config.webhook_url:
+            raise Exception("Mattermost webhook URL is required")
+
+    def dispose(self):
+        """
+        No need to dispose of anything, so just do nothing.
+        """
+        pass
+
+    def notify(self, message="", blocks=[], channel="", **kwargs: dict):
+        """
+        Notify alert message to Mattermost using the Mattermost Incoming Webhook API
+        https://docs.mattermost.com/developer/webhooks-incoming.html
+
+        Args:
+            kwargs (dict): The providers with context
+        """
+        self.logger.debug("Notifying alert message to Mattermost")
+        if not message:
+            message = blocks[0].get("text")
+        webhook_url = self.authentication_config.webhook_url
+        payload = {
+            "text": message,
+            "blocks": blocks
+        }
+        if channel:
+            payload["channel"] = channel
+
+        response = requests.post(webhook_url, json=payload)
+
+        if not response.ok:
+            raise ProviderException(
+                f"{self.__class__.__name__} failed to notify alert message to Mattermost: {response.text}"
+            )
+
+        self.logger.debug("Alert message notified to Mattermost")
+
+if __name__ == "__main__":
+    # Output debug messages
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG, handlers=[logging.StreamHandler()])
+    context_manager = ContextManager(
+        tenant_id="singletenant",
+        workflow_id="test",
+    )
+    # Load environment variables
+    import os
+
+    mattermost_webhook_url = os.environ.get("MATTERMOST_WEBHOOK_URL")
+
+    # Initalize the provider and provider config
+    config = ProviderConfig(
+        id="mattermost-test",
+        description="Mattermost Output Provider",
+        authentication={"webhook_url": mattermost_webhook_url},
+    )
+    provider = MattermostProvider(context_manager, provider_id="mattermost", config=config)
+    provider.notify(message="Simple alert showing context with name: John Doe")
