500c99a059de22222fc8fd822acb02d0b39ece46
talboren <tal@keephq.dev>
2023-10-01 17:41:36 +0200

    feat: pagerduty webhook (#373)

--- keep/providers/pagerduty_provider/pagerduty_provider.py
+++ keep/providers/pagerduty_provider/pagerduty_provider.py
@@ -7,10 +7,12 @@ import uuid
 import pydantic
 import requests
 
+from keep.api.models.alert import AlertDto
 from keep.contextmanager.contextmanager import ContextManager
 from keep.exceptions.provider_config_exception import ProviderConfigException
 from keep.providers.base.base_provider import BaseProvider
 from keep.providers.models.provider_config import ProviderConfig
+from keep.providers.providers_factory import ProvidersFactory
 
 # Todo: think about splitting in to PagerdutyIncidentsProvider and PagerdutyAlertsProvider
 # Read this: https://community.pagerduty.com/forum/t/create-incident-using-python/3596/3
@@ -36,6 +38,8 @@ class PagerdutyProviderAuthConfig:
 
 
 class PagerdutyProvider(BaseProvider):
+    SUBSCRIPTION_API_URL = "https://api.pagerduty.com/webhook_subscriptions"
+
     def __init__(
         self, context_manager: ContextManager, provider_id: str, config: ProviderConfig
     ):
@@ -82,7 +86,7 @@ class PagerdutyProvider(BaseProvider):
             },
         }
 
-    def _send_alert(self, title: str, alert_body: str, dedup: str | None = None):
+    def _send_alert(self, title: str, body: str, dedup: str | None = None):
         """
         Sends PagerDuty Alert
 
@@ -97,7 +101,7 @@ class PagerdutyProvider(BaseProvider):
 
         url = "https://events.pagerduty.com//v2/enqueue"
 
-        result = requests.post(url, json=self._build_alert(title, alert_body, dedup))
+        result = requests.post(url, json=self._build_alert(title, body, dedup))
 
         self.logger.debug("Alert status: %s", result.status_code)
         self.logger.debug("Alert response: %s", result.text)
@@ -144,7 +148,143 @@ class PagerdutyProvider(BaseProvider):
         """
         pass
 
-    def notify(self, **kwargs: dict):
+    def setup_webhook(
+        self, tenant_id: str, keep_api_url: str, api_key: str, setup_alerts: bool = True
+    ):
+        self.logger.info("Setting up Pagerduty webhook")
+        headers = {"Authorization": f"Token token={self.authentication_config.api_key}"}
+        request = requests.get(self.SUBSCRIPTION_API_URL, headers=headers)
+        if not request.ok:
+            raise Exception("Could not get existing webhooks")
+        existing_webhooks = request.json().get("webhook_subscriptions", [])
+        webhook_exists = next(
+            iter(
+                [
+                    webhook
+                    for webhook in existing_webhooks
+                    if tenant_id in webhook.get("description")
+                ]
+            ),
+            False,
+        )
+        webhook_payload = {
+            "webhook_subscription": {
+                "type": "webhook_subscription",
+                "delivery_method": {
+                    "type": "http_delivery_method",
+                    "url": keep_api_url,
+                    "custom_headers": [{"name": "X-API-KEY", "value": api_key}],
+                },
+                "description": f"Keep Pagerduty webhook ({tenant_id}) - do not change",
+                "events": [
+                    "incident.acknowledged",
+                    "incident.annotated",
+                    "incident.delegated",
+                    "incident.escalated",
+                    "incident.priority_updated",
+                    "incident.reassigned",
+                    "incident.reopened",
+                    "incident.resolved",
+                    "incident.responder.added",
+                    "incident.responder.replied",
+                    "incident.triggered",
+                    "incident.unacknowledged",
+                ],
+                "filter": {"type": "account_reference"},
+            },
+        }
+        if webhook_exists:
+            self.logger.info("Webhook already exists, removing and re-creating")
+            webhook_id = webhook_exists.get("id")
+            request = requests.delete(
+                f"{self.SUBSCRIPTION_API_URL}/{webhook_id}", headers=headers
+            )
+            if not request.ok:
+                raise Exception("Could not remove existing webhook")
+            self.logger.info("Webhook removed", extra={"webhook_id": webhook_id})
+
+        self.logger.info("Creating Pagerduty webhook")
+        request = requests.post(
+            self.SUBSCRIPTION_API_URL,
+            headers=headers,
+            json=webhook_payload,
+        )
+        if not request.ok:
+            self.logger.error("Failed to add webhook", extra=request.json())
+            raise Exception("Could not create webhook")
+        self.logger.info("Webhook created")
+
+    def get_alerts(self) -> list[AlertDto]:
+        request = requests.get(
+            "https://api.pagerduty.com/incidents",
+            headers={
+                "Authorization": f"Token token={self.authentication_config.api_key}",
+            },
+        )
+        if not request.ok:
+            self.logger.error("Failed to get alerts", extra=request.json())
+            raise Exception("Could not get alerts")
+        incidents = request.json().get("incidents", [])
+        incidents = [
+            self.format_alert({"event": {"data": incident}}) for incident in incidents
+        ]
+        return incidents
+
+    @staticmethod
+    def __get_priorty(priority):
+        if priority == "P1":
+            return "critical"
+        elif priority == "P2":
+            return "high"
+        elif priority == "P3":
+            return "medium"
+        elif priority == "P4":
+            return "low"
+        return "lowest"
+
+    @staticmethod
+    def format_alert(event: dict) -> AlertDto:
+        actual_event = event.get("event", {})
+        data = actual_event.get("data", {})
+        url = data.pop("self", data.pop("html_url"))
+        status = data.pop("status")
+        last_received = data.pop("created_at")
+        name = data.pop("title")
+        service = data.pop("service", {}).get("summary", "unknown")
+        priority_summary = (data.get("priority", {}) or {}).get("summary")
+        priority = PagerdutyProvider.__get_priorty(priority_summary)
+        environment = next(
+            iter(
+                [
+                    x
+                    for x in data.pop("custom_fields", [])
+                    if x.get("name") == "environment"
+                ]
+            ),
+            {},
+        ).get("value", "unknown")
+        return AlertDto(
+            **data,
+            url=url,
+            status=status,
+            lastReceived=last_received,
+            name=name,
+            severity=priority,
+            environment=environment,
+            source=["pagerduty"],
+            service=service,
+        )
+
+    def notify(
+        self,
+        title: str = "",
+        alert_body: str = "",
+        dedup: str = "",
+        service_id: str = "",
+        requester: str = "",
+        incident_id: str = "",
+        **kwargs: dict,
+    ):
         """
         Create a PagerDuty alert.
             Alert/Incident is created either via the Events API or the Incidents API.
@@ -154,6 +294,39 @@ class PagerdutyProvider(BaseProvider):
             kwargs (dict): The providers with context
         """
         if self.authentication_config.routing_key:
-            self._send_alert(**kwargs)
+            self._send_alert(title, alert_body, dedup=dedup, **kwargs)
         else:
-            self._trigger_incident(**kwargs)
+            self._trigger_incident(
+                service_id, title, alert_body, requester, incident_id, **kwargs
+            )
+
+
+if __name__ == "__main__":
+    # Output debug messages
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG, handlers=[logging.StreamHandler()])
+    context_manager = ContextManager(
+        tenant_id="singletenant",
+        workflow_id="test",
+    )
+    # Load environment variables
+    import os
+
+    api_key = os.environ.get("PAGERDUTY_API_KEY")
+
+    provider_config = {
+        "authentication": {"api_key": api_key},
+    }
+    provider = ProvidersFactory.get_provider(
+        context_manager=context_manager,
+        provider_id="keep-pd",
+        provider_type="pagerduty",
+        provider_config=provider_config,
+    )
+    results = provider.setup_webhook(
+        "keep",
+        "https://eb8a-77-137-44-66.ngrok-free.app/alerts/event/pagerduty?provider_id=keep-pd",
+        "just-a-test",
+        True,
+    )
