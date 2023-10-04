9ae590497d19856f1312d051dda7c33340fb37ad
Shahar Glazner <shaharglazner@gmail.com>
2023-10-04 16:14:04 +0300

    feat: gke (#375)

--- tests/test_iohandler.py
+++ tests/test_iohandler.py
@@ -21,27 +21,19 @@ def test_with_basic_context(context_manager):
     context_manager.steps_context = {
         "name": "s",
     }
-    context_manager.actions_context = {
+    context_manager.providers_context = {
         "name": "s2",
     }
-    context_manager.providers_context = {
-        "name": "s3",
-    }
     s = iohandler.render("hello {{ steps.name }}")
-    s2 = iohandler.render("hello {{ actions.name }}")
-    s3 = iohandler.render("hello {{ providers.name }}")
+    s2 = iohandler.render("hello {{ providers.name }}")
     assert s == "hello s"
     assert s2 == "hello s2"
-    assert s3 == "hello s3"
 
 
 def test_with_function(context_manager):
     iohandler = IOHandler(context_manager)
     context_manager.steps_context = {
         "some_list": [1, 2, 3],
-    }
-    context_manager.actions_context = {
-        "some_string": "abcde",
     }
     context_manager.providers_context = {
         "name": "s3",
@@ -55,13 +47,8 @@ def test_with_function_2(context_manager):
     context_manager.steps_context = {
         "some_list": [1, 2, 3],
     }
-    context_manager.actions_context = {
-        "some_string": "abcde",
-    }
     context_manager.providers_context = {
         "name": "s3",
     }
     s = iohandler.render("hello keep.first({{ steps.some_list }})")
-    s1 = iohandler.render("hello keep.len({{ actions.some_string }})")
     assert s == "hello 1"
-    assert s1 == "hello 5"
