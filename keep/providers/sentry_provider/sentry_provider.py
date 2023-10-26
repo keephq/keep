02c7c520ba696bb0d1bf2f8d817402f43fc3f072
Tal Borenstein <tal@keephq.dev>
2023-10-26 14:01:18 +0300

    fix: sentry working

--- keep/providers/sentry_provider/sentry_provider.py
+++ keep/providers/sentry_provider/sentry_provider.py
@@ -2,6 +2,7 @@
 SentryProvider is a class that provides a way to read data from Sentry.
 """
 import dataclasses
+import datetime
 
 import pydantic
 import requests
@@ -181,7 +182,145 @@ class SentryProvider(BaseProvider):
 
     @staticmethod
     def format_alert(event: dict) -> AlertDto | list[AlertDto]:
-        print(event)
+        event_data = event.get("event", {})
+        tags_as_dict = {v[0]: v[1] for v in event_data.get("tags", [])}
+        hashes = event_data.get("hashes", [])
+        return AlertDto(
+            id=event_data.pop("event_id"),
+            name=event_data.get("metadata", {}).get(
+                "type", event_data.get("metadata", {}).get("title")
+            ),
+            status=event.get("action", "triggered"),
+            lastReceived=event_data.get(
+                "datetime",
+                str(datetime.datetime.fromtimestamp(event_data.get("received"))),
+            ),
+            service=tags_as_dict.get("server_name"),
+            source=["sentry"],
+            message=event_data.get("metadata", {}).get("value"),
+            description=event_data.get("metadata", {}).get("value"),
+            pushed=True,
+            severity=event.pop("level", "high"),
+            url=event_data.pop("url"),
+            fingerprint=hashes[0] if len(hashes) > 0 else None,
+            **event_data,
+        )
+
+    def setup_webhook(
+        self, tenant_id: str, keep_api_url: str, api_key: str, setup_alerts: bool = True
+    ):
+        self.logger.info("Setting up Sentry webhook")
+        headers = {"Authorization": f"Bearer {self.authentication_config.api_key}"}
+        if self.project_slug:
+            project_slugs = [self.project_slug]
+        else:
+            # Get all projects if no project slug was given
+            projects_response = requests.get(
+                f"{self.SENTRY_API}/projects/",
+                headers=headers,
+            )
+            if not projects_response.ok:
+                raise Exception("Failed to get projects")
+            project_slugs = [
+                project.get("slug") for project in projects_response.json()
+            ]
+
+        for project_slug in project_slugs:
+            self.logger.info(f"Setting up webhook for project {project_slug}")
+            webhooks_request = requests.get(
+                f"{self.SENTRY_API}/projects/{self.sentry_org_slug}/{project_slug}/plugins/webhooks/",
+                headers=headers,
+            )
+            webhooks_request.raise_for_status()
+            webhooks_response = webhooks_request.json()
+            # Get existing urls so we won't override anything
+            config = next(
+                iter(
+                    [
+                        c
+                        for c in webhooks_response.get("config")
+                        if c.get("name") == "urls"
+                    ]
+                )
+            )
+            existing_webhooks_value: str = config.get("value", "")
+            existing_webhooks = existing_webhooks_value.split("\n")
+            # This means we already installed in that project
+            if f"{keep_api_url}?api_key={api_key}" in existing_webhooks:
+                # TODO: we might got here but did not create the alert, we should fix that in the future
+                #   e.g. make sure the alert exists and if not create it.
+                self.logger.info(
+                    f"Keep webhook already exists for project {project_slug}"
+                )
+                continue
+            existing_webhooks.append(f"{keep_api_url}?api_key={api_key}")
+            # Update the webhooks urls
+            update_response = requests.put(
+                f"{self.SENTRY_API}/projects/{self.sentry_org_slug}/{project_slug}/plugins/webhooks/",
+                headers=headers,
+                json={"urls": "\n".join(existing_webhooks)},
+            )
+            update_response.raise_for_status()
+            # Enable webhooks plugin for project
+            requests.post(
+                f"{self.SENTRY_API}/projects/{self.sentry_org_slug}/{project_slug}/plugins/webhooks/",
+                headers=headers,
+            ).raise_for_status()
+            # TODO: make sure keep alert does not exist and if it doesnt create it.
+            alert_name = f"Keep Alert Rule - {project_slug}"
+            alert_rules_response = requests.get(
+                f"{self.SENTRY_API}/projects/{self.sentry_org_slug}/{project_slug}/rules/",
+                headers=headers,
+            ).json()
+            alert_exists = next(
+                iter(
+                    [
+                        alert
+                        for alert in alert_rules_response
+                        if alert.get("name") == alert_name
+                    ]
+                ),
+                None,
+            )
+            if not alert_exists:
+                alert_payload = {
+                    "conditions": [
+                        {
+                            "id": "sentry.rules.conditions.first_seen_event.FirstSeenEventCondition",
+                        },
+                        {
+                            "id": "sentry.rules.conditions.regression_event.RegressionEventCondition",
+                        },
+                        {
+                            "id": "sentry.rules.conditions.reappeared_event.ReappearedEventCondition",
+                        },
+                    ],
+                    "filters": [],
+                    "actions": [
+                        {
+                            "service": "webhooks",
+                            "id": "sentry.rules.actions.notify_event_service.NotifyEventServiceAction",
+                            "name": "Send a notification via webhooks",
+                        },
+                    ],
+                    "actionMatch": "any",
+                    "filterMatch": "any",
+                    "frequency": 5,
+                    "name": alert_name,
+                    "dateCreated": "2023-10-09T13:40:37.144220Z",
+                    "projects": [project_slug],
+                    "status": "active",
+                }
+
+                requests.post(
+                    f"{self.SENTRY_API}/projects/{self.sentry_org_slug}/{project_slug}/rules/",
+                    headers=headers,
+                    json=alert_payload,
+                ).raise_for_status()
+                self.logger.info(f"Sentry webhook setup complete for {project_slug}")
+            else:
+                self.logger.info(f"Sentry webhook already exists for {project_slug}")
+        self.logger.info("Sentry webhook setup complete")
 
     def get_alerts(self, project_slug: str = None) -> list[AlertDto]:
         # get issues
