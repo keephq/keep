8dc44603cbf35367c3984b45eab82f676e9e5a7e
Tal Borenstein <tal@keephq.dev>
2023-11-02 15:24:40 +0200

    chore: remove unsued imports

--- keep-ui/app/alerts/alert-menu.tsx
+++ keep-ui/app/alerts/alert-menu.tsx
@@ -1,5 +1,5 @@
 import { Menu, Transition } from "@headlessui/react";
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment } from "react";
 import { Bars3Icon } from "@heroicons/react/20/solid";
 import { Icon } from "@tremor/react";
 import { TrashIcon } from "@radix-ui/react-icons";
