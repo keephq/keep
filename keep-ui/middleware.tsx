5afe59182468cd7d25380fcac7f01e7bcad67b02
Shahar Glazner <shaharglazner@gmail.com>
2023-11-04 14:06:21 +0200

    feat: add users management to single tenant (#436)

--- keep-ui/middleware.tsx
+++ keep-ui/middleware.tsx
@@ -1,27 +1,24 @@
 import { withAuth } from "next-auth/middleware";
 
-const isSingleTenant = process.env.NEXT_PUBLIC_AUTH_ENABLED == "false";
 
-export default isSingleTenant
-  ? () => {}
-  : withAuth({
-      callbacks: {
-        authorized: async ({ req, token }) => {
-          const pathname = req.nextUrl.pathname;
-          if (pathname.endsWith("svg")) {
-            return true;
-          }
-          // validate that token exists and is not expired
-          // todo: understand how token shuold be validated in frontend (if it should)
-          // todo 2: refresh token?
-          if (token && (token.exp as number) > new Date().getTime() / 1000) {
-            return true;
-          }
+export default withAuth({
+    callbacks: {
+      authorized: async ({ req, token }) => {
+        const pathname = req.nextUrl.pathname;
+        if (pathname.endsWith("svg")) {
+          return true;
+        }
+        // validate that token exists and is not expired
+        // todo: understand how token shuold be validated in frontend (if it should)
+        // todo 2: refresh token?
+        if (token && (token.exp as number) > new Date().getTime() / 1000) {
+          return true;
+        }
 
-          return false;
-        },
+        return false;
       },
-      pages: {
-        signIn: "/signin",
-      },
-    });
+    },
+    pages: {
+      signIn: "/signin",
+    },
+  });
