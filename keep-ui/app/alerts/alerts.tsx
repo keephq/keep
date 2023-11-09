9028f69e46a284f5f7f62dc594f8b975e10fa1a3
Tal Borenstein <tal@keephq.dev>
2023-11-09 18:18:42 +0200

    fix: isLoading

--- keep-ui/app/alerts/alerts.tsx
+++ keep-ui/app/alerts/alerts.tsx
@@ -31,14 +31,13 @@ export default function Alerts({ accessToken }: { accessToken: string }) {
   const [selectedEnvironments, setSelectedEnvironments] = useState<string[]>(
     []
   );
-  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [alertNameSearchString, setAlertNameSearchString] =
     useState<string>("");
   const [selectedStatus, setSelectedStatus] = useState<string[]>([]);
   const {
     data,
     error,
-    isLoading: dataIsLoading,
+    isLoading,
     mutate,
   } = useSWR<Alert[]>(`${apiUrl}/alerts`, (url) => fetcher(url, accessToken));
   const { data: workflows } = useSWR<Workflow[]>(`${apiUrl}/workflows`, (url) =>
@@ -61,7 +60,7 @@ export default function Alerts({ accessToken }: { accessToken: string }) {
       </Callout>
     );
   }
-  if (dataIsLoading || isLoading || !data) return <Loading />;
+  if (isLoading || !data) return <Loading />;
 
   const environments = data
     .map((alert) => alert.environment.toLowerCase())
