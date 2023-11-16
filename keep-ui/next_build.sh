1bac88baae59be452f2aa4a3d8b3fc9804a1133f
Tal Borenstein <tal@keephq.dev>
2023-11-16 13:23:31 +0200

    fix: next_build.sh

--- keep-ui/next_build.sh
+++ keep-ui/next_build.sh
@@ -6,8 +6,20 @@ if [ -n "$VERCEL_GIT_COMMIT_REF" ]; then
     branch_name_sanitized=$(echo $VERCEL_GIT_COMMIT_REF | sed 's/\//-/g' | cut -c 1-63)
     # Here we replace 'keep-api' in the URL with 'keep-api-{sanitizedBranchName}' if the branch is not main
     if [ "$branch_name_sanitized" != "main" ]; then
-        export NEXT_PUBLIC_API_URL=$(echo $NEXT_PUBLIC_API_URL | sed "s|keep-api|keep-api-${branch_name_sanitized}|")
-        export API_URL=$(echo $API_URL | sed "s|keep-api|keep-api-${branch_name_sanitized}|")
+        service_name="keep-api-${branch_name_sanitized}"
+
+        # Ensure the service_name is no longer than 63 characters
+        if [ ${#service_name} -gt 63 ]; then
+            service_name=$(echo "$service_name" | cut -c 1-63)
+        fi
+
+        # Check if the last character of service_name is a hyphen ("-")
+        if [ "${service_name: -1}" = "-" ]; then
+            service_name="${service_name::-1}" # Remove the last character
+        fi
+
+        export NEXT_PUBLIC_API_URL=$(echo $NEXT_PUBLIC_API_URL | sed "s|keep-api|${service_name}|")
+        export API_URL=$(echo $API_URL | sed "s|keep-api|${service_name}|")
     fi
 fi
 
