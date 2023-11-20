e85da5e794444885ac05cd503ba405a0a9406e45
shahargl <shaharglazner@gmail.com>
2023-11-20 12:46:40 +0200

    feat: change

--- keep-ui/utils/authenticationType.ts
+++ keep-ui/utils/authenticationType.ts
@@ -2,7 +2,7 @@
 // AuthenticationType.ts
 
 export enum AuthenticationType {
-    MULTI_TENANT = "MULTI_TENANT", // Default
+    MULTI_TENANT = "MULTI_TENANT",
     SINGLE_TENANT = "SINGLE_TENANT",
-    NO_AUTH = "NO_AUTH"
+    NO_AUTH = "NO_AUTH"  // Default
 }
