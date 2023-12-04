eb031ac515c9852711d6a4628c90265b178b9ab5
talboren <tal@keephq.dev>
2023-12-04 13:48:44 +0200

    feat: ngrok static domain (#603)

--- keep/api/api.py
+++ keep/api/api.py
@@ -256,8 +256,19 @@ def get_app(
 
     if os.environ.get("USE_NGROK", "false") == "true":
         from pyngrok import ngrok
+        from pyngrok.conf import PyngrokConfig
 
-        public_url = ngrok.connect(PORT).public_url
+        ngrok_config = PyngrokConfig(
+            auth_token=os.environ.get("NGROK_AUTH_TOKEN", None)
+        )
+        # If you want to use a custom domain, set the NGROK_DOMAIN & NGROK_AUTH_TOKEN environment variables
+        # read https://ngrok.com/blog-post/free-static-domains-ngrok-users -> https://dashboard.ngrok.com/cloud-edge/domains
+        ngrok_connection = ngrok.connect(
+            PORT,
+            pyngrok_config=ngrok_config,
+            domain=os.environ.get("NGROK_DOMAIN", None),
+        )
+        public_url = ngrok_connection.public_url
         logger.info(f"ngrok tunnel: {public_url}")
         os.environ["KEEP_API_URL"] = public_url
 
