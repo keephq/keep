539b4a3fbf2b6b19da4336253476f63eef51f1d1
Tal Borenstein <tal@keephq.dev>
2023-12-10 12:22:42 +0200

    feat: working but menu is buggy

--- keep-ui/app/alerts/alert-filters.tsx
+++ keep-ui/app/alerts/alert-filters.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Dispatch, SetStateAction } from "react";
 import { AlertDto } from "./models";
 import CreatableSelect from "react-select/creatable";
@@ -19,15 +19,18 @@ export default function AlertFilters({
   setSelectedOptions: Dispatch<SetStateAction<Option[]>>;
 }) {
   const [options, setOptions] = useState<Option[]>([]);
+  const selectRef = useRef(null);
+  const [inputValue, setInputValue] = useState("");
   const uniqueValuesMap = new Map<string, Set<string>>();
 
   // Populating the map with keys and values
   alerts.forEach((alert) => {
     Object.entries(alert).forEach(([key, value]) => {
+      if (typeof value !== "string") return;
       if (!uniqueValuesMap.has(key)) {
         uniqueValuesMap.set(key, new Set());
       }
-      if (!uniqueValuesMap.get(key)?.has(value?.toString().trim()))
+      if (!uniqueValuesMap.get(key)?.has(value?.trim()))
         uniqueValuesMap.get(key)?.add(value?.toString().trim());
     });
   });
@@ -54,33 +57,57 @@ export default function AlertFilters({
     // Only allow creating new options if the input includes '='
     return inputValue.includes("=");
   };
+
   // Handler for key down events
   const handleKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
     const inputElement = event.target as HTMLInputElement; // Cast to HTMLInputElement
 
-    if (event.key === "Enter" || event.key === "Tab") {
+    if (event.key === "Enter") {
       if (!inputElement.value.includes("=")) {
         event.preventDefault();
       }
     }
+
+    if (event.key === "Tab") {
+      event.preventDefault();
+      // Only add to selectedOptions if focusedOption is not null
+      const select = selectRef.current as any;
+      if (select?.state.focusedOption) {
+        const value = select.state.focusedOption.value;
+        if (value.includes("=")) {
+          handleInputChange(select.state.focusedOption.value);
+        } else {
+          handleInputChange(`${value}=`);
+        }
+      }
+    }
   };
 
-  const handleChange = (selected: any) => {
-    setSelectedOptions(selected);
+  const handleChange = (selected: any, actionMeta: any) => {
+    if (
+      actionMeta.action === "select-option" &&
+      selected.some((option: any) => !option.value.includes("="))
+    ) {
+      // Handle invalid option selection
+      handleInputChange(`${selected[0].value}=`);
+      // Optionally, you can prevent the selection or handle it differently
+    } else {
+      setSelectedOptions(selected);
+    }
   };
 
   const handleInputChange = (inputValue: string) => {
+    setInputValue(inputValue);
     if (inputValue.includes("=")) {
       const [inputKey, inputValuePart] = inputValue.split("=");
       if (uniqueValuesMap.has(inputKey)) {
         const filteredValues = Array.from(
           uniqueValuesMap.get(inputKey) || []
-        ).filter((value) => value.startsWith(inputValuePart));
+        ).filter((value) => value?.startsWith(inputValuePart));
         const newOptions = filteredValues.map((value) => ({
           label: `${inputKey}=${value}`,
           value: `${inputKey}=${value}`,
         }));
-        console.log(newOptions);
         setOptions(newOptions);
       } else {
         setOptions([]);
@@ -106,9 +133,11 @@ export default function AlertFilters({
       value={selectedOptions}
       onChange={handleChange}
       onInputChange={handleInputChange}
+      inputValue={inputValue}
       filterOption={filterOption}
       onKeyDown={handleKeyDown}
       isValidNewOption={isValidNewOption}
+      ref={selectRef}
     />
   );
 }
