eff749a4098ffeb1c161cde220bb137527e1843a
asharonbaltazar <58940073+asharonbaltazar@users.noreply.github.com>
2023-12-21 03:49:01 -0500

    feat: move `Alerts` to react-table (#635)
    Signed-off-by: talboren <talboren2@gmail.com>
    Co-authored-by: talboren <tal@keephq.dev>
    Co-authored-by: talboren <talboren2@gmail.com>

--- keep-ui/app/alerts/alert-table.tsx
+++ keep-ui/app/alerts/alert-table.tsx
@@ -5,9 +5,10 @@ import {
   TableHeaderCell,
   Icon,
   Callout,
+  CategoryBar,
 } from "@tremor/react";
 import { AlertsTableBody } from "./alerts-table-body";
-import { AlertDto, AlertTableKeys } from "./models";
+import { AlertDto } from "./models";
 import {
   CircleStackIcon,
   QuestionMarkCircleIcon,
@@ -15,6 +16,35 @@ import {
 import { Provider } from "app/providers/providers";
 import { User } from "app/settings/models";
 import { User as NextUser } from "next-auth";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import PushPullBadge from "@/components/ui/push-pulled-badge/push-pulled-badge";
+import moment from "moment";
+import Image from "next/image";
+import { Workflow } from "app/workflows/models";
+import { useRouter } from "next/navigation";
+import AlertName from "./alert-name";
+import AlertAssignee from "./alert-assignee";
+import AlertMenu from "./alert-menu";
+import AlertSeverity from "./alert-severity";
+import AlertExtraPayload from "./alert-extra-payload";
+
+const getAlertLastReceieved = (lastRecievedFromAlert: Date) => {
+  let lastReceived = "unknown";
+  if (lastRecievedFromAlert) {
+    lastReceived = lastRecievedFromAlert.toString();
+    try {
+      lastReceived = moment(lastRecievedFromAlert).fromNow();
+    } catch {}
+  }
+  return lastReceived;
+};
+
+const columnHelper = createColumnHelper<AlertDto>();
 
 interface Props {
   alerts: AlertDto[];
@@ -39,8 +69,8 @@ export function AlertTable({
   alerts,
   groupedByAlerts = {},
   groupBy,
-  workflows,
-  providers,
+  workflows = [],
+  providers = [],
   mutate,
   isAsyncLoading = false,
   onDelete,
@@ -49,6 +79,137 @@ export function AlertTable({
   currentUser,
   openModal,
 }: Props) {
+  const router = useRouter();
+
+  const handleWorkflowClick = (workflows: Workflow[]) => {
+    if (workflows.length === 1) {
+      router.push(`workflows/${workflows[0].id}`);
+    } else {
+      router.push("workflows");
+    }
+  };
+
+  const columns = [
+    columnHelper.display({
+      id: "alertMenu",
+      cell: (context) => (
+        <div className="pb-9">
+          <AlertMenu
+            alert={context.row.original}
+            canOpenHistory={!groupedByAlerts![(alert as any)[groupBy!]]}
+            openHistory={() => openModal!(context.row.original)}
+            provider={providers.find(
+              (p) => p.type === context.row.original.source![0]
+            )}
+            mutate={mutate}
+            callDelete={onDelete}
+            setAssignee={setAssignee}
+            currentUser={currentUser}
+          />
+        </div>
+      ),
+    }),
+    columnHelper.accessor("severity", {
+      header: () => "Severity",
+      cell: (context) => <AlertSeverity severity={context.getValue()} />,
+    }),
+    columnHelper.accessor("name", {
+      header: () => "Name",
+      cell: (context) => (
+        <AlertName
+          alert={context.row.original}
+          workflows={workflows}
+          handleWorkflowClick={handleWorkflowClick}
+        />
+      ),
+    }),
+    columnHelper.accessor("description", {
+      header: () => "Description",
+      cell: (context) => (
+        <div
+          className="max-w-[340px] flex items-center"
+          title={context.getValue()}
+        >
+          <div className="truncate">{context.getValue()}</div>
+        </div>
+      ),
+    }),
+    columnHelper.accessor("pushed", {
+      header: () => (
+        <div className="flex items-center gap-1">
+          <span>Pushed</span>
+          <Icon
+            icon={QuestionMarkCircleIcon}
+            tooltip="Whether the alert was pushed or pulled from the alert source"
+            variant="simple"
+            color="gray"
+          />
+        </div>
+      ),
+      cell: (context) => <PushPullBadge pushed={context.getValue()} />,
+    }),
+    columnHelper.accessor("status", {
+      header: "Status",
+    }),
+    columnHelper.accessor("lastReceived", {
+      header: "When",
+      cell: (context) => getAlertLastReceieved(context.getValue()),
+    }),
+    columnHelper.accessor("source", {
+      header: "Source",
+      cell: (context) =>
+        (context.getValue() ?? []).map((source, index) => (
+          <Image
+            className={`inline-block ${index == 0 ? "" : "-ml-2"}`}
+            key={source}
+            alt={source}
+            height={24}
+            width={24}
+            title={source}
+            src={`/icons/${source}-icon.png`}
+          />
+        )),
+    }),
+    columnHelper.accessor("assignee", {
+      header: "Assignee",
+      cell: (context) => (
+        <AlertAssignee assignee={context.getValue()} users={users} />
+      ),
+    }),
+    columnHelper.accessor("fatigueMeter", {
+      header: () => (
+        <div className="flex items-center gap-1">
+          <span>Fatigue Meter</span>
+          <Icon
+            icon={QuestionMarkCircleIcon}
+            tooltip="Calculated based on various factors"
+            variant="simple"
+            color="gray"
+          />
+        </div>
+      ),
+      cell: (context) => (
+        <CategoryBar
+          values={[40, 30, 20, 10]}
+          colors={["emerald", "yellow", "orange", "rose"]}
+          markerValue={context.getValue() ?? 0}
+          tooltip={(context.getValue() ?? 0).toString() ?? "0"}
+          className="min-w-[192px]"
+        />
+      ),
+    }),
+    columnHelper.display({
+      id: "extraPayload",
+      cell: (context) => <AlertExtraPayload alert={context.row.original} />,
+    }),
+  ];
+
+  const table = useReactTable({
+    data: alerts,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
     <>
       {isAsyncLoading && (
@@ -63,39 +224,20 @@ export function AlertTable({
       )}
       <Table>
         <TableHead>
-          <TableRow>
-            {<TableHeaderCell>{/** Menu */}</TableHeaderCell>}
-            {Object.keys(AlertTableKeys).map((key) => (
-              <TableHeaderCell key={key}>
-                <div className="flex items-center">
-                  {key}{" "}
-                  {AlertTableKeys[key] !== "" && (
-                    <Icon
-                      icon={QuestionMarkCircleIcon}
-                      tooltip={AlertTableKeys[key]}
-                      variant="simple"
-                      color="gray"
-                    />
-                  )}{" "}
-                </div>
-              </TableHeaderCell>
-            ))}
-          </TableRow>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHeaderCell key={header.id}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </TableHeaderCell>
+              ))}
+            </TableRow>
+          ))}
         </TableHead>
-        <AlertsTableBody
-          alerts={alerts}
-          groupBy={groupBy}
-          groupedByData={groupedByAlerts}
-          openModal={openModal}
-          workflows={workflows}
-          providers={providers}
-          mutate={mutate}
-          showSkeleton={isAsyncLoading}
-          onDelete={onDelete}
-          setAssignee={setAssignee}
-          users={users}
-          currentUser={currentUser}
-        />
+        <AlertsTableBody table={table} showSkeleton={isAsyncLoading} />
       </Table>
     </>
   );
