ae42f8dafbdbd9633f9544b9a8f2e2d933ae0bca
shahargl <shaharglazner@gmail.com>
2023-12-24 13:56:23 +0200

    feat: fix

--- keep-ui/utils/apiUrl.ts
+++ keep-ui/utils/apiUrl.ts
@@ -8,13 +8,32 @@ export function getApiURL(): string {
   // so we need to use the "http://localhost:3000" url instead.
   const componentType = typeof window === "undefined" ? "server" : "client";
   let apiUrl = "";
-  if (componentType === "server") {
-    apiUrl = process.env.API_URL!;
+  // we need to check if we are on vercel or not
+  const gitBranchName = process.env.VERCEL_GIT_COMMIT_REF || "notvercel";
+  // main branch or not vercel
+  if(gitBranchName === "main" || gitBranchName === "notvercel"){
+    // server
+    if (componentType === "server") {
+      apiUrl = process.env.API_URL!;
+    }
+    // on the frontend, we want to use the same url as the browser but with the "/backend" prefix so that middleware.ts can proxy the request to the backend
+    else {
+      apiUrl = "/backend";
+    }
   }
-  // on the frontend, we want to use the same url as the browser but with the "/backend" prefix so that middleware.ts can proxy the request to the backend
-  else {
-    apiUrl = "/backend";
-  }
+  // else, preview branch on vercel [ONLY SERVER!]
+  else{
+    console.log("preview branch on vercel");
+    let branchNameSanitized = gitBranchName.replace(/\//g, '-').substring(0, 63);
+    let serviceName = `keep-api-${branchNameSanitized}`;
+    if (serviceName.length > 63) {
+      serviceName = serviceName.substring(0, 49);
+    }
 
+    if (serviceName.endsWith('-')) {
+      serviceName = serviceName.slice(0, -1);
+    }
+    apiUrl = process.env.API_URL!.replace('keep-api', serviceName);
+  }
   return apiUrl;
 }
