12ed9524affaac2bed2d9c2201983583507123b2
Shahar Glazner <shaharglazner@gmail.com>
2024-01-04 14:23:49 +0200

    Update keep-ui/app/navbar-inner.tsx
    Co-authored-by: talboren <tal@keephq.dev>
    Signed-off-by: Shahar Glazner <shaharglazner@gmail.com>

--- keep-ui/app/navbar-inner.tsx
+++ keep-ui/app/navbar-inner.tsx
@@ -217,10 +217,9 @@ export default function NavbarInner({ session }: { session: any }) {
                       leaveTo="transform opacity-0 scale-95"
                     >
                       <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          <Menu.Item>
+                          {!isNocRole && <Menu.Item>
                             {({ active }) => (
                               <>
-                              {!isNocRole &&
                                 <a
                                   className={classNames(
                                     "flex w-full px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
