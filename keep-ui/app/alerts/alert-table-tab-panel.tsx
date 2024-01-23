fd9bfb952a4c3104609bcd40851fc6532d7e1985
asharonbaltazar <asharonbaltazar@outlook.com>
2024-01-22 21:25:01 -0500

    refactor:move table columns to `alert-table`

--- keep-ui/app/alerts/alert-table-tab-panel.tsx
+++ keep-ui/app/alerts/alert-table-tab-panel.tsx
@@ -1,10 +1,17 @@
 import { useMemo, useState } from "react";
-import { RowSelectionState } from "@tanstack/react-table";
+import {
+  ColumnDef,
+  PaginationState,
+  RowSelectionState,
+} from "@tanstack/react-table";
 import AlertPresets, { Option } from "./alert-presets";
-import { AlertTable } from "./alert-table";
+import { AlertTable, columnHelper, getAlertTableColumns } from "./alert-table";
 import { AlertDto, Preset } from "./models";
 import AlertActions from "./alert-actions";
 import { Tab } from "@headlessui/react";
+import AlertTableCheckbox from "./alert-table-checkbox";
+import AlertMenu from "./alert-menu";
+import { useRouter } from "next/navigation";
 
 const getPresetAlerts = (alert: AlertDto, preset: Preset): boolean => {
   if (preset.options.length === 0) {
@@ -48,9 +55,16 @@ export default function AlertTableTabPanel({
   preset,
   isAsyncLoading,
 }: Props) {
+  const router = useRouter();
+
   const [selectedOptions, setSelectedOptions] = useState<Option[]>(
     preset.options
   );
+
+  const [rowPagination, setRowPagination] = useState<PaginationState>({
+    pageSize: 10,
+    pageIndex: 0,
+  });
 
   const [rowSelection, setRowSelection] = useState<RowSelectionState>({});
   const selectedRowIds = Object.entries(rowSelection).reduce<string[]>(
@@ -67,6 +81,52 @@ export default function AlertTableTabPanel({
     .filter((alert) => getPresetAlerts(alert, preset))
     .sort((a, b) => b.lastReceived.getTime() - a.lastReceived.getTime());
 
+  const alertTableColumns: ColumnDef<AlertDto>[] = useMemo(
+    () => [
+      ...(preset.name !== "Deleted"
+        ? [
+            columnHelper.display({
+              id: "checkbox",
+              header: (context) => (
+                <AlertTableCheckbox
+                  checked={context.table.getIsAllRowsSelected()}
+                  indeterminate={context.table.getIsSomeRowsSelected()}
+                  onChange={context.table.getToggleAllRowsSelectedHandler()}
+                  disabled={alerts.length === 0}
+                />
+              ),
+              cell: (context) => (
+                <AlertTableCheckbox
+                  checked={context.row.getIsSelected()}
+                  indeterminate={context.row.getIsSomeSelected()}
+                  onChange={context.row.getToggleSelectedHandler()}
+                />
+              ),
+            }),
+          ]
+        : ([] as ColumnDef<AlertDto>[])),
+      ...(getAlertTableColumns() as ColumnDef<AlertDto>[]),
+      columnHelper.display({
+        id: "alertMenu",
+        meta: {
+          thClassName: "sticky right-0",
+          tdClassName: "sticky right-0",
+        },
+        cell: (context) => (
+          <AlertMenu
+            alert={context.row.original}
+            openHistory={() =>
+              router.replace(`/alerts?id=${context.row.original.id}`, {
+                scroll: false,
+              })
+            }
+          />
+        ),
+      }) as ColumnDef<AlertDto>,
+    ],
+    [alerts.length, router, preset.name]
+  );
+
   return (
     <Tab.Panel className="mt-4">
       {selectedRowIds.length ? (
@@ -82,12 +142,13 @@ export default function AlertTableTabPanel({
       )}
       <AlertTable
         alerts={presetAlerts}
+        columns={alertTableColumns}
         isAsyncLoading={isAsyncLoading}
-        rowSelection={
-          preset.name !== "Deleted"
-            ? { state: rowSelection, onChange: setRowSelection }
-            : undefined
-        }
+        rowSelection={{ state: rowSelection, onChange: setRowSelection }}
+        rowPagination={{
+          state: rowPagination,
+          onChange: setRowPagination,
+        }}
         presetName={preset.name}
       />
     </Tab.Panel>
