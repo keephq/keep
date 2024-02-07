1f3b6c66cd1f875a37e0b937f3c0aebe1047952b
asharonbaltazar <58940073+asharonbaltazar@users.noreply.github.com>
2024-02-07 02:50:05 -0500

    fix(ui): Column Width & History Modal (#802)
    Co-authored-by: Tal Borenstein <tal@keephq.dev>

--- keep-ui/app/alerts/alert-name.tsx
+++ keep-ui/app/alerts/alert-name.tsx
@@ -4,7 +4,7 @@ import {
   Cog8ToothIcon,
   TicketIcon,
   TrashIcon,
-  PencilSquareIcon
+  PencilSquareIcon,
 } from "@heroicons/react/24/outline";
 import { Icon } from "@tremor/react";
 import { AlertDto, AlertKnownKeys } from "./models";
@@ -44,13 +44,16 @@ interface Props {
   setNoteModalAlert?: (alert: AlertDto) => void;
   setTicketModalAlert?: (alert: AlertDto) => void;
 }
-export default function AlertName({ alert, setNoteModalAlert, setTicketModalAlert}: Props) {
+export default function AlertName({
+  alert,
+  setNoteModalAlert,
+  setTicketModalAlert,
+}: Props) {
   const router = useRouter();
   const { data: workflows = [] } = useWorkflows();
 
   const handleNoteClick = () => {
-    if(setNoteModalAlert)
-    {
+    if (setNoteModalAlert) {
       setNoteModalAlert(alert);
     }
   };
@@ -59,7 +62,7 @@ export default function AlertName({ alert, setNoteModalAlert, setTicketModalAler
     if (!ticketUrl && setTicketModalAlert) {
       setTicketModalAlert(alert);
     } else {
-      window.open(ticketUrl, '_blank'); // Open the ticket URL in a new tab
+      window.open(ticketUrl, "_blank"); // Open the ticket URL in a new tab
     }
   };
 
@@ -68,7 +71,6 @@ export default function AlertName({ alert, setNoteModalAlert, setTicketModalAler
     url,
     generatorURL,
     deleted,
-    lastReceived,
     note,
     ticket_url: ticketUrl,
     ticket_status: ticketStatus,
@@ -88,118 +90,118 @@ export default function AlertName({ alert, setNoteModalAlert, setTicketModalAler
     [alert, workflows]
   );
 
-
-
-
   return (
-      <div className="max-w-[340px]">
-        <div className="flex items-center justify-between">
-          <div className="truncate" title={alert.name}>
-            {name}{" "}
-          </div>
-          <div>
-            {(url ?? generatorURL) && (
-              <a href={url || generatorURL} target="_blank">
-                <Icon
-                  icon={ArrowTopRightOnSquareIcon}
-                  tooltip="Open Original Alert"
-                  color="gray"
-                  variant="solid"
-                  size="xs"
-                  className="ml-1"
-                />
-              </a>
-            )}
-            { setTicketModalAlert && (
-                <Icon
-                icon={TicketIcon}
-                tooltip={ticketUrl ? `Ticket Assigned ${ticketStatus ? `(status: ${ticketStatus})` : ''}` : "Click to assign Ticket"}
-                size="xs"
-                color={ticketUrl ? "green" : "gray"}
-                className="ml-1 cursor-pointer"
-                variant="solid"
-                onClick={handleTicketClick}
-              />
-            )}
-
-            {playbook_url && (
-              <a href={playbook_url} target="_blank">
-                <Icon
-                  icon={BookOpenIcon}
-                  tooltip="Playbook"
-                  size="xs"
-                  color="gray"
-                  className="ml-1"
-                  variant="solid"
-                />
-              </a>
-            )}
-            {
-              setNoteModalAlert && (
-                <Icon
-                icon={PencilSquareIcon}
-                tooltip="Click to add note"
-                size="xs"
-                color={note ? "green" : "gray"}
-                className="ml-1 cursor-pointer"
-                variant="solid"
-                onClick={handleNoteClick}
-              />
-            )}
-
-            {deleted && (
-              <Icon
-                icon={TrashIcon}
-                tooltip="This alert has been deleted"
-                size="xs"
-                color="gray"
-                className="ml-1"
-                variant="solid"
-              />
-            )}
-            {relevantWorkflows.length > 0 && (
-              <Icon
-                icon={Cog8ToothIcon}
-                size="xs"
-                color={`${
-                  relevantWorkflows.every(
-                    (wf) => wf.last_execution_status === "success"
-                  )
-                    ? "green"
-                    : relevantWorkflows.some(
-                        (wf) => wf.last_execution_status === "error"
-                      )
-                    ? "red"
-                    : relevantWorkflows.some(
-                        (wf) =>
-                          wf.last_execution_status === "providers_not_configured"
-                      )
-                    ? "amber"
-                    : "gray"
-                }`}
-                tooltip={`${
-                  relevantWorkflows.every(
-                    (wf) => wf.last_execution_status === "success"
-                  )
-                    ? "All workflows executed successfully"
-                    : relevantWorkflows.some(
-                        (wf) => wf.last_execution_status === "error"
-                      )
-                    ? "Some workflows failed to execute"
-                    : relevantWorkflows.some(
-                        (wf) =>
-                          wf.last_execution_status === "providers_not_configured"
-                      )
-                    ? "Some workflows are not configured"
-                    : "Workflows have yet to execute"
-                }`}
-                onClick={() => handleWorkflowClick(relevantWorkflows)}
-                className="ml-1 cursor-pointer"
-                variant="solid"
-              />
-            )}
-          </div>
-        </div>
+    <div className="flex items-center justify-between">
+      <div className="truncate" title={alert.name}>
+        {name}
       </div>
+      <div>
+        {(url ?? generatorURL) && (
+          <a href={url || generatorURL} target="_blank">
+            <Icon
+              icon={ArrowTopRightOnSquareIcon}
+              tooltip="Open Original Alert"
+              color="gray"
+              variant="solid"
+              size="xs"
+              className="ml-1"
+            />
+          </a>
+        )}
+        {setTicketModalAlert && (
+          <Icon
+            icon={TicketIcon}
+            tooltip={
+              ticketUrl
+                ? `Ticket Assigned ${
+                    ticketStatus ? `(status: ${ticketStatus})` : ""
+                  }`
+                : "Click to assign Ticket"
+            }
+            size="xs"
+            color={ticketUrl ? "green" : "gray"}
+            className="ml-1 cursor-pointer"
+            variant="solid"
+            onClick={handleTicketClick}
+          />
+        )}
+
+        {playbook_url && (
+          <a href={playbook_url} target="_blank">
+            <Icon
+              icon={BookOpenIcon}
+              tooltip="Playbook"
+              size="xs"
+              color="gray"
+              className="ml-1"
+              variant="solid"
+            />
+          </a>
+        )}
+        {setNoteModalAlert && (
+          <Icon
+            icon={PencilSquareIcon}
+            tooltip="Click to add note"
+            size="xs"
+            color={note ? "green" : "gray"}
+            className="ml-1 cursor-pointer"
+            variant="solid"
+            onClick={handleNoteClick}
+          />
+        )}
+
+        {deleted && (
+          <Icon
+            icon={TrashIcon}
+            tooltip="This alert has been deleted"
+            size="xs"
+            color="gray"
+            className="ml-1"
+            variant="solid"
+          />
+        )}
+        {relevantWorkflows.length > 0 && (
+          <Icon
+            icon={Cog8ToothIcon}
+            size="xs"
+            color={`${
+              relevantWorkflows.every(
+                (wf) => wf.last_execution_status === "success"
+              )
+                ? "green"
+                : relevantWorkflows.some(
+                    (wf) => wf.last_execution_status === "error"
+                  )
+                ? "red"
+                : relevantWorkflows.some(
+                    (wf) =>
+                      wf.last_execution_status === "providers_not_configured"
+                  )
+                ? "amber"
+                : "gray"
+            }`}
+            tooltip={`${
+              relevantWorkflows.every(
+                (wf) => wf.last_execution_status === "success"
+              )
+                ? "All workflows executed successfully"
+                : relevantWorkflows.some(
+                    (wf) => wf.last_execution_status === "error"
+                  )
+                ? "Some workflows failed to execute"
+                : relevantWorkflows.some(
+                    (wf) =>
+                      wf.last_execution_status === "providers_not_configured"
+                  )
+                ? "Some workflows are not configured"
+                : "Workflows have yet to execute"
+            }`}
+            onClick={() => handleWorkflowClick(relevantWorkflows)}
+            className="ml-1 cursor-pointer"
+            variant="solid"
+          />
+        )}
+      </div>
+    </div>
   );
 }
