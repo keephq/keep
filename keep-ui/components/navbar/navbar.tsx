17ad07cde9b12a95ce9a05dc252291fc1e612891
asharonbaltazar <asharonbaltazar@outlook.com>
2024-02-13 16:04:36 -0500

    feat:create `Search` component

--- keep-ui/components/navbar/navbar.tsx
+++ keep-ui/components/navbar/navbar.tsx
@@ -1,14 +1,65 @@
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { getSession, signOut, useSession } from "next-auth/react";
+import { Fragment, useState } from "react";
+import {
+  Bars3Icon,
+  DocumentTextIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import { VscDebugDisconnect } from "react-icons/vsc";
+import { LuWorkflow } from "react-icons/lu";
+import { AiOutlineAlert } from "react-icons/ai";
+import { MdOutlineEngineering } from "react-icons/md";
+
+import Link from "next/link";
+import { Icon } from "@tremor/react";
+import { AuthenticationType } from "utils/authenticationType";
+import useSWR from "swr";
+import { fetcher } from "utils/fetcher";
+import { User } from "next-auth";
+import { InternalConfig } from "types/internal-config";
+import { NameInitialsAvatar } from "react-name-initials-avatar";
+import DarkModeToggle from "../../app/dark-mode-toggle";
+import { useConfig } from "utils/hooks/useConfig";
+import { Search } from "./Search";
+import { ConfigureLinks } from "./ConfigureLinks";
+import { MonitorLinks } from "./MonitorLinks";
+import { AnalyzeLinks } from "./AnalyzeLinks";
+import { LearnLinks } from "./LearnLinks";
+import { UserInfo } from "./UserInfo";
 import InitPostHog from "./init-posthog";
-import NavbarInner from "./navbar-inner";
 
-import { CMDK } from "../../app/command-menu";
+const navigation = [
+  { name: "Providers", href: "/providers", icon: VscDebugDisconnect },
+  { name: "Alerts", href: "/alerts", icon: AiOutlineAlert },
+  { name: "Alert Groups", href: "/rules", icon: MdOutlineEngineering },
+  { name: "Workflows", href: "/workflows", icon: LuWorkflow },
+];
 
-export default function Navbar() {
+// noc navigation incldues only alerts
+const nocNavigation = [
+  { name: "Alerts", href: "/alerts", icon: AiOutlineAlert },
+];
+
+export default async function NavbarInner() {
   return (
     <>
       <InitPostHog />
-      <NavbarInner />
-      <CMDK />
+      <aside className="bg-gray-50 col-span-1 border-r border-gray-300 h-full">
+        <nav className="flex flex-col">
+          <div className="flex-1">
+            <Search />
+            <ConfigureLinks />
+            <MonitorLinks />
+            <AnalyzeLinks />
+            <LearnLinks />
+          </div>
+
+          <UserInfo />
+        </nav>
+      </aside>
     </>
   );
 }
--- keep-ui/components/navbar/search.tsx
+++
@@ -1,3 +0,0 @@
-export const Search = () => {
-  return null;
-};
