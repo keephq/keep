a746b68f5ba7e8873de09f3fd4b2ea2e9fef52f9
asharonbaltazar <asharonbaltazar@outlook.com>
2024-02-14 10:01:07 -0500

    feat:add links to `LearnLinks`

--- keep-ui/components/navbar/LearnLinks.tsx
+++ keep-ui/components/navbar/LearnLinks.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import { Subtitle } from "@tremor/react";
+import { LinkWithIcon } from "components/LinkWithIcon";
+import { AiOutlineBook } from "react-icons/ai";
+
 export const LearnLinks = () => {
-  return null;
+  return (
+    <div className="space-y-2">
+      <Subtitle className="text-xs pl-3">LEARN</Subtitle>
+      <ul className="space-y-2">
+        <li>
+          <LinkWithIcon
+            href="https://docs.keephq.dev"
+            icon={AiOutlineBook}
+            target="_blank"
+          >
+            Documentation
+          </LinkWithIcon>
+        </li>
+      </ul>
+    </div>
+  );
 };
