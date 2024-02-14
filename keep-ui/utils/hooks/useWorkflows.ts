c67539746da572d8b403fbac447133c51239a678
Tal Borenstein <tal@keephq.dev>
2024-02-14 15:34:16 +0200

    feat: run workflow from alert

--- keep-ui/utils/hooks/useWorkflows.ts
+++ keep-ui/utils/hooks/useWorkflows.ts
@@ -1,16 +1,21 @@
 import { Workflow } from "app/workflows/models";
 import { useSession } from "next-auth/react";
-import useSWR from "swr";
+import useSWR, { SWRConfiguration } from "swr";
 import { getApiURL } from "../apiUrl";
 import { fetcher } from "../fetcher";
 
-export const useWorkflows = () => {
+export const useWorkflows = (
+  options: SWRConfiguration = {
+    revalidateOnFocus: false,
+    revalidateOnMount: false,
+  }
+) => {
   const { data: session } = useSession();
   const apiUrl = getApiURL();
 
   return useSWR<Workflow[]>(
     () => (session ? `${apiUrl}/workflows` : null),
     (url) => fetcher(url, session?.accessToken),
-    { revalidateOnFocus: false, revalidateOnMount: false }
+    options
   );
 };
