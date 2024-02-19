95082852f999c5065a8246abda6ef51b66f9c2ec
asharonbaltazar <asharonbaltazar@outlook.com>
2024-02-19 10:51:30 -0500

    fix:fix `useConfig` error

--- keep-ui/utils/hooks/useConfig.ts
+++ keep-ui/utils/hooks/useConfig.ts
@@ -1,7 +1,12 @@
+import { useSession } from "next-auth/react";
 import useSWRImmutable from "swr/immutable";
 import { InternalConfig } from "types/internal-config";
 import { fetcher } from "utils/fetcher";
 
 export const useConfig = () => {
-  return useSWRImmutable<InternalConfig>("/api/config", fetcher);
+  const { data: session } = useSession();
+
+  return useSWRImmutable<InternalConfig>("/api/config", () =>
+    fetcher("/api/config", session?.accessToken)
+  );
 };
