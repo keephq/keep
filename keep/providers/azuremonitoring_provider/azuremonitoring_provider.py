4ea5973a70d231ca82c1418ce8d97850b3ff27cb
shahargl <shaharglazner@gmail.com>
2024-03-04 12:20:45 +0200

    feat: typo

--- keep/providers/azuremonitoring_provider/azuremonitoring_provider.py
+++ keep/providers/azuremonitoring_provider/azuremonitoring_provider.py
@@ -16,7 +16,7 @@ class AzuremonitoringProvider(BaseProvider):
     webhook_description = ""
     webhook_template = ""
     webhook_markdown = """
-💡 For more details on how to configure Azure Monitor to send alerts to Keep, see the [Keep documentation](https://docs.keephq.dev/docs/integrations/azure-monitor). 💡
+💡 For more details on how to configure Azure Monitor to send alerts to Keep, see the [Keep documentation](https://docs.keephq.dev/providers/documentation/azuremonitoring-provider). 💡
 
 To send alerts from Azure Monitor to Keep, Use the following webhook url to configure Azure Monitor send alerts to Keep:
 
