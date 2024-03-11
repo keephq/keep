440f552c8f0a9aa097788ea31d64debec7487aed
Furkan Pehlivan <65170388+pehlicd@users.noreply.github.com>
2024-03-11 14:42:51 +0100

    chore(providers): add new field for squadcast provider (#942)

--- keep/providers/squadcast_provider/squadcast_provider.py
+++ keep/providers/squadcast_provider/squadcast_provider.py
@@ -67,9 +67,6 @@ class SquadcastProvider(BaseProvider):
         """
         Validates that the user has the required scopes to use the provider.
         """
-        return {
-            "authenticated": True,
-        }
         refresh_headers = {
             "content-type": "application/json",
             "X-Refresh-Token": f"{self.authentication_config.refresh_token}",
@@ -120,6 +117,7 @@ class SquadcastProvider(BaseProvider):
         priority: str = "",
         status: str = "",
         event_id: str = "",
+        additional_json: str = "",
     ):
         body = json.dumps(
             {
@@ -131,6 +129,9 @@ class SquadcastProvider(BaseProvider):
             }
         )
 
+        # append body to additional_json we are doing this way because we don't want to override the core body fields
+        body = json.dumps({**json.loads(additional_json), **json.loads(body)})
+        
         return requests.post(
             self.authentication_config.webhook_url, data=body, headers=headers
         )
@@ -155,11 +156,13 @@ class SquadcastProvider(BaseProvider):
         status: str = "",
         event_id: str = "",
         attachments: list = [],
+        additional_json: str = "",
         **kwargs,
     ) -> dict:
         """
         Create an incident or notes using the Squadcast API.
         """
+
         self.logger.info(
             f"Creating {notify_type} using SquadcastProvider",
             extra={notify_type: notify_type},
@@ -187,6 +190,7 @@ class SquadcastProvider(BaseProvider):
                 priority=priority,
                 status=status,
                 event_id=event_id,
+                additional_json=additional_json,
             )
         elif notify_type == "notes":
             if message == "" or incident_id == "":
