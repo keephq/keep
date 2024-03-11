e60b96ffe7b808c25f5802d9b7a02aa5769fce25
pehlicd <65170388+pehlicd@users.noreply.github.com>
2024-03-11 13:31:23 +0100

    chore(providers): update eval function and remove replace

--- keep/providers/squadcast_provider/squadcast_provider.py
+++ keep/providers/squadcast_provider/squadcast_provider.py
@@ -130,8 +130,11 @@ class SquadcastProvider(BaseProvider):
         )
 
         if additional_json != "":
-            additional_json = additional_json.replace("'", "\"")
-            j = eval(additional_json)
+            from asteval import Interpreter
+            aeval = Interpreter()
+
+            additional_json = additional_json.replace("'", "\"")            
+            j = aeval(additional_json)
             # assign body to the json object to prevent core field from being overwritten
             j["message"] = message
             j["description"] = description
