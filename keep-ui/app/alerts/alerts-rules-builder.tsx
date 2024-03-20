78a217da02e419e795a8e2e8a3821a2cb95da99e
asharonbaltazar <asharonbaltazar@outlook.com>
2024-03-20 10:41:50 -0400

    fix:some operator and expression fixes

--- keep-ui/app/alerts/alerts-rules-builder.tsx
+++ keep-ui/app/alerts/alerts-rules-builder.tsx
@@ -3,7 +3,9 @@ import Modal from "components/ui/Modal";
 import { Button, Textarea } from "@tremor/react";
 import QueryBuilder, {
   Field,
+  Operator,
   RuleGroupType,
+  defaultOperators,
   formatQuery,
   parseCEL,
 } from "react-querybuilder";
@@ -16,18 +18,24 @@ const getAllMatches = (pattern: RegExp, string: string) =>
   // make sure string is a String, and make sure pattern has the /g flag
   String(string).match(new RegExp(pattern, "g"));
 
+const sanitizeCELIntoJS = (celExpression: string) => {
+  // "contains" is not a valid JS function
+  return celExpression.replace("contains", "includes");
+};
+
 // this pattern is far from robust
 const variablePattern = /[a-zA-Z$_][0-9a-zA-Z$_]*/;
 
-export const evalWithContext = (context: AlertDto, expression: string) => {
+export const evalWithContext = (context: AlertDto, celExpression: string) => {
   try {
-    if (expression.length === 0) {
+    if (celExpression.length === 0) {
       return new Function();
     }
 
-    const variables = getAllMatches(variablePattern, expression) ?? [];
+    const variables = getAllMatches(variablePattern, celExpression) ?? [];
+    const jsExpression = sanitizeCELIntoJS(celExpression);
 
-    const func = new Function(...variables, `return (${expression})`);
+    const func = new Function(...variables, `return (${jsExpression})`);
 
     const args = variables.map((arg) =>
       Object.hasOwnProperty.call(context, arg)
@@ -41,31 +49,38 @@ export const evalWithContext = (context: AlertDto, expression: string) => {
   }
 };
 
+const getOperators = (id: string): Operator[] => {
+  if (id === "source") {
+    return [
+      { name: "contains", label: "contains" },
+      { name: "null", label: "null" },
+    ];
+  }
+
+  return defaultOperators;
+};
+
 type AlertsRulesBuilderProps = {
   table: Table<AlertDto>;
 };
 
 export const AlertsRulesBuilder = ({ table }: AlertsRulesBuilderProps) => {
   const [isGUIOpen, setIsGUIOpen] = useState(false);
-  const [CELRules, setCELRules] = useState("");
+  const [celRules, setCELRules] = useState("");
   const [query, setQuery] = useState<RuleGroupType>({
     combinator: "and",
     rules: [],
   });
 
-  const parcedCELRulesToQuery = parseCEL(CELRules);
-  const isValidCEL = formatQuery(parcedCELRulesToQuery, "cel") === CELRules;
+  const parcedCELRulesToQuery = parseCEL(celRules);
+  const isValidCEL = formatQuery(parcedCELRulesToQuery, "cel") === celRules;
 
   const onApplyFilter = () => {
-    if (CELRules.length === 0) {
-      return table.setGlobalFilter("");
+    if (celRules.length === 0) {
+      return table.resetGlobalFilter();
     }
 
-    if (!isValidCEL) {
-      alert("Your CEL string is invalid");
-    }
-
-    return table.setGlobalFilter(CELRules);
+    return table.setGlobalFilter(celRules);
   };
 
   const onGenerateQuery = () => {
@@ -74,9 +89,9 @@ export const AlertsRulesBuilder = ({ table }: AlertsRulesBuilderProps) => {
   };
 
   const onGUIView = () => {
-    if (CELRules.length && !isValidCEL) {
+    if (celRules.length && !isValidCEL) {
       const isConfirmed = confirm(
-        "Your CEL string is invalid. Some conditions might not be displayed. Are you sure you want to generate the query rules?"
+        "Your CEL string is invalid. Some rules might be lost. Are you sure you want to generate the query rules?"
       );
 
       if (!isConfirmed) {
@@ -94,6 +109,7 @@ export const AlertsRulesBuilder = ({ table }: AlertsRulesBuilderProps) => {
     .map(({ id, columnDef }) => ({
       name: id,
       label: columnDef.header as string,
+      operators: getOperators(id),
     }));
 
   return (
@@ -127,7 +143,7 @@ export const AlertsRulesBuilder = ({ table }: AlertsRulesBuilderProps) => {
       </Modal>
       <Textarea
         className="max-h-64 min-h-10"
-        value={CELRules}
+        value={celRules}
         onValueChange={(newValue) => setCELRules(newValue)}
       />
       <div className="flex justify-end gap-x-2">
