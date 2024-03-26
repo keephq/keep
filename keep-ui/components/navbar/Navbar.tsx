b36157c206e559b8c89c84004d4997adc180780e
asharonbaltazar <asharonbaltazar@outlook.com>
2024-03-26 11:57:49 -0400

    refactor: remove `LearnLinks` and styling touchups

--- keep-ui/components/navbar/Navbar.tsx
+++ keep-ui/components/navbar/Navbar.tsx
@@ -2,12 +2,11 @@ import { getServerSession } from "next-auth";
 import { Search } from "components/navbar/Search";
 import { ConfigureLinks } from "components/navbar/ConfigureLinks";
 import { AnalyseLinks } from "components/navbar/AnalyseLinks";
-import { LearnLinks } from "components/navbar/LearnLinks";
 import { UserInfo } from "components/navbar/UserInfo";
 import { InitPostHog } from "components/navbar/InitPostHog";
 import { Menu } from "components/navbar/Menu";
 import { MinimizeMenuButton } from "./MinimizeMenuButton";
-import {authOptions} from "pages/api/auth/[...nextauth]";
+import { authOptions } from "pages/api/auth/[...nextauth]";
 
 export default async function NavbarInner() {
   const session = await getServerSession(authOptions);
@@ -16,15 +15,11 @@ export default async function NavbarInner() {
     <>
       <InitPostHog />
       <Menu>
-        <div className="flex-1 h-full">
-          <Search />
-          <div className="pt-6 space-y-4">
-            <AnalyseLinks />
-            <ConfigureLinks session={session} />
-            <LearnLinks />
-          </div>
+        <Search />
+        <div className="pt-6 space-y-4 flex-1 overflow-auto">
+          <AnalyseLinks session={session} />
+          <ConfigureLinks session={session} />
         </div>
-
         <UserInfo session={session} />
       </Menu>
       <MinimizeMenuButton />
