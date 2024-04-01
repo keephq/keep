4a959e9bb0f4086d4414b8299842a596b95e381a
asharonbaltazar <asharonbaltazar@outlook.com>
2024-04-01 15:55:02 -0400

    feat:add transition to slideout

--- keep-ui/app/rules/CreateCorrelationSidebar/index.tsx
+++ keep-ui/app/rules/CreateCorrelationSidebar/index.tsx
@@ -1,6 +1,7 @@
-import { Dialog } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import { CreateCorrelationSidebarHeader } from "./CreateCorrelationSidebarHeader";
 import { CreateCorrelationSidebearBody } from "./CreateCorrelationSidebarBody";
+import { Fragment } from "react";
 
 type CreateCorrelationSidebarProps = {
   isOpen: boolean;
@@ -12,12 +13,34 @@ export const CreateCorrelationSidebar = ({
   toggle,
 }: CreateCorrelationSidebarProps) => {
   return (
-    <Dialog open={isOpen} onClose={toggle}>
-      <div className="fixed inset-0 bg-black/30 z-20" aria-hidden="true" />
-      <Dialog.Panel className="fixed right-0 inset-y-0 w-3/4 bg-white z-30 p-6">
-        <CreateCorrelationSidebarHeader toggle={toggle} />
-        <CreateCorrelationSidebearBody />
-      </Dialog.Panel>
-    </Dialog>
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog onClose={toggle}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/30 z-20" aria-hidden="true" />
+        </Transition.Child>
+        <Transition.Child
+          as={Fragment}
+          enter="transition ease-in-out duration-300 transform"
+          enterFrom="translate-x-full"
+          enterTo="translate-x-0"
+          leave="transition ease-in-out duration-300 transform"
+          leaveFrom="translate-x-0"
+          leaveTo="translate-x-full"
+        >
+          <Dialog.Panel className="fixed right-0 inset-y-0 w-3/4 bg-white z-30 p-6 overflow-auto">
+            <CreateCorrelationSidebarHeader toggle={toggle} />
+            <CreateCorrelationSidebearBody />
+          </Dialog.Panel>
+        </Transition.Child>
+      </Dialog>
+    </Transition>
   );
 };
