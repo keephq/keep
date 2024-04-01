6a3d832ae7a35713cea99bfd81f0816f26dade9d
asharonbaltazar <asharonbaltazar@outlook.com>
2024-04-01 12:38:14 -0400

    feat:create `useRules` hook

---
+++ keep-ui/utils/hooks/useRules.ts
@@ -0,0 +1,28 @@
+import { useSession } from "next-auth/react";
+import useSWR, { SWRConfiguration } from "swr";
+import { getApiURL } from "utils/apiUrl";
+import { fetcher } from "utils/fetcher";
+
+export type Rule = {
+  id: string;
+  name: string;
+  definition: string;
+  definition_cel: string;
+  timeframe: number;
+  created_by: string;
+  creation_time: string;
+  updated_by: string;
+  update_time: string;
+  distribution: { [group: string]: { [timestamp: string]: number } };
+};
+
+export const useRules = (options?: SWRConfiguration) => {
+  const apiUrl = getApiURL();
+  const { data: session } = useSession();
+
+  return useSWR<Rule[]>(
+    () => (session ? `${apiUrl}/workflows/executions` : null),
+    async (url) => fetcher(url, session?.accessToken),
+    options
+  );
+};
