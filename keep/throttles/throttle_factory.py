5c20b02628d7c3ca060c359f8120431663ae27e5
pehlicd <65170388+pehlicd@users.noreply.github.com>
2024-04-08 13:27:03 +0200

    fix: fix type error in throttle mechanism

--- keep/throttles/throttle_factory.py
+++ keep/throttles/throttle_factory.py
@@ -1,13 +1,14 @@
 import importlib
 
 from keep.throttles.base_throttle import BaseThrottle
+from keep.contextmanager.contextmanager import ContextManager
 
 
 class ThrottleFactory:
     @staticmethod
-    def get_instance(throttle_type, throttle_config) -> BaseThrottle:
+    def get_instance(context_manager, throttle_type, throttle_config) -> BaseThrottle:
         module = importlib.import_module(f"keep.throttles.{throttle_type}_throttle")
         throttle_class = getattr(
             module, throttle_type.title().replace("_", "") + "Throttle"
         )
-        return throttle_class(throttle_type, throttle_config)
+        return throttle_class(context_manager, throttle_type, throttle_config)
