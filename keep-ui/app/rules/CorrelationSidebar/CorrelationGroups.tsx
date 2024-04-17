657c1b50eff3af9126a61b38a89f1f921b6a1d44
asharonbaltazar <asharonbaltazar@outlook.com>
2024-04-16 22:35:39 -0400

    feat:add tooltip to groups

--- keep-ui/app/rules/CorrelationSidebar/CorrelationGroups.tsx
+++ keep-ui/app/rules/CorrelationSidebar/CorrelationGroups.tsx
@@ -2,15 +2,32 @@ import QueryBuilder from "react-querybuilder";
 import { RuleGroup } from "./RuleGroup";
 import { Controller, useFormContext } from "react-hook-form";
 import { CorrelationForm } from ".";
+import { Button } from "@tremor/react";
+import { QuestionMarkIcon } from "@radix-ui/react-icons";
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
 
 export const CorrelationGroups = () => {
   const { control } = useFormContext<CorrelationForm>();
 
   return (
     <div>
-      <p className="text-tremor-default font-medium text-tremor-content-strong mb-2">
-        Add group(s) of conditions
-      </p>
+      <div className="flex justify-between items-center">
+        <p className="text-tremor-default font-medium text-tremor-content-strong mb-2">
+          Add group(s) of conditions
+        </p>
+
+        <Button
+          className="cursor-default"
+          type="button"
+          tooltip="Any Rule consists of one or more Alert Groups. Each alert group is evaluated separately and the results are combined using AND combinator. 
+                  For example, if you want to group alerts that has a severity of 'critical' and another alert with a source of 'Kibana', you would create a rule with two alert groups. 
+                  The first alert group would have a rule with severity = 'critical' and the second alert group would have a rule with source = 'kibana'."
+          icon={QuestionMarkCircleIcon}
+          size="xs"
+          variant="light"
+          color="slate"
+        />
+      </div>
       <Controller
         control={control}
         name="query"
