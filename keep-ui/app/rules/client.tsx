9fed22b4e9ebece97840f0cfb16a15a7f79f87a9
asharonbaltazar <asharonbaltazar@outlook.com>
2024-04-16 22:29:05 -0400

    fix:return `<Loading />` before placeholder

--- keep-ui/app/rules/client.tsx
+++ keep-ui/app/rules/client.tsx
@@ -3,9 +3,14 @@
 import { useRules } from "utils/hooks/useRules";
 import { CorrelationPlaceholder } from "./CorrelationPlaceholder";
 import { CorrelationTable } from "./CorrelationTable";
+import Loading from "app/loading";
 
 export const Client = () => {
-  const { data: rules = [] } = useRules();
+  const { data: rules = [], isLoading } = useRules();
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   if (rules.length === 0) {
     return <CorrelationPlaceholder />;
