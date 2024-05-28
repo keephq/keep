66c4a7f4dcf6f12787e7cea4aa3ae3b47f57b6e7
Shahar Glazner <shaharglazner@gmail.com>
2024-05-28 12:31:18 +0300

    fix: telegram (#1197)

--- keep/providers/telegram_provider/telegram_provider.py
+++ keep/providers/telegram_provider/telegram_provider.py
@@ -1,6 +1,8 @@
 """
 TelegramProvider is a class that implements the BaseProvider interface for Telegram messages.
 """
+
+import asyncio
 import dataclasses
 
 import pydantic
@@ -44,12 +46,7 @@ class TelegramProvider(BaseProvider):
         """
         pass
 
-    async def _notify(
-            self,
-            chat_id: str = "",
-            message: str = "",
-            **kwargs: dict
-            ):
+    def _notify(self, chat_id: str = "", message: str = "", **kwargs: dict):
         """
         Notify alert message to Telegram using the Telegram Bot API
         https://core.telegram.org/bots/api
@@ -63,9 +60,13 @@ class TelegramProvider(BaseProvider):
             raise ProviderException(
                 f"{self.__class__.__name__} failed to notify alert message to Telegram: chat_id is required"
             )
+        loop = asyncio.new_event_loop()
         telegram_bot = telegram.Bot(token=self.authentication_config.bot_token)
         try:
-            await telegram_bot.send_message(chat_id=chat_id, text=message)
+            task = loop.create_task(
+                telegram_bot.send_message(chat_id=chat_id, text=message)
+            )
+            loop.run_until_complete(task)
         except Exception as e:
             raise ProviderException(
                 f"{self.__class__.__name__} failed to notify alert message to Telegram: {e}"
@@ -74,7 +75,7 @@ class TelegramProvider(BaseProvider):
         self.logger.debug("Alert message notified to Telegram")
 
 
-async def send_message():
+async def test_send_message():
     # Output debug messages
     import logging
 
@@ -107,4 +108,4 @@ if __name__ == "__main__":
     # Send the message
     import asyncio
 
-    asyncio.run(send_message())
+    asyncio.run(test_send_message())
--- tests/test_steps.py
+++ tests/test_steps.py
@@ -1,9 +1,9 @@
-import pytest
 import time
-import asyncio
-from unittest.mock import Mock, AsyncMock,  patch
-from keep.step.step import Step, StepError, ActionError, StepType
+from unittest.mock import Mock
 
+import pytest
+
+from keep.step.step import Step, StepError, StepType
 
 # constants for on-failure->retry mechanism
 RETRY_COUNT = 2
@@ -17,14 +17,9 @@ def sample_step():
     config = {
         "name": "Test Step",
         "provider": {
-            "on-failure": {
-                "retry": {
-                    "count": RETRY_COUNT,
-                    "interval": RETRY_INTERVAL
-                }
-            }
+            "on-failure": {"retry": {"count": RETRY_COUNT, "interval": RETRY_INTERVAL}}
         },
-        "throttle": False
+        "throttle": False,
     }
     step_type = StepType.STEP
     provider = Mock()
@@ -42,9 +37,7 @@ def sample_step():
 
     # Mock the context
     step.io_handler.render_context = Mock(
-        return_value={
-            "param1": "value1", "param2": "value2"
-        }
+        return_value={"param1": "value1", "param2": "value2"}
     )
 
     return step
@@ -59,23 +52,7 @@ def test_run_single(sample_step):
 
     # Assertions
     assert result is True  # Action should run successfully
-    sample_step.provider.query.assert_called_with(
-        param1="value1", param2="value2"
-    )
-    assert sample_step.provider.query.call_count == 1
-
-
-def test_run_single_async(sample_step):
-    # Simulate an asynchronous result
-    sample_step.provider.query = AsyncMock(return_value="result")
-
-    # Run the method
-    sample_step._run_single_async()
-
-    # Assertions
-    sample_step.provider.query.assert_called_with(
-        param1="value1", param2="value2"
-    )
+    sample_step.provider.query.assert_called_with(param1="value1", param2="value2")
     assert sample_step.provider.query.call_count == 1
 
 
@@ -93,35 +70,7 @@ def test_run_single_exception(sample_step):
     execution_time = end_time - start_time
 
     # Provider query should be called RETRY_COUNT+1 times
-    assert sample_step.provider.query.call_count == RETRY_COUNT+1
+    assert sample_step.provider.query.call_count == RETRY_COUNT + 1
 
     # _run_single should take around RETRY_COUNT*RETRT_INTERVAL time due to retries
-    assert execution_time >= RETRY_COUNT * RETRY_INTERVAL
-
-
-def test_run_single_async_exception(sample_step):
-    # Create a custom loop to capture calls to run_until_complete
-    custom_loop = asyncio.new_event_loop()
-    custom_loop.create_task = Mock()
-
-    # Simulate loop.run_until_complete, assuming the provider.query/notify
-    # executes with an exception
-    custom_loop.run_until_complete = Mock(
-        side_effect=Exception("Test exception")
-    )
-
-    start_time = time.time()
-
-    # Run the method and expect an exception to be raised
-    with pytest.raises(ActionError):
-        with patch('asyncio.new_event_loop', return_value=custom_loop):
-            sample_step._run_single_async()
-
-    end_time = time.time()
-    execution_time = end_time - start_time
-
-    # loop.run_until_complete should be called RETRY_COUNT+1 times
-    assert custom_loop.run_until_complete.call_count == RETRY_COUNT + 1
-
-    # _run_single_async should take around RETRY_COUNT*RETRT_INTERVAL time due to retries
     assert execution_time >= RETRY_COUNT * RETRY_INTERVAL
