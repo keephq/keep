b9b20c802db46e7e9aa22ece21088125c7d0af0e
shahargl <shaharglazner@gmail.com>
2024-05-29 09:36:58 +0300

    feat: wip

--- a.py
+++ a.py
@@ -1,9 +1,11 @@
 import json
 import os
 
+from dotenv import load_dotenv
 from elasticsearch import Elasticsearch, helpers
 
 # Initialize Elasticsearch client
+load_dotenv()
 api_key = os.environ.get("ELASTIC_API_KEY")
 hosts = os.environ.get("ELASTIC_HOSTS")
 client = Elasticsearch(hosts=hosts.split(","), api_key=api_key)
@@ -23,24 +25,30 @@ def process_and_upload_to_elasticsearch(file_path, batch_size=10000):
                 try:
                     # Parse the JSON part
                     json_data = json.loads(parts[0])
-
+                    tenant_id = parts[2]
                     # Create an action for the bulk API
-                    actions.append(json_data)
+                    action = {
+                        "_index": f"search-keeptest-{tenant_id}",
+                        "_source": json_data,
+                    }
+                    actions.append(action)
 
                     # Increment the line count
                     line_count += 1
 
                     # If batch_size is reached, upload to Elasticsearch
                     if line_count % batch_size == 0:
-                        helpers.bulk(client, actions, index="search-keeptest")
+                        helpers.bulk(client, actions)
                         print(f"Uploaded {line_count} lines to Elasticsearch")
                         actions = []  # Reset actions list
 
                 except json.JSONDecodeError as e:
                     print(f"Error parsing JSON: {e}")
+                    actions = []  # Reset actions list
                     continue
                 except Exception as e:
                     print(f"Error processing line: {e}")
+                    actions = []  # Reset actions list
                     continue
 
         # Upload any remaining actions to Elasticsearch
