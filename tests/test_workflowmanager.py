48f7d1bf30c218d51edd129aa23ee474d3eca19c
Omri Nuri <omrinuri@gmail.com>
2024-06-13 16:44:56 -0500

    feat: test get_workflow_from_dict

---
+++ tests/test_workflowmanager.py
@@ -0,0 +1,49 @@
+import pytest
+from unittest.mock import Mock, patch
+from fastapi import HTTPException
+
+# Assuming WorkflowParser is the class containing the get_workflow_from_dict method
+from keep.workflowmanager.workflow import Workflow
+from keep.workflowmanager.workflowstore import WorkflowStore
+from keep.parser.parser import Parser
+from pathlib import Path
+
+path_to_test_resources = Path(__file__).parent / "workflows"
+
+
+def test_get_workflow_from_dict():
+    mock_parser = Mock(spec=Parser)
+    mock_workflow = Mock(spec=Workflow, id="workflow1")
+    mock_parser.parse.return_value = [mock_workflow]
+    workflow_store = WorkflowStore()
+    workflow_store.parser = mock_parser
+
+    tenant_id = "test_tenant"
+    workflow_path = str(path_to_test_resources / "db_disk_space_for_testing.yml")
+    workflow_dict = workflow_store._parse_workflow_to_dict(workflow_path=workflow_path)
+    result = workflow_store.get_workflow_from_dict(
+        tenant_id=tenant_id, workflow=workflow_dict
+    )
+    mock_parser.parse.assert_called_once_with(tenant_id, workflow_dict)
+    assert result.id == "workflow1"
+
+
+def test_get_workflow_from_dict_raises_exception():
+    mock_parser = Mock(spec=Parser)
+    mock_parser.parse.return_value = []
+    workflow_store = WorkflowStore()
+    workflow_store.parser = mock_parser
+
+    tenant_id = "test_tenant"
+
+    workflow_path = str(path_to_test_resources / "db_disk_space_for_testing.yml")
+    workflow_dict = workflow_store._parse_workflow_to_dict(workflow_path=workflow_path)
+
+    with pytest.raises(HTTPException) as exc_info:
+        workflow_store.get_workflow_from_dict(
+            tenant_id=tenant_id, workflow=workflow_dict
+        )
+
+    assert exc_info.value.status_code == 500
+    assert exc_info.value.detail == "Unable to parse workflow from dict"
+    mock_parser.parse.assert_called_once_with(tenant_id, workflow_dict)
