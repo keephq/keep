c94cf067b1a6812347006326d4090624e5a914ac
35C4n0r <jaykumar20march@gmail.com>
2024-06-27 21:11:16 +0530

    patch: handle broad Exception
    Signed-off-by: 35C4n0r <jaykumar20march@gmail.com>

--- keep/providers/openobserve_provider/openobserve_provider.py
+++ keep/providers/openobserve_provider/openobserve_provider.py
@@ -153,7 +153,7 @@ class OpenobserveProvider(BaseProvider):
                 },
                 timeout=10,
             )
-        except Exceptions as e:
+        except Exception as e:
             self.logger.error(
                 "Error while validating scopes for OpenObserve",
                 extra=e,
