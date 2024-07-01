77cfbead4cb3a424db747695b1ed10a8cafa9123
Tal Borenstein <tal@keephq.dev>
2024-07-01 18:41:25 +0300

    fix: tests

--- keep/api/models/db/action.py
+++ keep/api/models/db/action.py
@@ -1,22 +1,25 @@
 from datetime import datetime
 from typing import Optional
+from uuid import uuid4
 
 from sqlalchemy import UniqueConstraint
-from sqlmodel import Column, Field, SQLModel, TEXT
+from sqlmodel import TEXT, Column, Field, SQLModel
 
 
 class Action(SQLModel, table=True):
     __table_args__ = (UniqueConstraint("tenant_id", "name", "use"),)
 
-    id: str = Field(default=None, primary_key=True)
-    tenant_id: str = Field(foreign_key="tenant.id")
+    id: str = Field(
+        default_factory=lambda: str(uuid4()), primary_key=True, max_length=36
+    )
+    tenant_id: str = Field(foreign_key="tenant.id", max_length=36)
     use: str
     name: str
     description: Optional[str]
     action_raw: str = Field(sa_column=Column(TEXT))
     installed_by: str
     installation_time: datetime
-   
+
     class Config:
         orm_mode = True
         unique_together = ["tenant_id", "name", "use"]
