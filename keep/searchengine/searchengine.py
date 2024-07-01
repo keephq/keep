42f590c61cd881f4fdcbb387f080b783ba5a5d89
Tal <tal@keephq.dev>
2024-07-01 13:04:14 +0300

    fix: small issue with grafana provider fingerprint (#1314)

--- keep/searchengine/searchengine.py
+++ keep/searchengine/searchengine.py
@@ -39,20 +39,6 @@ class SearchEngine:
         alerts = get_last_alerts(
             tenant_id=self.tenant_id, limit=limit, timeframe=timeframe
         )
-        # deduplicate fingerprints
-        # shahar: this is backward compatibility for before we had milliseconds in the timestamp
-        #          note that we want to keep the order of the alerts
-        #          so we will keep the first alert and remove the rest
-        dedup_alerts = []
-        seen_fingerprints = set()
-        for alert in alerts:
-            if alert.fingerprint not in seen_fingerprints:
-                dedup_alerts.append(alert)
-                seen_fingerprints.add(alert.fingerprint)
-            # this shouldn't appear with time (after migrating to milliseconds in timestamp)
-            else:
-                self.logger.info("Skipping fingerprint", extra={"alert_id": alert.id})
-        alerts = dedup_alerts
         # convert the alerts to DTO
         alerts_dto = convert_db_alerts_to_dto_alerts(alerts)
         self.logger.info("Finished getting last alerts")
