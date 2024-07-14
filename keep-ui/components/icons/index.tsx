703282006d52ab8c5241a1c39725d13f992a40f5
Matvey Kukuy <motakuk@gmail.com>
2024-07-14 18:45:06 +0300

    Icon

--- keep-ui/components/icons/index.tsx
+++ keep-ui/components/icons/index.tsx
@@ -167,6 +167,23 @@ const Mapping = () => (
   </svg>
 );
 
+const AIIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    className="tremor-Icon-icon shrink-0 h-5 w-5"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3"
+    />
+  </svg>
+);
+
 const DoorbellNotification = () => (
   <svg
     className="tremor-Icon-icon shrink-0 h-5 w-5"
@@ -254,5 +271,6 @@ export {
   DoorbellNotification,
   SilencedDoorbellNotification,
   Trashcan,
-  ExportIcon
+  ExportIcon,
+  AIIcon,
 };
