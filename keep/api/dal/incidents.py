606b068d3bc1e2ee6eb8fe47913964445f207d50
Vladimir Filonov <vladimir@filonov.dev>
2024-07-15 22:42:35 +0400

    Add methods and routes to add/remove alerts to the incident

--- keep/api/dal/incidents.py
+++ keep/api/dal/incidents.py
@@ -1,13 +1,14 @@
 import logging
 from datetime import timezone, timedelta, datetime
-from typing import Optional
+from typing import Optional, List
 
+from pydantic.types import UUID
 from sqlalchemy import desc
-from sqlmodel import Session, select
+from sqlmodel import Session, select, col
 
 from keep.api.core.db import engine
 from keep.api.models.alert import IncidentDtoIn
-from keep.api.models.db.alert import Incident
+from keep.api.models.db.alert import Incident, Alert, AlertToIncident
 
 logger = logging.getLogger(__name__)
 
@@ -117,3 +118,78 @@ def delete_incident_by_fingerprint(
         return True
 
 
+def get_incident_alerts_by_incident_fingerprint(tenant_id: str, fingerprint: str) -> List[Alert]:
+    with Session(engine) as session:
+        query = (
+            session.query(
+                Alert,
+            )
+            .join(AlertToIncident, AlertToIncident.alert_id == Alert.id)
+            .filter(AlertToIncident.tenant_id == tenant_id, Incident.incident_fingerprint == fingerprint)
+        )
+
+    return query.all()
+
+
+def add_alerts_to_incident_by_incident_fingerprint(
+    tenant_id: str,
+    fingerprint: str,
+    alert_ids: List[UUID]
+):
+    with Session(engine) as session:
+        incident = session.exec(
+            select(Incident).where(
+                Incident.tenant_id == tenant_id,
+                Incident.incident_fingerprint == fingerprint
+            )
+        ).first()
+
+        if not incident:
+            return None
+
+        existed_alert_ids = session.exec(
+            select(AlertToIncident.alert_id).where(
+                AlertToIncident.tenant_id == tenant_id,
+                AlertToIncident.incident_id == incident.id,
+                col(AlertToIncident.alert_id).in_(alert_ids)
+            )
+        ).all()
+
+        alert_to_incident_entries = [
+            AlertToIncident(alert_id=alert_id, incident_id=incident.id, tenant_id=tenant_id)
+            for alert_id in alert_ids if alert_id not in existed_alert_ids
+        ]
+
+        session.bulk_save_objects(alert_to_incident_entries)
+        session.commit()
+        return True
+
+
+def remove_alerts_to_incident_by_incident_fingerprint(
+    tenant_id: str,
+    fingerprint: str,
+    alert_ids: List[UUID]
+) -> Optional[int]:
+    with Session(engine) as session:
+        incident = session.exec(
+            select(Incident).where(
+                Incident.tenant_id == tenant_id,
+                Incident.incident_fingerprint == fingerprint
+            )
+        ).first()
+
+        if not incident:
+            return None
+
+        deleted = (
+            session.query(AlertToIncident)
+            .where(
+                AlertToIncident.tenant_id == tenant_id,
+                AlertToIncident.incident_id == incident.id,
+                col(AlertToIncident.alert_id).in_(alert_ids)
+            )
+            .delete()
+        )
+
+        session.commit()
+        return deleted
