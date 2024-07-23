4285f08f65bbbc9aeb11dcf7ff1caae6e4449784
Tal Borenstein <tal@keephq.dev>
2024-07-23 14:42:50 +0300

    fix(splunk): exception class from _raw

--- keep/providers/splunk_provider/splunk_provider.py
+++ keep/providers/splunk_provider/splunk_provider.py
@@ -178,9 +178,9 @@ class SplunkProvider(BaseProvider):
             "exception",
             result.get(
                 "exception",
-                result.get("exception_class", raw_dict.get("exception_class")),
+                result.get("exception_class"),
             ),
-        )
+        ) or raw_dict.get("exception_class", "")
 
         # override stacktrace with _raw stacktrace if it doesnt exist in result
         stacktrace = result.get("stacktrace", raw_dict.get("stacktrace", ""))
