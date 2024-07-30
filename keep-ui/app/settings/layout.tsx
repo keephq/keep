f9aa1214585d7bbe66907947525efe54e1b3add4
Kirill Chernakov <yakiryous@gmail.com>
2024-07-30 17:43:24 +0400

    fix: use the same content paddings for settings as for providers, etc

--- keep-ui/app/settings/layout.tsx
+++ keep-ui/app/settings/layout.tsx
@@ -3,10 +3,10 @@ import { Card, Title, Subtitle } from "@tremor/react";
 export default function Layout({ children }: { children: any }) {
   return (
     <>
-      <main className="p-4 md:p-10 mx-auto max-w-7xl">
+      <main className="p-4 mx-auto max-w-7xl">
         <Title>Settings</Title>
         <Subtitle>Setup and configure Keep</Subtitle>
-        <Card className="mt-10 p-4 md:p-10 mx-auto">{children}</Card>
+        <Card className="mt-10 p-4 mx-auto">{children}</Card>
       </main>
     </>
   );
