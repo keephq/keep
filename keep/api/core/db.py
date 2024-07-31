55219d816ce9345d1b2f59ddc225676ddb687c9b
Vladimir Filonov <vladimir@filonov.dev>
2024-07-31 15:28:49 +0400

    Remove unused imports

--- keep/api/core/db.py
+++ keep/api/core/db.py
@@ -16,10 +16,9 @@ from uuid import uuid4
 import validators
 from dotenv import find_dotenv, load_dotenv
 from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
-from sqlalchemy import and_, desc, func, null, update, cast
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy import and_, desc, null, update
 from sqlalchemy.exc import IntegrityError, OperationalError
-from sqlalchemy.orm import joinedload, selectinload, subqueryload, aliased
+from sqlalchemy.orm import joinedload, selectinload, subqueryload
 from sqlalchemy.orm.attributes import flag_modified
 from sqlalchemy.orm.exc import StaleDataError
 from sqlalchemy.sql import expression
