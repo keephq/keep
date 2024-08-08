bc8821ca26b1a9518ef876d5b691afa5ff9b520d
Matvey Kukuy <motakuk@gmail.com>
2024-08-08 13:57:59 +0300

    Unused imports

--- keep/api/models/db/migrations/versions/2024-08-08-13-55_42098785763c.py
+++ keep/api/models/db/migrations/versions/2024-08-08-13-55_42098785763c.py
@@ -6,11 +6,6 @@ Create Date: 2024-08-08 13:55:55.191243
 
 """
 
-import sqlalchemy as sa
-import sqlalchemy_utils
-import sqlmodel
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision = "42098785763c"
 down_revision = ("67f1efb93c99", "4147d9e706c0")
