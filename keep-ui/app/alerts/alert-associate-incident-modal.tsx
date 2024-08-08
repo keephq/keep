72d388dd9240b412a80adf00c4d3f21ade42fad3
Tal Borenstein <tal@keephq.dev>
2024-08-08 18:42:24 +0300

    fix(ui): build failing due to #1540 issue that got merged

--- keep-ui/app/alerts/alert-associate-incident-modal.tsx
+++ keep-ui/app/alerts/alert-associate-incident-modal.tsx
@@ -23,30 +23,26 @@ const AlertAssociateIncidentModal = ({
   handleClose,
   alerts,
 }: AlertAssociateIncidentModalProps) => {
-  const [createIncident, setCreateIncident] = useState(false)
+  const [createIncident, setCreateIncident] = useState(false);
 
   const { data: incidents, isLoading, mutate } = useIncidents(true, 100);
-  usePollIncidents(mutate)
+  usePollIncidents(mutate);
 
   const [selectedIncident, setSelectedIncident] = useState<string | null>(null);
   // get the token
   const { data: session } = useSession();
   const router = useRouter();
-  // if this modal should not be open, do nothing
-  if (!alerts) return null;
 
   const associateAlertsHandler = async (incidentId: string) => {
     const apiUrl = getApiURL();
-    const response = await fetch(
-      `${apiUrl}/incidents/${incidentId}/alerts`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${session?.accessToken}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(alerts.map(({ event_id }) => event_id)),
-      });
+    const response = await fetch(`${apiUrl}/incidents/${incidentId}/alerts`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${session?.accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(alerts.map(({ event_id }) => event_id)),
+    });
     if (response.ok) {
       handleSuccess();
       await mutate();
@@ -56,30 +52,39 @@ const AlertAssociateIncidentModal = ({
         "Failed to associated alerts with incident, please contact us if this issue persists."
       );
     }
-  }
+  };
 
   const handleAssociateAlerts = (e: FormEvent) => {
     e.preventDefault();
-    associateAlertsHandler(selectedIncident)
-  }
+    if (selectedIncident) associateAlertsHandler(selectedIncident);
+  };
 
-  const showCreateIncidentForm = useCallback(() => setCreateIncident(true), [])
+  const showCreateIncidentForm = useCallback(() => setCreateIncident(true), []);
 
-  const hideCreateIncidentForm = useCallback(() => setCreateIncident(false), [])
+  const hideCreateIncidentForm = useCallback(
+    () => setCreateIncident(false),
+    []
+  );
 
-  const onIncidentCreated = useCallback((incidentId: string) => {
-    hideCreateIncidentForm()
-    handleClose()
-    associateAlertsHandler(incidentId)
-  }, [])
+  const onIncidentCreated = useCallback(
+    (incidentId: string) => {
+      hideCreateIncidentForm();
+      handleClose();
+      associateAlertsHandler(incidentId);
+    },
+    [associateAlertsHandler, handleClose, hideCreateIncidentForm]
+  );
 
   // reset modal state after closing
   useEffect(() => {
     if (!isOpen) {
-      hideCreateIncidentForm()
-      setSelectedIncident(null)
+      hideCreateIncidentForm();
+      setSelectedIncident(null);
     }
-  }, [isOpen])
+  }, [hideCreateIncidentForm, isOpen]);
+
+  // if this modal should not be open, do nothing
+  if (!alerts) return null;
 
   return (
     <Modal
@@ -97,71 +102,68 @@ const AlertAssociateIncidentModal = ({
             createCallback={onIncidentCreated}
             exitCallback={hideCreateIncidentForm}
           />
-        ): incidents && incidents.items.length > 0 ? (
-            <div className="h-full justify-center">
-              <Select
-                className="my-2.5"
-                placeholder={`Select incident`}
-                value={selectedIncident}
-                onValueChange={(value) => setSelectedIncident(value)}
+        ) : incidents && incidents.items.length > 0 ? (
+          <div className="h-full justify-center">
+            <Select
+              className="my-2.5"
+              placeholder={`Select incident`}
+              value={selectedIncident}
+              onValueChange={(value) => setSelectedIncident(value)}
+            >
+              {
+                incidents.items?.map((incident) => {
+                  return (
+                    <SelectItem key={incident.id} value={incident.id}>
+                      {incident.name}
+                    </SelectItem>
+                  );
+                })!
+              }
+            </Select>
+            <Divider />
+            <div className="flex items-center justify-between gap-6">
+              <Button
+                className="flex-1"
+                color="orange"
+                onClick={handleAssociateAlerts}
+                disabled={selectedIncident === null}
               >
-                {
-                  incidents.items?.map((incident) => {
-                    return (
-                      <SelectItem
-                        key={incident.id}
-                        value={incident.id}
-                      >
-                        {incident.name}
-                      </SelectItem>
-                    );
-                  })!
-                }
-              </Select>
-              <Divider />
-              <div className="flex items-center justify-between gap-6">
-                <Button
-                  className="flex-1"
-                  color="orange"
-                  onClick={handleAssociateAlerts}
-                  disabled={selectedIncident === null}
-                >
-                  Associate {alerts.length} alert{alerts.length > 1 ? "s" : ""}
-                </Button>
+                Associate {alerts.length} alert{alerts.length > 1 ? "s" : ""}
+              </Button>
 
-                <Button 
-                  className="flex-1"
-                  color="green"
-                  onClick={showCreateIncidentForm}
-                >
-                  Create a new incident
-                </Button>
-              </div>
-          </div>
-        ): (
-          <div className="flex flex-col items-center justify-center gap-y-8 h-full">
-              <div className="text-center space-y-3">
-                <Title className="text-2xl">No Incidents Yet</Title>
-              </div>
-
-              <div className="flex items-center justify-between w-full gap-6">
-                <Button
-                  className="flex-1"
-                  color="orange"
-                  onClick={() => router.push("/incidents")}
-                >
-                  Incidents page
-                </Button>
-
-                <Button
-                  className="flex-1"
-                  color="green"
-                  onClick={showCreateIncidentForm}
-                >
-                  Create a new incident
-                </Button>
-              </div>
+              <Button
+                className="flex-1"
+                color="green"
+                onClick={showCreateIncidentForm}
+              >
+                Create a new incident
+              </Button>
             </div>
+          </div>
+        ) : (
+          <div className="flex flex-col items-center justify-center gap-y-8 h-full">
+            <div className="text-center space-y-3">
+              <Title className="text-2xl">No Incidents Yet</Title>
+            </div>
+
+            <div className="flex items-center justify-between w-full gap-6">
+              <Button
+                className="flex-1"
+                color="orange"
+                onClick={() => router.push("/incidents")}
+              >
+                Incidents page
+              </Button>
+
+              <Button
+                className="flex-1"
+                color="green"
+                onClick={showCreateIncidentForm}
+              >
+                Create a new incident
+              </Button>
+            </div>
+          </div>
         )}
       </div>
     </Modal>
