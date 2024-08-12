baae7b09dd52da2883484a6eb884ead7515fe69d
Matvey Kukuy <motakuk@gmail.com>
2024-08-12 12:00:50 +0300

    Over-complicated imports so noone will ever be able to debug this

--- keep/api/utils/import_ee.py
+++ keep/api/utils/import_ee.py
@@ -2,15 +2,20 @@ import os
 import sys
 import pathlib
 
-ee_enabled = os.environ.get("EE_ENABLED", "false") == "true"
-if ee_enabled:
+EE_ENABLED = os.environ.get("EE_ENABLED", "false") == "true"
+EE_PATH = os.environ.get("EE_PATH", "../ee")  # Path related to the fastapi root directory
+
+if EE_ENABLED:
     path_with_ee = (
-        str(pathlib.Path(__file__).parent.resolve()) + "/../../../ee/experimental"
+        str(pathlib.Path(__file__).parent.resolve()) + 
+        "/../../" +  # To go to the fastapi root directory
+        EE_PATH + 
+        "/../" # To go to the parent directory of the ee directory to allow imports like ee.abc.abc
     )
     sys.path.insert(0, path_with_ee)
-    from incident_utils import mine_incidents_and_create_objects  # noqa
-    from incident_utils import ALGORITHM_VERBOSE_NAME  # noqa 
+
+    from ee.experimental.incident_utils import mine_incidents_and_create_objects  # noqa
+    from ee.experimental.incident_utils import ALGORITHM_VERBOSE_NAME  # noqa 
 else:
     mine_incidents_and_create_objects = NotImplemented
     ALGORITHM_VERBOSE_NAME = NotImplemented
-    
