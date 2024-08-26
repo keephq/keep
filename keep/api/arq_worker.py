3c593f3bfb5e7c4cc82ef62ae0b8d9ed8df80e18
Matvey Kukuy <Matvey-Kuk@users.noreply.github.com>
2024-08-26 15:52:19 +0300

    fix: Extended arq timeouts and a clear separation between AI and non-ai workers (#1701)
    Co-authored-by: Shahar Glazner <shaharglazner@gmail.com>
    Co-authored-by: Tal <tal@keephq.dev>

--- keep/api/arq_worker.py
+++ keep/api/arq_worker.py
@@ -1,29 +1,49 @@
-# builtins
+import logging
 from typing import Optional
 
-# third-party
 from arq import Worker, create_pool, cron
 from arq.connections import RedisSettings
 from arq.worker import create_worker
 from pydantic.utils import import_string
 from starlette.datastructures import CommaSeparatedStrings
 
-# internals
+import keep.api.logging
 from keep.api.core.config import config
 from keep.api.tasks.process_background_ai_task import process_background_ai_task
 from keep.api.tasks.healthcheck_task import healthcheck_task
+from keep.api.consts import (
+    KEEP_ARQ_TASK_POOL, 
+    KEEP_ARQ_TASK_POOL_AI, 
+    KEEP_ARQ_TASK_POOL_ALL, 
+    KEEP_ARQ_TASK_POOL_BASIC_PROCESSING,
+)
+
+keep.api.logging.setup_logging()
+logger = logging.getLogger(__name__)
+
+# Current worker will pick up tasks only according to it's execution pool:
+all_tasks_for_the_worker = ["keep.api.tasks.healthcheck_task.healthcheck_task"]
+
+if KEEP_ARQ_TASK_POOL == KEEP_ARQ_TASK_POOL_ALL or \
+    KEEP_ARQ_TASK_POOL == KEEP_ARQ_TASK_POOL_BASIC_PROCESSING:
+    all_tasks_for_the_worker += [
+        "keep.api.tasks.process_event_task.async_process_event",
+        "keep.api.tasks.process_topology_task.async_process_topology",
+    ]
+
+if KEEP_ARQ_TASK_POOL == KEEP_ARQ_TASK_POOL_ALL or \
+    KEEP_ARQ_TASK_POOL == KEEP_ARQ_TASK_POOL_AI:
+    all_tasks_for_the_worker += [
+        "keep.api.tasks.process_background_ai_task.process_background_ai_task",
+        "keep.api.tasks.process_background_ai_task.process_correlation",
+    ]
 
 ARQ_BACKGROUND_FUNCTIONS: Optional[CommaSeparatedStrings] = config(
     "ARQ_BACKGROUND_FUNCTIONS",
     cast=CommaSeparatedStrings,
-    default=[
-        "keep.api.tasks.process_event_task.async_process_event",
-        "keep.api.tasks.process_topology_task.async_process_topology",
-        "keep.api.tasks.process_background_ai_task.process_background_ai_task",
-        "keep.api.tasks.process_background_ai_task.process_correlation",
-        "keep.api.tasks.healthcheck_task.healthcheck_task",
-    ],
+    default=all_tasks_for_the_worker,
 )
+
 FUNCTIONS: list = (
     [
         import_string(background_function)
@@ -54,8 +74,7 @@ async def get_pool():
         )
     )
 
-
-def get_worker() -> Worker:
+def get_arq_worker() -> Worker:
     keep_result = config(
         "ARQ_KEEP_RESULT", cast=int, default=3600
     )  # duration to keep job results for
@@ -68,6 +87,7 @@ def get_worker() -> Worker:
 
 def at_every_x_minutes(x: int, start: int = 0, end: int = 59):
     return {*list(range(start, end, x))}
+
 
 class WorkerSettings:
     """
@@ -85,9 +105,10 @@ class WorkerSettings:
         conn_retries=10,
         conn_retry_delay=10,
     )
+    # Only if it's an AI-dedicated worker, we can set large timeout, otherwise keeping low to avoid clogging
+    timeout = 60 * 15 if KEEP_ARQ_TASK_POOL == KEEP_ARQ_TASK_POOL_AI else 30
     functions: list = FUNCTIONS
     cron_jobs = [
-      
         cron(
             healthcheck_task,
             minute=at_every_x_minutes(1),
@@ -96,12 +117,16 @@ class WorkerSettings:
             max_tries=1, 
             run_at_startup=True,
         ),
-        cron(
-            process_background_ai_task,
-            minute=at_every_x_minutes(1),
-            unique=True,
-            timeout=30, 
-            max_tries=1, 
-            run_at_startup=True,
-        ),
     ]
+    if KEEP_ARQ_TASK_POOL == KEEP_ARQ_TASK_POOL_ALL or \
+        KEEP_ARQ_TASK_POOL == KEEP_ARQ_TASK_POOL_AI:
+        cron_jobs.append(
+            cron(
+                process_background_ai_task,
+                minute=at_every_x_minutes(1),
+                unique=True,
+                timeout=30, 
+                max_tries=1, 
+                run_at_startup=True,
+            )
+        )
