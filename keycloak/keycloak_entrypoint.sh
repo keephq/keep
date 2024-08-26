a8457445874619aea7fd1d9e029abbce7e7ba424
shahargl <shaharglazner@gmail.com>
2024-08-26 11:22:54 +0300

    feat: wip

--- keycloak/keycloak_entrypoint.sh
+++ keycloak/keycloak_entrypoint.sh
@@ -1,16 +1,36 @@
 #!/bin/sh
 
+# Verify env var and if not exit
+if [ -z "$KEYCLOAK_ADMIN" ]; then
+    echo "KEYCLOAK_ADMIN is not set. Exiting..."
+    exit 1
+fi
+if [ -z "$KEYCLOAK_ADMIN_PASSWORD" ]; then
+    echo "KEYCLOAK_ADMIN_PASSWORD is not set. Exiting..."
+    exit 1
+fi
+if [ -z "$KC_HTTP_RELATIVE_PATH" ]; then
+    echo "KC_HTTP_RELATIVE_PATH is not set. Exiting..."
+    exit 1
+fi
+
+# If not KEEP_URL, default Keep frontend to http://localhost:3000
+if [ -z "$KEEP_URL" ]; then
+    echo "KEEP_URL is not set. Defaulting to http://localhost:3000"
+    KEEP_URL="http://localhost:3000"
+fi
+
 # Start Keycloak in the background
 echo "Starting Keycloak"
 /opt/keycloak/bin/kc.sh start-dev --features=preview --import-realm -Dkeycloak.profile.feature.scripts=enabled -Dkeycloak.migration.strategy=OVERWRITE_EXISTIN &
 echo "Keycloak started"
 # Try to connect to Keycloak - wait until Keycloak is ready or timeout
 echo "Waiting for Keycloak to be ready"
-/opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user keep_kc --password keep_kc
+/opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/${KC_HTTP_RELATIVE_PATH} --realm master --user ${KEYCLOAK_ADMIN} --password ${KEYCLOAK_ADMIN_PASSWORD}
 while [ $? -ne 0 ]; do
      echo "Keycloak is not ready yet"
      sleep 5
-     /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user keep_kc --password keep_kc
+     /opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/${KC_HTTP_RELATIVE_PATH} --realm master --user ${KEYCLOAK_ADMIN} --password ${KEYCLOAK_ADMIN_PASSWORD}
 done
 
 if [ $? -eq 0 ]; then
@@ -21,20 +41,27 @@ else
 fi
 
 # Configure the theme
-echo "Configuring theme"
+echo "Configuring Signin theme (for Keep tenant)"
 /opt/keycloak/bin/kcadm.sh update realms/keep -s "loginTheme=keywind"
-/opt/keycloak/bin/kcadm.sh update realms/keep -s "loginTheme=phasetwo.v2"
-echo "Theme configured"
+echo "Configuring Admin Console theme (for Orgs)"
+/opt/keycloak/bin/kcadm.sh update realms/keep -s "adminTheme=phasetwo.v2"
+/opt/keycloak/bin/kcadm.sh update realms/master -s "adminTheme=phasetwo.v2"
+echo "Themes configured"
 
-# Export the realm
-echo "Exporting realm"
-/opt/keycloak/bin/kcadm.sh get realms/keep -o /tmp/realm-export.json
-echo "Realm exported"
+# Configure the event listener provider
+echo "Configuring event listener provider"
+/opt/keycloak/bin/kcadm.sh update realms/keep -s "eventsListeners+=last_login"
+echo "Event listener 'last_login' configured"
+
+# Configure Content-Security-Policy and X-Frame-Options
+# So that the SSO connect works with the Keep UI
+echo "Configuring Content-Security-Policy and X-Frame-Options"
+/opt/keycloak/bin/kcadm.sh update realms/keep -s 'browserSecurityHeaders.contentSecurityPolicy="frame-src '\''self'\'' '"$KEEP_URL"'; frame-ancestors '\''self'\'' '"$KEEP_URL"'; object-src '\''none'\'';"'
+/opt/keycloak/bin/kcadm.sh update realms/keep -s 'browserSecurityHeaders.xFrameOptions="ALLOW"'
+echo "Content-Security-Policy and X-Frame-Options configured"
 
 # just to keep the container running
 tail -f /dev/null
 
 # command to run this container with mount to this directory:
 # docker run -v $(pwd):/mnt -it --entrypoint /bin/sh quay.io/keycloak/keycloak:latest
-# keep->realm settings -> security defenses -> Content-Security-Policy -> frame-src 'self' http://localhost:3000; frame-ancestors 'self' http://localhost:3000; object-src 'none';
-# keep->realm settings -> security defenses -> X-Frame-Options -> Allow
