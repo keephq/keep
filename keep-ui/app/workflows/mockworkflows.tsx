45f7553a23e22384927f6c4430f837a326a767fa
Kirill Chernakov <yakiryous@gmail.com>
2024-08-26 18:25:34 +0400

    fix: Optimize UI for smaller screens (#1220) (#1491)
    Signed-off-by: Matvey Kukuy <Matvey-Kuk@users.noreply.github.com>
    Co-authored-by: Tal <tal@keephq.dev>
    Co-authored-by: Matvey Kukuy <Matvey-Kuk@users.noreply.github.com>
    Co-authored-by: Shahar Glazner <shaharglazner@gmail.com>

--- keep-ui/app/workflows/mockworkflows.tsx
+++ keep-ui/app/workflows/mockworkflows.tsx
@@ -151,7 +151,7 @@ export default function MockWorkflowCardSection({
             return (
               <Card
                 key={index}
-                className="p-4 flex flex-col justify-between w-full hover:border-orange-400 border-2 border-white"
+                className="p-4 flex flex-col justify-between w-full border-2 border-transparent hover:border-orange-400 "
               >
                 <div>
                   <WorkflowSteps workflow={workflow} />
