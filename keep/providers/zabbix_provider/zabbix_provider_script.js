eb1ead38d602eb0db889e19e7023e5947c44b4ac
Tal <tal@keephq.dev>
2024-08-30 12:35:26 +0300

    fix(zabbix): media type response failure (#1765)

--- keep/providers/zabbix_provider/zabbix_provider_script.js
+++ keep/providers/zabbix_provider/zabbix_provider_script.js
@@ -36,18 +36,10 @@ try {
   Zabbix.log(4, '[Keep Webhook] Received response: HTTP ' + req.getStatus() + ': ' + resp);
 
   if (req.getStatus() != 202) {
-      throw 'Response code not 200';
-      Zabbix.log(3, '[Keep Webhook] Error:' + JSON.stringify(resp));
+    throw 'Response code not 202';
   }
   else {
-      try {
-          resp = JSON.parse(resp);
-      }
-      catch (error) {
-          throw 'Incorrect response. Keep returned a non-JSON object.';
-          Zabbix.log(3, '[Keep Webhook] Error: Incorrect response. Keep returned a non-JSON object ' + JSON.stringify(resp));
-      }
-      return JSON.stringify(result);
+    return resp;
   }
 }
 catch (error) {
