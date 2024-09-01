57d0d10253601527b94b89c49351f78aa3153fb9
shahargl <shaharglazner@gmail.com>
2024-09-01 14:56:18 +0300

    feat: merge from main

--- keep-ui/app/settings/layout.tsx
+++ keep-ui/app/settings/layout.tsx
@@ -3,7 +3,7 @@ import { Card, Title, Subtitle } from "@tremor/react";
 export default function Layout({ children }: { children: any }) {
   return (
     <>
-     <main className="flex flex-col h-screen p-4 md:p-10">
+     <main className="flex flex-col h-full">
         <div className="mb-4">
           <Title>Settings</Title>
           <Subtitle>Setup and configure Keep</Subtitle>
