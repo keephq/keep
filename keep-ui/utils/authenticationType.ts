fdd547099c81510a94a81d88e93f7dc44312210e
shahargl <shaharglazner@gmail.com>
2024-09-02 10:24:49 +0300

    feat: last minute bug

--- keep-ui/utils/authenticationType.ts
+++ keep-ui/utils/authenticationType.ts
@@ -8,9 +8,9 @@ export enum AuthenticationType {
 }
 
 // Backward compatibility
-export const MULTI_TENANT = AuthenticationType.AUTH0;
-export const SINGLE_TENANT = AuthenticationType.DB;
-export const NO_AUTH = AuthenticationType.NOAUTH;
+export const MULTI_TENANT = "MULTI_TENANT";
+export const SINGLE_TENANT = "SINGLE_TENANT";
+export const NO_AUTH = "NO_AUTH";
 
 export const NoAuthUserEmail = "keep";
 export const NoAuthTenant = "keep";
