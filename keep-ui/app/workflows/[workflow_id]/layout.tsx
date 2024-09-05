ae99b6774773be19afbe6739415eb49195673377
Rajesh Jonnalagadda <38752904+rajeshj11@users.noreply.github.com>
2024-09-05 16:33:17 +0530

    feat: new workflow execution page local main (#1762)
    Co-authored-by: Tal <tal@keephq.dev>

--- keep-ui/app/workflows/[workflow_id]/layout.tsx
+++ keep-ui/app/workflows/[workflow_id]/layout.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { Card, Title, Subtitle, Text, Icon } from "@tremor/react";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { MdModeEdit } from "react-icons/md";
-import { useWorkflows } from "utils/hooks/useWorkflows";
+
 
 export default function Layout({
   children,
@@ -12,38 +10,18 @@ export default function Layout({
   children: any;
   params: { workflow_id: string };
 }) {
-  const pathname = usePathname();
-  const { data: workflows } = useWorkflows();
-  const workflow = workflows?.find((wf) => wf.id === params.workflow_id);
+
   return (
     <>
-      <main className="p-4 md:p-10 mx-auto max-w-full">
-        <div className="flex justify-between">
-          <div>
-            <Title>Workflow {workflow && ` - ${workflow.name}`}</Title>
-            {workflow && (
-              <Text>
-                Description:{" "}
-                <span className="font-bold">{workflow.description}</span>
-              </Text>
-            )}
-            {pathname?.includes("runs") === false && (
-              <Subtitle>List of all recent workflow executions</Subtitle>
-            )}
-          </div>
-          <Link href={`/workflows/builder/${params.workflow_id}`}>
-            <Icon
-              icon={MdModeEdit}
-              color="orange"
-              className="hover:bg-orange-100"
-              size="xl"
-              tooltip="Edit Workflow"
-              variant="solid"
-            />
-          </Link>
-        </div>
-        <Card className="mt-10 p-4 md:p-10 mx-auto">{children}</Card>
-      </main>
+      <div className="flex items-center mb-4 max-h-full">
+        <Link
+          href="/workflows"
+          className="flex items-center text-gray-500 hover:text-gray-700"
+        >
+          <ArrowLeftIcon className="h-5 w-5 mr-1" /> Back to Workflows
+        </Link>
+      </div>
+      <div className="overflow-auto">{children}</div>
     </>
   );
 }
