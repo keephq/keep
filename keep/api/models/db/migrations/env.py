a19e57d11efd471330dc0201a1507636770041c1
shahargl <shaharglazner@gmail.com>
2024-09-11 13:11:13 +0300

    feat: wip

--- keep/api/models/db/migrations/env.py
+++ keep/api/models/db/migrations/env.py
@@ -11,14 +11,11 @@ from keep.api.models.db.action import *
 from keep.api.models.db.alert import *
 from keep.api.models.db.dashboard import *
 from keep.api.models.db.extraction import *
-from keep.api.models.db.maintenance_window import *
 from keep.api.models.db.mapping import *
 from keep.api.models.db.preset import *
 from keep.api.models.db.provider import *
 from keep.api.models.db.rule import *
-from keep.api.models.db.statistics import *
 from keep.api.models.db.tenant import *
-from keep.api.models.db.topology import *
 from keep.api.models.db.user import *
 from keep.api.models.db.workflow import *
 
@@ -55,7 +52,6 @@ async def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-        render_as_batch=True,
     )
 
     with context.begin_transaction():
@@ -68,9 +64,7 @@ def do_run_migrations(connection: Connection) -> None:
 
     :param connection: connection to the database.
     """
-    context.configure(
-        connection=connection, target_metadata=target_metadata, render_as_batch=True
-    )
+    context.configure(connection=connection, target_metadata=target_metadata)
 
     with context.begin_transaction():
         context.run_migrations()
