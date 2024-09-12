a0e46971b09038857c2cef8cc813964fd1a91e69
Vladimir Filonov <vladimir@filonov.dev>
2024-09-12 14:51:15 +0400

    Remove unused imports

--- keep/api/models/db/migrations/versions/2024-09-12-14-26_9af44b2de182.py
+++ keep/api/models/db/migrations/versions/2024-09-12-14-26_9af44b2de182.py
@@ -6,11 +6,6 @@ Create Date: 2024-09-12 14:26:29.985470
 
 """
 
-import sqlalchemy as sa
-import sqlalchemy_utils
-import sqlmodel
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision = "9af44b2de182"
 down_revision = ("772790c2e50a", "710b4ff1d19e")
