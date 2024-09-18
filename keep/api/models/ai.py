abcd4d5f64dbabee36df1bc5b3fa447dd4d16315
GlebBerjoskin <berjoskin@protonmail.com>
2024-09-18 16:00:23 +0200

    refactor: correct used parameters

--- keep/api/models/ai.py
+++ keep/api/models/ai.py
@@ -13,7 +13,7 @@ class IncidentMineConfiguration(BaseModel):
     pmi_threshold: float = 0.0
     knee_threshold: float = 0.8
     min_incident_size: int = 5
-    min_alert_number: int = None
+    min_alert_number: int = 100
     incident_similarity_threshold: float = 0.8
     general_temp_dir: str = None
     sliding_window: int = 1 * 60 * 60
