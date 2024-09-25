c9d826287b8ad786701b4ebb486ea9ed6c82a0bf
shahargl <shaharglazner@gmail.com>
2024-09-25 12:20:01 +0300

    feat: revert redundant

--- keep/api/models/db/migrations/env.py
+++ keep/api/models/db/migrations/env.py
@@ -55,6 +55,7 @@ async def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        render_as_batch=True,
     )
 
     with context.begin_transaction():
@@ -67,7 +68,9 @@ def do_run_migrations(connection: Connection) -> None:
 
     :param connection: connection to the database.
     """
-    context.configure(connection=connection, target_metadata=target_metadata)
+    context.configure(
+        connection=connection, target_metadata=target_metadata, render_as_batch=True
+    )
 
     with context.begin_transaction():
         context.run_migrations()
