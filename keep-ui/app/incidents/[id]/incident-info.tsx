f4e10654213f9d806019eb6d96140c1718ac698f
Vladimir Filonov <vladimir@filonov.dev>
2024-10-03 22:56:08 +0400

    Show status and severity at incident detail view page

--- keep-ui/app/incidents/[id]/incident-info.tsx
+++ keep-ui/app/incidents/[id]/incident-info.tsx
@@ -1,4 +1,4 @@
-import { Button, Icon, Title } from "@tremor/react";
+import {Badge, Button, Icon, Title} from "@tremor/react";
 import { IncidentDto } from "../models";
 import CreateOrUpdateIncident from "../create-or-update-incident";
 import Modal from "@/components/ui/Modal";
@@ -13,6 +13,8 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { format } from "date-fns";
 import { ArrowUturnLeftIcon } from "@heroicons/react/24/outline";
+import IncidentChangeStatusModal from "@/app/incidents/incident-change-status-modal";
+import {STATUS_ICONS} from "@/app/incidents/statuses";
 
 interface Props {
   incident: IncidentDto;
@@ -37,14 +39,29 @@ export default function IncidentInformation({ incident }: Props) {
     mutate();
   };
 
+  const [changeStatusIncident, setChangeStatusIncident] =
+    useState<IncidentDto | null>();
+
+  const handleChangeStatus = (e: React.MouseEvent, incident: IncidentDto) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setChangeStatusIncident(incident);
+  };
+
   const formatString = "dd, MMM yyyy - HH:mm.ss 'UTC'";
   const summary = incident.user_summary || incident.generated_summary;
+
+  const severity = incident.severity;
+  let severityColor;
+  if (severity === "critical") severityColor = "red";
+  else if (severity === "info") severityColor = "blue";
+  else if (severity === "warning") severityColor = "yellow";
 
   return (
     <div className="flex h-full flex-col justify-between">
       <div className="flex flex-col gap-2">
-        <div className="flex justify-between text-sm">
-          <Title className="">
+        <div className="flex justify-between text-sm gap-1">
+          <Title className="flex-grow items-center">
             {incident.is_confirmed ? "⚔️ " : "Possible "}Incident
           </Title>
           {incident.is_confirmed && (
@@ -102,8 +119,6 @@ export default function IncidentInformation({ incident }: Props) {
               />
             </div>
           )}
-        </div>
-        <div className="prose-2xl flex gap-2 items-start">
           <Icon
             icon={ArrowUturnLeftIcon}
             tooltip="Go Back"
@@ -111,9 +126,20 @@ export default function IncidentInformation({ incident }: Props) {
             className="cursor-pointer"
             onClick={() => router.back()}
           />
+        </div>
+        <div className="prose-2xl flex gap-2 items-center">
+          <Badge color={severityColor} className="capitalize">{incident.severity}</Badge>
           <span>
             {incident.user_generated_name || incident.ai_generated_name}
           </span>
+        </div>
+        <div>
+          <h3 className="text-gray-500 text-sm">Status</h3>
+          <div>
+            <div onClick={(e) => handleChangeStatus(e, incident)} className="capitalize flex-grow-0 inline-flex items-center cursor-pointer">
+              {STATUS_ICONS[incident.status]} {incident.status}
+            </div>
+          </div>
         </div>
         <div>
           <h3 className="text-gray-500 text-sm">Summary</h3>
@@ -155,6 +181,12 @@ export default function IncidentInformation({ incident }: Props) {
           exitCallback={handleFinishEdit}
         />
       </Modal>
+
+      <IncidentChangeStatusModal
+        incident={changeStatusIncident}
+        mutate={mutate}
+        handleClose={() => setChangeStatusIncident(null)}
+      />
     </div>
   );
 }
