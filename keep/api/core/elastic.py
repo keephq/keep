95cab65f8057c4ae4d666ac931c75f568b30e3d8
Shahar Glazner <shaharglazner@gmail.com>
2024-10-04 17:42:43 +0300

    fix(backend): order modify the alert itself (#2088)

--- keep/api/core/elastic.py
+++ keep/api/core/elastic.py
@@ -195,11 +195,11 @@ class ElasticClient:
             return
 
         try:
-            # change severity to number so we can sort by it
-            alert.severity = AlertSeverity(alert.severity.lower()).order
             # query
             alert_dict = alert.dict()
             alert_dict["dismissed"] = bool(alert_dict["dismissed"])
+            # change severity to number so we can sort by it
+            alert_dict["severity"] = AlertSeverity(alert.severity.lower()).order
             self._client.index(
                 index=self.alerts_index,
                 body=alert_dict,
