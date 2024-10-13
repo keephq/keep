44c564e24df657ca2884abde97ded6605e0061f9
Kirill Chernakov <yakiryous@gmail.com>
2024-10-13 19:34:47 +0400

    Revert "chore: add @next/bundle-analyzer to devDependencies (#2177)"
    This reverts commit 035ee455f0a90350c4067ec98ca2a400be39ba03.

--- keep-ui/next.config.js
+++ keep-ui/next.config.js
@@ -66,8 +66,4 @@ const nextConfig = {
   },
 };
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
