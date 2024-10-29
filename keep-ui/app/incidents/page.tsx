0dbcd98a5b1a55ce0bf1bb0ade22c50cca310acd
Kirill Chernakov <yakiryous@gmail.com>
2024-10-29 16:52:53 +0400

    feat: use server-side rendering for incident list (#2336)

--- keep-ui/app/incidents/page.tsx
+++ keep-ui/app/incidents/page.tsx
@@ -1,7 +1,32 @@
+import { getServerSession } from "next-auth/next";
 import IncidentList from "./incident-list";
+import { getApiURL } from "@/utils/apiUrl";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import {
+  getIncidents,
+  GetIncidentsParams,
+} from "@/entities/incidents/api/incidents";
+import { PaginatedIncidentsDto } from "./models";
 
-export default function Page() {
-  return <IncidentList />;
+const defaultIncidentsParams: GetIncidentsParams = {
+  confirmed: true,
+  limit: 20,
+  offset: 0,
+  sorting: { id: "creation_time", desc: true },
+  filters: {},
+};
+
+export default async function Page() {
+  let incidents: PaginatedIncidentsDto | null = null;
+  try {
+    const session = await getServerSession(authOptions);
+    const apiUrl = getApiURL();
+
+    incidents = await getIncidents(apiUrl, session, defaultIncidentsParams);
+  } catch (error) {
+    console.log(error);
+  }
+  return <IncidentList initialData={incidents ?? undefined} />;
 }
 
 export const metadata = {
---
+++ keep-ui/entities/incidents/api/incidents.ts
@@ -0,0 +1,52 @@
+import { PaginatedIncidentsDto } from "@/app/incidents/models";
+import { fetcher } from "@/utils/fetcher";
+import { Session } from "next-auth";
+
+interface Filters {
+  status: string[];
+  severity: string[];
+  assignees: string[];
+  sources: string[];
+  affected_services: string[];
+}
+
+export type GetIncidentsParams = {
+  confirmed: boolean;
+  limit: number;
+  offset: number;
+  sorting: { id: string; desc: boolean };
+  filters: Filters | {};
+};
+
+export function buildIncidentsUrl(apiUrl: string, params: GetIncidentsParams) {
+  const filtersParams = new URLSearchParams();
+
+  Object.entries(params.filters).forEach(([key, value]) => {
+    if (value.length == 0) {
+      filtersParams.delete(key as string);
+    } else {
+      value.forEach((s: string) => {
+        filtersParams.append(key, s);
+      });
+    }
+  });
+
+  return `${apiUrl}/incidents?confirmed=${params.confirmed}&limit=${params.limit}&offset=${params.offset}&sorting=${
+    params.sorting.desc ? "-" : ""
+  }${params.sorting.id}&${filtersParams.toString()}`;
+}
+
+export async function getIncidents(
+  apiUrl: string,
+  session: Session | null,
+  params: GetIncidentsParams
+) {
+  if (!session) {
+    return null;
+  }
+  const url = buildIncidentsUrl(apiUrl, params);
+  return (await fetcher(
+    url,
+    session.accessToken
+  )) as Promise<PaginatedIncidentsDto>;
+}
