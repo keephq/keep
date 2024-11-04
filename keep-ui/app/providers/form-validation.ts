4e77c5c54f9d2c8e13bf13a1506d3734e22bf220
theedigerati <39467790+theedigerati@users.noreply.github.com>
2024-11-04 23:48:23 +0100

    add new validation logic for url

--- keep-ui/app/providers/form-validation.ts
+++ keep-ui/app/providers/form-validation.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { Provider } from "./providers";
 
-type UrlOptions = {
+type URLOptions = {
   protocols: string[];
   requireTld: boolean;
   requireProtocol: boolean;
@@ -12,7 +12,7 @@ type UrlOptions = {
 
 type ValidatorRes = { success: true } | { success: false; msg: string };
 
-const defaultUrlOptions: UrlOptions = {
+const defaultURLOptions: URLOptions = {
   protocols: [],
   requireTld: false,
   requireProtocol: true,
@@ -23,81 +23,159 @@ const defaultUrlOptions: UrlOptions = {
 
 function mergeOptions<T extends Record<string, unknown>>(
   defaults: T,
-  opts?: T
-) {
+  opts?: Partial<T>
+): T {
   if (!opts) return defaults;
-  for (const key in defaults) {
-    if (typeof opts[key] === "undefined") {
-      opts[key] = defaults[key];
-    }
-  }
-  return opts;
+  return { ...defaults, ...opts };
 }
 
 const error = (msg: string) => ({ success: false, msg });
-const urlError = { success: false, msg: "Please provide a valid URL." };
-const protocolError = {
-  success: false,
-  msg: "A valid URL protocol is required.",
-};
-const relProtocolError = {
-  success: false,
-  msg: "A protocol-relavie URL is not allowed.",
-};
+const urlError = error("Please provide a valid URL.");
+const protocolError = error("A valid URL protocol is required.");
+const relProtocolError = error("A protocol-relavie URL is not allowed.");
+const missingPortError = error("A URL with a port number is required.");
+const portError = error("Invalid port number.");
+const hostError = error("Invalid URL host.");
+const hostWildcardError = error("Wildcard in URL host is not allowed");
+const tldError = error(
+  "URL must contain a valid TLD e.g .com, .io, .dev, .net"
+);
 
-function getProtocolError(opts: UrlOptions["protocols"]) {
+function getProtocolError(opts: URLOptions["protocols"]) {
   if (opts.length === 0) return protocolError;
-  if (opts.length === 1) return error(`A URL with \`${opts[0]}\` is required.`);
+  if (opts.length === 1)
+    return error(`A URL with \`${opts[0]}\` protocol is required.`);
   if (opts.length === 2)
-    return error(`A URL with \`${opts[0]}\` or \`${opts[1]}\` is required.`);
+    return error(
+      `A URL with \`${opts[0]}\` or \`${opts[1]}\` protocol is required.`
+    );
   const lst = opts.length - 1;
-  const wrap = (x: string, y: string) => `\`${x} + ${y}\``;
+  const wrap = (acc: string, p: string) => acc + `\`${p}\``;
   const optsStr = opts.reduce(
     (acc, p, i) =>
-      i === 0
+      i === lst
         ? wrap(acc, p)
-        : i === lst
-          ? wrap(acc, `or ${p}`)
-          : wrap(acc, `, ${p}`),
+        : i === lst - 1
+          ? wrap(acc, p) + " or "
+          : wrap(acc, p) + ", ",
     ""
   );
-  return error(`A URL with one of ${optsStr} is required.`);
+  return error(`A URL with one of ${optsStr} protocols is required.`);
 }
 
-function isUrl(url: string, options?: UrlOptions): ValidatorRes {
-  const opts = mergeOptions(defaultUrlOptions, options);
+function isFQDN(str: string, options?: Partial<URLOptions>): ValidatorRes {
+  const opts = mergeOptions(defaultURLOptions, options);
 
-  if (url.length === 0 || /[\s<>]/.test(url)) return urlError;
-  if (opts.validateLength && url.length > opts.maxLength) {
-    return {
-      success: false,
-      msg: `Invalid url length, max of ${opts.maxLength} expected.`,
-    };
+  if (str[str.length - 1] === ".") return hostError; // trailing dot not allowed
+  if (str.indexOf("*.") === 0) return hostWildcardError; // wildcard not allowed
+
+  const parts = str.split(".");
+  const tld = parts[parts.length - 1];
+  const tldRegex =
+    /^([a-z\u00A1-\u00A8\u00AA-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]{2,}|xn[a-z0-9-]{2,})$/i;
+
+  if (
+    opts.requireTld &&
+    (parts.length < 2 || !tldRegex.test(tld) || /\s/.test(tld))
+  )
+    return tldError;
+
+  const partsValid = parts.every((part) => {
+    if (!/^[a-z_\u00a1-\uffff0-9-]+$/i.test(part)) {
+      return false;
+    }
+
+    // disallow full-width chars
+    if (/[\uff01-\uff5e]/.test(part)) {
+      return false;
+    }
+
+    // disallow parts starting or ending with hyphen
+    if (/^-|-$/.test(part)) {
+      return false;
+    }
+
+    return true;
+  });
+
+  return partsValid ? { success: true } : hostError;
+}
+
+function isIP(str: string) {
+  const validation = z.string().ip().safeParse(str);
+  return validation.success;
+}
+
+function isURL(str: string, options?: Partial<URLOptions>): ValidatorRes {
+  const opts = mergeOptions(defaultURLOptions, options);
+
+  if (str.length === 0 || /[\s<>]/.test(str)) return urlError;
+  if (opts.validateLength && str.length > opts.maxLength) {
+    return error(`Invalid url length, max of ${opts.maxLength} expected.`);
   }
 
-  let _url = url;
-  let protocol: string;
+  let url = str;
   let host: string;
-  let hostname: string;
   let port: number;
-  let portStr: string | null;
+  let portStr: string = "";
   let split: string[];
-  let ipv6: string | null;
 
   split = url.split("#");
-  _url = split.shift() ?? "";
+  url = split.shift() ?? "";
 
   split = url.split("?");
-  _url = split.shift() ?? "";
+  url = split.shift() ?? "";
 
-  if (_url.slice(0, 2) === "//") return relProtocolError;
+  if (url.slice(0, 2) === "//") return relProtocolError;
 
   split = url.split("://");
-  protocol = split?.shift()?.toLowerCase() ?? "";
+  const protocol = split?.shift()?.toLowerCase() ?? "";
   if (opts.requireProtocol && opts.protocols.indexOf(protocol) === -1)
     return getProtocolError(opts.protocols);
+  url = split.join("://");
 
-  return { success: true };
+  split = url.split("/");
+  url = split.shift() ?? "";
+  if (!url.length) return urlError;
+
+  split = url.split("@");
+  if (split.length > 1 && !split[0]) return urlError;
+  if (split.length > 1) {
+    const auth = split.shift() ?? "";
+    if (auth.split(":").length > 2) return urlError;
+    const [user, pass] = auth.split(":");
+    if (!user && !pass) return urlError;
+  }
+
+  const hostname = split.join("@");
+  const wrapped_ipv6 = /^\[([^\]]+)\](?::([0-9]+))?$/;
+  const ipv6Match = hostname.match(wrapped_ipv6);
+  if (ipv6Match) {
+    host = ipv6Match[1];
+    portStr = ipv6Match[2];
+  } else {
+    split = hostname.split(":");
+    host = split.shift() ?? "";
+    if (split.length) portStr = split.join(":");
+  }
+
+  if (portStr.length) {
+    port = parseInt(portStr, 10);
+    if (Number.isNaN(port)) return missingPortError;
+    if (port <= 0 || port > 65_535) return portError;
+  } else if (opts.requirePort) return missingPortError;
+
+  if (!host) return hostError;
+  if (isIP(host)) return { success: true };
+  return isFQDN(host);
+}
+
+function addZodErr(valdn: ValidatorRes, ctx: z.RefinementCtx) {
+  if (valdn.success) return;
+  ctx.addIssue({
+    code: z.ZodIssueCode.custom,
+    message: valdn.msg,
+  });
 }
 
 export function getZodSchema(fields: Provider["config"]) {
@@ -107,6 +185,7 @@ export function getZodSchema(fields: Provider["config"]) {
     .string()
     .optional()
     .transform((val) => (val?.length === 0 ? null : val));
+
   const kvPairs = Object.entries(fields).map(([field, config]) => {
     if (config.type === "form") {
       const baseFormSchema = z.record(z.string(), z.string()).array();
@@ -147,23 +226,19 @@ export function getZodSchema(fields: Provider["config"]) {
       return [field, switchSchema];
     }
 
-    const urlSchema = z.string({ required_error }).url({
-      message:
-        "Please provide a valid url, with a scheme & hostname as required.",
+    const urlStr = z.string({ required_error });
+    const urlSchema = urlStr.superRefine((url, ctx) => {
+      const valdn = isURL(url);
+      addZodErr(valdn, ctx);
     });
-    const urlTldSchema = z.string().regex(new RegExp(/\.[a-z]{2,63}$/), {
-      message: "Url must contain a valid TLD e.g .com, .io, .dev, .net",
+    const baseAnyHttpSchema = urlStr.superRefine((url, ctx) => {
+      const valdn = isURL(url, { protocols: ["http", "https"] });
+      addZodErr(valdn, ctx);
     });
-    const baseAnyHttpSchema = urlSchema.refine(
-      (url) => url.startsWith("http://") || url.startsWith("https://"),
-      { message: "A url with `http` or `https` protocol is reuquired." }
-    );
-    const baseHttpSchema = baseAnyHttpSchema.and(urlTldSchema);
-    const baseHttpsSchema = urlSchema
-      .refine((url) => url.startsWith("https://"), {
-        message: "A url with `https` protocol is required.",
-      })
-      .and(urlTldSchema);
+    const baseHttpsSchema = urlStr.superRefine((url, ctx) => {
+      const valdn = isURL(url, { requireTld: true, protocols: ["https"] });
+      addZodErr(valdn, ctx);
+    });
 
     if (config.validation === "any_url") {
       const anyUrlSchema = config.required
@@ -171,19 +246,11 @@ export function getZodSchema(fields: Provider["config"]) {
         : emptyStringToNull.pipe(urlSchema.nullish());
       return [field, anyUrlSchema];
     }
-
     if (config.validation === "any_http_url") {
       const anyHttpSchema = config.required
         ? baseAnyHttpSchema
         : emptyStringToNull.pipe(baseAnyHttpSchema.nullish());
       return [field, anyHttpSchema];
-    }
-
-    if (config.validation === "http_url") {
-      const httpSchema = config.required
-        ? baseHttpSchema
-        : emptyStringToNull.pipe(baseHttpSchema.nullish());
-      return [field, httpSchema];
     }
     if (config.validation === "https_url") {
       const httpsSchema = config.required
