9f4eab273ca59cbc8df8cd345ca849158c569842
Kirill Chernakov <yakiryous@gmail.com>
2024-11-04 15:21:19 +0400

    refactor: use plain css instead of react-chrono for incident activity

--- keep-ui/app/incidents/[id]/activity/incident-activity.tsx
+++ keep-ui/app/incidents/[id]/activity/incident-activity.tsx
@@ -15,13 +15,7 @@ import Loading from "@/app/loading";
 import { useSession } from "next-auth/react";
 import { IncidentActivityItem } from "./ui/IncidentActivityItem";
 import { IncidentActivityComment } from "./ui/IncidentActivityComment";
-import dynamic from "next/dynamic";
-
-// react-chrono is not compatible with server-side rendering, so we need to import it dynamically
-const Chrono = dynamic(
-  () => import("react-chrono").then((mod) => mod.Chrono),
-  { ssr: false } // Disable SSR for this component
-);
+import { useMemo } from "react";
 
 interface IncidentActivity {
   id: string;
@@ -31,17 +25,16 @@ interface IncidentActivity {
   initiator?: string | AlertDto;
 }
 
-// FIX: if the activity loaded as SSR, there's no activities displayed, only comment field is shown
 export function IncidentActivity({ incident }: { incident: IncidentDto }) {
   const { data: session } = useSession();
   const { useMultipleFingerprintsAlertAudit, useAlertAudit } = useAlerts();
   const { data: alerts, isLoading: alertsLoading } = useIncidentAlerts(
     incident.id
   );
-  const { data: auditEvents, isLoading: auditEventsLoading } =
+  const { data: auditEvents = [], isLoading: auditEventsLoading } =
     useMultipleFingerprintsAlertAudit(alerts?.items.map((m) => m.fingerprint));
   const {
-    data: incidentEvents,
+    data: incidentEvents = [],
     isLoading: incidentEventsLoading,
     mutate: mutateIncidentActivity,
   } = useAlertAudit(incident.id);
@@ -49,12 +42,45 @@ export function IncidentActivity({ incident }: { incident: IncidentDto }) {
   const { data: users, isLoading: usersLoading } = useUsers();
   usePollIncidentComments(incident.id);
 
+  const auditActivities = useMemo(() => {
+    if (!auditEvents.length && !incidentEvents.length) {
+      return [];
+    }
+    return (
+      auditEvents
+        .concat(incidentEvents)
+        .sort(
+          (a, b) =>
+            new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
+        )
+        .map((auditEvent) => {
+          const _type =
+            auditEvent.action === "A comment was added to the incident" // @tb: I wish this was INCIDENT_COMMENT and not the text..
+              ? "comment"
+              : "alert";
+          return {
+            id: auditEvent.id,
+            type: _type,
+            initiator:
+              _type === "comment"
+                ? auditEvent.user_id
+                : alerts?.items.find(
+                    (a) => a.fingerprint === auditEvent.fingerprint
+                  ),
+            text: _type === "comment" ? auditEvent.description : "",
+            timestamp: auditEvent.timestamp,
+          } as IncidentActivity;
+        }) || []
+    );
+  }, [auditEvents, incidentEvents, alerts]);
+
   if (
     usersLoading ||
     incidentEventsLoading ||
     auditEventsLoading ||
     alertsLoading
   ) {
+    console.log("51 loading");
     return <Loading />;
   }
 
@@ -65,35 +91,9 @@ export function IncidentActivity({ incident }: { incident: IncidentDto }) {
     initiator: session?.user.email,
   };
 
-  const auditActivities =
-    auditEvents
-      ?.concat(incidentEvents || [])
-      .sort(
-        (a, b) =>
-          new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
-      )
-      .map((auditEvent) => {
-        const _type =
-          auditEvent.action === "A comment was added to the incident" // @tb: I wish this was INCIDENT_COMMENT and not the text..
-            ? "comment"
-            : "alert";
-        return {
-          id: auditEvent.id,
-          type: _type,
-          initiator:
-            _type === "comment"
-              ? auditEvent.user_id
-              : alerts?.items.find(
-                  (a) => a.fingerprint === auditEvent.fingerprint
-                ),
-          text: _type === "comment" ? auditEvent.description : "",
-          timestamp: auditEvent.timestamp,
-        } as IncidentActivity;
-      }) || [];
-
   const activities = [newCommentActivity, ...auditActivities];
 
-  const chronoContent = activities?.map((activity, index) =>
+  const renderActivity = (activity: IncidentActivity) =>
     activity.type === "newcomment" ? (
       <IncidentActivityComment
         mutator={mutateIncidentActivity}
@@ -102,9 +102,9 @@ export function IncidentActivity({ incident }: { incident: IncidentDto }) {
       />
     ) : (
       <IncidentActivityItem key={activity.id} activity={activity} />
-    )
-  );
-  const chronoIcons = activities?.map((activity, index) => {
+    );
+
+  const renderIcon = (activity: IncidentActivity) => {
     if (activity.type === "comment" || activity.type === "newcomment") {
       const user = users?.find((user) => user.email === activity.initiator);
       return (
@@ -130,26 +130,27 @@ export function IncidentActivity({ incident }: { incident: IncidentDto }) {
         />
       );
     }
-  });
+  };
+
+  const renderItem = (item: any) => {
+    return (
+      <div className="flex gap-4">
+        <div className="relative py-6 w-12 flex items-center justify-center">
+          {/* vertical line */}
+          <div className="absolute mx-auto right-0 left-0 top-0 bottom-0 h-full bg-gray-200 w-px" />
+          {/* wrapping icon to avoid vertical line visible behind transparent background */}
+          <div className="relative z-[1] bg-tremor-background rounded-full border border-2 border-tremor-background">
+            {renderIcon(item)}
+          </div>
+        </div>
+        <div className="py-6 flex-1">{renderActivity(item)}</div>
+      </div>
+    );
+  };
 
   return (
-    <Chrono
-      items={activities?.map((activity) => ({
-        id: activity.id,
-        title: activity.timestamp,
-      }))}
-      hideControls
-      disableToolbar
-      borderLessCards={true}
-      slideShow={false}
-      mode="VERTICAL"
-      cardWidth={600}
-      cardHeight={100}
-      allowDynamicUpdate={true}
-      disableAutoScrollOnClick={true}
-    >
-      {chronoContent}
-      <div className="chrono-icons">{chronoIcons}</div>
-    </Chrono>
+    <div className="flex flex-col">
+      {activities?.map((activity) => renderItem(activity))}
+    </div>
   );
 }
--- keep-ui/app/incidents/[id]/activity/ui/IncidentActivityItem.tsx
+++ keep-ui/app/incidents/[id]/activity/ui/IncidentActivityItem.tsx
@@ -13,22 +13,22 @@ export function IncidentActivityItem({ activity }: { activity: any }) {
       : (activity.initiator?.status === "firing" ? " triggered" : " resolved") +
         ". ";
   return (
-    <div className="relative h-full w-full flex items-center">
-      {activity.type === "alert" &&
-        (activity.initiator as AlertDto)?.severity && (
-          <AlertSeverity
-            severity={(activity.initiator as AlertDto).severity}
-            marginLeft={false}
-          />
-        )}
-      <span className="font-semibold mr-2.5">{title}</span>
-      <span className="text-gray-300">
-        {subTitle} <TimeAgo date={activity.timestamp + "Z"} />
-      </span>
+    <div className="relative h-full w-full flex flex-col">
+      <div className="flex items-center gap-2">
+        {activity.type === "alert" &&
+          (activity.initiator as AlertDto)?.severity && (
+            <AlertSeverity
+              severity={(activity.initiator as AlertDto).severity}
+              marginLeft={false}
+            />
+          )}
+        <span className="font-semibold mr-2.5">{title}</span>
+        <span className="text-gray-300">
+          {subTitle} <TimeAgo date={activity.timestamp + "Z"} />
+        </span>
+      </div>
       {activity.text && (
-        <div className="absolute top-14 font-light text-gray-800">
-          {activity.text}
-        </div>
+        <div className="font-light text-gray-800">{activity.text}</div>
       )}
     </div>
   );
