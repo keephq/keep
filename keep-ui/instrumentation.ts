ba60c98487ccba8febb323035610f42f8f225c0b
Kirill Chernakov <yakiryous@gmail.com>
2024-11-07 21:28:41 +0400

    chore: add sentry to keep-ui
    (cherry picked from commit 6f136983ead8e93ffac7d0fc04a77ef5a5102000)

---
+++ keep-ui/instrumentation.ts
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/nextjs";
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("./sentry.server.config");
+  }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
+    await import("./sentry.edge.config");
+  }
+}
+
+// We need NextJS 15 to use this
+// export const onRequestError = Sentry.captureRequestError;
---
+++ keep-ui/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://5f5b1242cb21c80a4cddc2910d2e64a4@o4505515398922240.ingest.us.sentry.io/4508258058764288",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
