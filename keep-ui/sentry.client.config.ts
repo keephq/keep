1c92145ab94470715907ac37f713a8085f71f6bd
Kirill Chernakov <yakiryous@gmail.com>
2024-11-12 20:04:58 +0400

    fix: do not pass env, rely on auto-detection (e.g. vercel-preview)

--- keep-ui/sentry.client.config.ts
+++ keep-ui/sentry.client.config.ts
@@ -7,8 +7,6 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-  environment: process.env.NODE_ENV,
-
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
