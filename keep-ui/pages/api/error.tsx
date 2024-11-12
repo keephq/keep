832158fcf45a5d6b764ac9727530ff37b31679f9
Kirill Chernakov <yakiryous@gmail.com>
2024-11-12 21:06:04 +0400

    chore: remove example pages

--- keep-ui/pages/api/error.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-const error = (req: NextApiRequest, res: NextApiResponse) => {
-  throw new Error("API throw error test");
-  res.status(200).json({ name: "John Doe" });
-};
-
-export default error;
