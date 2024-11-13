26262d34b4aef4db94f15e536052d9fb761b7e97
Kirill Chernakov <yakiryous@gmail.com>
2024-11-13 16:25:42 +0400

    fix: do not fetch data twice due to wait for config (#2463)

---
+++ keep-ui/shared/ui/PostHogPageView.tsx
@@ -0,0 +1,53 @@
+// app/PostHogPageView.tsx
+"use client";
+
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { usePostHog } from "posthog-js/react";
+import { useConfig } from "@/utils/hooks/useConfig";
+import { useSession } from "next-auth/react";
+import { NoAuthUserEmail } from "@/utils/authenticationType";
+
+export default function PostHogPageView(): null {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthog = usePostHog();
+  const { data: config } = useConfig();
+  const { data: session } = useSession();
+
+  const isPosthogDisabled =
+    config?.POSTHOG_DISABLED === "true" || !config?.POSTHOG_KEY;
+
+  useEffect(() => {
+    // Track pageviews
+    if (!pathname || !posthog || isPosthogDisabled) {
+      return;
+    }
+    let url = window.origin + pathname;
+    if (searchParams && searchParams.toString()) {
+      url = url + `?${searchParams.toString()}`;
+    }
+    posthog.capture("$pageview", {
+      $current_url: url,
+      keep_version: process.env.NEXT_PUBLIC_KEEP_VERSION ?? "unknown",
+    });
+  }, [pathname, searchParams, posthog, isPosthogDisabled]);
+
+  useEffect(() => {
+    // Identify user in PostHog
+    if (isPosthogDisabled || !session) {
+      return;
+    }
+
+    const { user } = session;
+
+    const posthog_id = user.email;
+
+    if (posthog_id && posthog_id !== NoAuthUserEmail) {
+      console.log("Identifying user in PostHog");
+      posthog.identify(posthog_id);
+    }
+  }, [session, posthog, isPosthogDisabled]);
+
+  return null;
+}
