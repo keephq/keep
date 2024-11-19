f21d1b51b64d8d9c25f7005e4483460c5a47e487
Kirill Chernakov <yakiryous@gmail.com>
2024-11-20 00:09:04 +0400

    chore: remove unused function

--- keep-ui/utils/state.ts
+++ keep-ui/utils/state.ts
@@ -1,15 +1,5 @@
 import { useSWRConfig } from "swr";
 
-type MutateArgs = [string, (data: any) => any];
-
-export const mutateLocalMultiple = (args: MutateArgs[]) => {
-  const { cache } = useSWRConfig();
-  args.forEach(([key, mutateFunction]) => {
-    const currentData = cache.get(key as string);
-    cache.set(key as string, mutateFunction(currentData));
-  });
-};
-
 export const useRevalidateMultiple = () => {
   const { mutate } = useSWRConfig();
   return (keys: string[]) =>
