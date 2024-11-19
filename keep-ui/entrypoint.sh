2bf718f1a8c74e33d3ce52eec810ecd60090b98a
shahargl <shaharglazner@gmail.com>
2024-11-19 21:15:44 +0200

    feat: wip

--- keep-ui/entrypoint.sh
+++ keep-ui/entrypoint.sh
@@ -5,7 +5,7 @@ echo "Starting Nextjs [${API_URL}]"
 if [ -n "$HTTP_PROXY" ] || [ -n "$HTTPS_PROXY" ] || [ -n "$http_proxy" ] || [ -n "$https_proxy" ]; then
     echo "Proxy environment variables detected, running with patch"
     # Run with patch
-    exec node -r ./proxy-patch.js server.js
+    exec node server.js
 else
     exec node server.js
 fi
