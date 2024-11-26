b1cd5ee3ef6897d892af0257dc1fd0ece0d86242
35C4n0r <jaykumar20march@gmail.com>
2024-11-26 19:50:31 +0530

    feat: read system default theme.
    Signed-off-by: 35C4n0r <jaykumar20march@gmail.com>

--- keep-ui/app/dark-mode-toggle.tsx
+++ keep-ui/app/dark-mode-toggle.tsx
@@ -1,45 +1,72 @@
 import { Icon, Subtitle, Switch } from "@tremor/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { MdDarkMode } from "react-icons/md";
-import { useLocalStorage } from "utils/hooks/useLocalStorage";
 
 export default function DarkModeToggle() {
-  const [darkMode, setDarkMode] = useLocalStorage("darkMode", false);
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const localDarkMode = localStorage.getItem("keephq-darkMode");
+      let initialDarkMode;
+
+      if (localDarkMode !== null) {
+        // Use the value from localStorage if it exists
+        initialDarkMode = JSON.parse(localDarkMode);
+      } else {
+        // Otherwise, use the system preference
+        initialDarkMode = window.matchMedia(
+          "(prefers-color-scheme: dark)"
+        ).matches;
+      }
+
+      setDarkMode(initialDarkMode);
+
+      // Apply styles based on the initial dark mode setting
+      if (initialDarkMode) {
+        applyDarkModeStyles();
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (darkMode !== null) {
+      localStorage.setItem("keephq-darkMode", JSON.stringify(darkMode));
+
+      if (darkMode) {
+        applyDarkModeStyles();
+      } else {
+        removeDarkModeStyles();
+      }
+    }
+  }, [darkMode]);
 
   const applyDarkModeStyles = () => {
     /**
      * Taken from https://dev.to/jochemstoel/re-add-dark-mode-to-any-website-with-just-a-few-lines-of-code-phl
      */
-    var h = document.getElementsByTagName("head")[0],
-      s = document.createElement("style");
-    s.setAttribute("type", "text/css");
-    s.setAttribute("id", "nightify");
-    s.appendChild(
+    const head = document.getElementsByTagName("head")[0];
+    const styleElement = document.createElement("style");
+    styleElement.setAttribute("type", "text/css");
+    styleElement.setAttribute("id", "nightify");
+    styleElement.appendChild(
       document.createTextNode(
         "html{-webkit-filter:invert(100%) hue-rotate(180deg) contrast(80%) !important; background: #fff;} .line-content {background-color: #fefefe;}"
       )
     );
-    h.appendChild(s);
+    head.appendChild(styleElement);
+  };
+
+  const removeDarkModeStyles = () => {
+    const existingStyles = document.querySelectorAll("#nightify");
+    if (existingStyles.length) {
+      existingStyles.forEach((style) => style.parentNode?.removeChild(style));
+    }
   };
 
   const toggleDarkMode = () => {
-    /**
-     * Taken from https://dev.to/jochemstoel/re-add-dark-mode-to-any-website-with-just-a-few-lines-of-code-phl
-     */
-    setDarkMode(!darkMode);
-    let q = document.querySelectorAll("#nightify");
-    if (q.length) {
-      q.forEach((q) => q.parentNode?.removeChild(q));
-      return false;
-    }
-    applyDarkModeStyles();
+    setDarkMode((prevMode) => !prevMode);
   };
-
-  useEffect(() => {
-    if (darkMode) {
-      applyDarkModeStyles();
-    }
-  }, [darkMode]);
 
   return (
     <label
@@ -53,7 +80,13 @@ export default function DarkModeToggle() {
         />
         <Subtitle className="ml-2">Dark Mode</Subtitle>
       </span>
-      <Switch id="dark-mode" name="dark-mode" color="orange" onChange={toggleDarkMode} checked={darkMode} />
+      <Switch
+        id="dark-mode"
+        name="dark-mode"
+        color="orange"
+        onChange={toggleDarkMode}
+        checked={darkMode}
+      />
     </label>
   );
 }
