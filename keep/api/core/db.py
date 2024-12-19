498e8bbe041376b501180095f681aaaa7b259f4d
Kirill Chernakov <yakiryous@gmail.com>
2024-12-19 15:59:33 +0400

    fix: update incident severity after alert removal

--- keep/api/core/db.py
+++ keep/api/core/db.py
@@ -3561,6 +3561,12 @@ def get_future_incidents_by_incident_id(
 
     return query.all(), total_count
 
+def get_int_severity(input_severity: int | str) -> int:
+    if isinstance(input_severity, int):
+        return input_severity
+    else:
+        return IncidentSeverity(input_severity).order
+
 
 def get_alerts_data_for_incident(
     tenant_id: str,
@@ -3893,6 +3899,28 @@ def remove_alerts_to_incident_by_incident_id(
         )
         sources_existed = session.exec(existed_sources_query)
 
+        severity_field = get_json_extract_field(session, Alert.event, "severity")
+        # checking if severities of removed alerts are still presented in alerts
+        # which still assigned with the incident
+        updated_severities_query = (
+            select(severity_field)
+            .select_from(LastAlert)
+            .join(
+                LastAlertToIncident,
+                and_(
+                    LastAlert.tenant_id == LastAlertToIncident.tenant_id,
+                    LastAlert.fingerprint == LastAlertToIncident.fingerprint,
+                ),
+            )
+            .join(Alert, LastAlert.alert_id == Alert.id)
+            .filter(
+                LastAlertToIncident.deleted_at == NULL_FOR_DELETED_AT,
+                LastAlertToIncident.incident_id == incident_id,
+            )
+        )
+        updated_severities_result = session.exec(updated_severities_query)
+        updated_severities = [get_int_severity(severity) for severity in updated_severities_result]
+
         # Making lists of services and sources to remove from the incident
         services_to_remove = [
             service
@@ -3933,7 +3961,7 @@ def remove_alerts_to_incident_by_incident_id(
         ]
 
         incident.alerts_count -= alerts_data_for_incident["count"]
-        incident.severity = alerts_data_for_incident["max_severity"].order
+        incident.severity = max(updated_severities) if updated_severities else IncidentSeverity.LOW.order
         incident.start_time = started_at
         incident.last_seen_time = last_seen_at
 
