7882750b427ade64484105fc0e09bc355057f3f2
Vladimir Filonov <vladimir@filonov.dev>
2024-12-23 16:24:45 +0400

    Fix migrations order

--- keep/api/models/db/migrations/versions/2024-12-10-19-11_7297ae99cd21.py
+++ keep/api/models/db/migrations/versions/2024-12-10-19-11_7297ae99cd21.py
@@ -1,7 +1,7 @@
 """Add Rule.create_on
 
 Revision ID: 7297ae99cd21
-Revises: c6e5594c99f8
+Revises: 3d20d954e058
 Create Date: 2024-12-10 19:11:28.512095
 
 """
@@ -12,7 +12,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "7297ae99cd21"
-down_revision = "55cc64020f6d"
+down_revision = "3d20d954e058"
 branch_labels = None
 depends_on = None
 
