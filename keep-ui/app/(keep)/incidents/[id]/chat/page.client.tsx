721dcd017861ea6cc8bc3a768d34896a39c41a49
Tal Borenstein <tal@keephq.dev>
2025-01-06 11:44:25 +0200

    fix: fix

--- keep-ui/app/(keep)/incidents/[id]/chat/page.client.tsx
+++ keep-ui/app/(keep)/incidents/[id]/chat/page.client.tsx
@@ -2,10 +2,8 @@
 
 import { IncidentChat } from "./incident-chat";
 import { IncidentDto } from "@/entities/incidents/model";
-import { EmptyStateCard } from "@/shared/ui";
 import { useConfig } from "@/utils/hooks/useConfig";
 import { CopilotKit } from "@copilotkit/react-core";
-import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
 export function IncidentChatClientPage({
   incident,
