998b107eee60579c9bff0b10dbb7b9310f77d2bd
Matvey Kukuy <motakuk@gmail.com>
2025-01-12 14:54:25 +0100

    fix

--- keep/providers/mongodb_provider/mongodb_provider.py
+++ keep/providers/mongodb_provider/mongodb_provider.py
@@ -160,7 +160,7 @@ class MongodbProvider(BaseProvider):
         Returns:
             list | tuple: list of results or single result if single_row is True
         """
-        if type(query) == str:
+        if type(query) is str:
             query = json.loads(query)
             
         client = self.__generate_client()
