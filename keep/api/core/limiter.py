6e2c6d994e6d1ee1962af662fdfe89576e4efeb5
shahargl <shaharglazner@gmail.com>
2025-01-14 12:47:50 +0200

    fix: add limit log

--- keep/api/core/limiter.py
+++ keep/api/core/limiter.py
@@ -1,8 +1,13 @@
 # https://slowapi.readthedocs.io/en/latest/#fastapi
+import logging
+
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
 from keep.api.core.config import config
 
+logger = logging.getLogger(__name__)
 limiter_enabled = config("KEEP_USE_LIMITER", default="false", cast=bool)
+logger.warning(f"Rate limiter is {'enabled' if limiter_enabled else 'disabled'}")
+
 limiter = Limiter(key_func=get_remote_address, enabled=limiter_enabled)
