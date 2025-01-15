87ed24a7cf0a28aa73c474f6d100e2c4aa970976
Matvey Kukuy <motakuk@gmail.com>
2025-01-15 19:55:55 +0100

    feat: example of dumping workflows from s3

---
+++ keep/providers/s3_provider/s3_provider.py
@@ -0,0 +1,101 @@
+"""
+S3 Provider for querying S3 buckets.
+"""
+
+import base64
+import dataclasses
+from urllib.parse import urljoin
+
+import boto3
+import pydantic
+import requests
+
+from keep.contextmanager.contextmanager import ContextManager
+from keep.exceptions.provider_exception import ProviderException
+from keep.providers.base.base_provider import BaseProvider
+from keep.providers.models.provider_config import ProviderConfig, ProviderScope
+
+
+@pydantic.dataclasses.dataclass
+class S3ProviderAuthConfig:
+    access_key: str = dataclasses.field(
+        metadata={
+            "required": True,
+            "description": "S3 Access Token",
+            "sensitive": True,
+        },
+        default=None,
+    )
+
+    secret_access_key: str = dataclasses.field(
+        metadata={
+            "required": True,
+            "description": "S3 Secret Access Token",
+            "sensitive": True,
+        },
+        default=None,
+    )
+
+
+class S3Provider(BaseProvider):
+    PROVIDER_DISPLAY_NAME = "AWS S3"
+    PROVIDER_CATEGORY = ["Cloud Infrastructure"]
+
+    def dispose(self):
+        pass
+
+    def validate_config(self):
+        self.authentication_config = S3ProviderAuthConfig(
+            **self.config.authentication
+        )
+        if (
+            self.authentication_config.access_key is None
+            or self.authentication_config.secret_access_key is None
+        ):
+            raise ProviderException("Access key and secret access key are required")
+        boto3.client(
+            "s3",
+            aws_access_key_id=self.authentication_config.access_key,
+            aws_secret_access_key=self.authentication_config.secret_access_key,
+        )
+        # List all S3 buckets to validate the credentials
+        s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=self.authentication_config.access_key,
+            aws_secret_access_key=self.authentication_config.secret_access_key,
+        )
+        try:
+            s3_client.list_buckets()
+        except Exception as e:
+            raise ProviderException(f"Failed to list S3 buckets: {e}")
+
+    def _query(self, bucket: str, **kwargs: dict):
+        """
+        Query bucket for files. Downdload only yaml, json, xml and csv files.
+
+        Returns:
+            list[file_content]: results the list of downloaded files
+        """
+        s3_client = boto3.client(
+            "s3",
+            aws_access_key_id=self.authentication_config.access_key,
+            aws_secret_access_key=self.authentication_config.secret_access_key,
+        )
+        try:
+            response = s3_client.list_objects_v2(Bucket=bucket)
+        except Exception as e:
+            raise ProviderException(f"Failed to list objects in bucket: {e}")
+        files = []
+        for obj in response.get("Contents", []):
+            key = obj.get("Key")
+            valid_extensions = [".yaml", ".json", ".xml", ".csv", ".yml"]
+            if any(key.endswith(ext) for ext in valid_extensions):
+                try:
+                    response = s3_client.get_object(Bucket=bucket, Key=key)
+                    files.append(response.get("Body").read().decode('utf-8'))
+                    print(files)
+                except Exception as e:
+                    self.logger.exception(
+                        "Failed to download object from S3: %s", str(e)
+                    )
+        return files
