bff80a610314da626e9fdce544e327ef1b35dbe8
35C4n0r <jaykumar20march@gmail.com>
2025-02-06 18:31:52 +0530

    fix: mysql manual field
    Signed-off-by: 35C4n0r <jaykumar20march@gmail.com>

--- keep/api/models/db/migrations/versions/2025-01-26-15-25_8176d7153747.py
+++ keep/api/models/db/migrations/versions/2025-01-26-15-25_8176d7153747.py
@@ -8,8 +8,9 @@ Create Date: 2025-01-26 15:25:23.811890
 
 import sqlalchemy as sa
 from alembic import op
+from sqlalchemy.dialects import mysql
 
-# revision identifiers, used by Alembic.
+# revision identifiers, used by Alembic
 revision = "8176d7153747"
 down_revision = "e343054ae740"
 branch_labels = None
@@ -18,12 +19,14 @@ depends_on = None
 
 def upgrade() -> None:
     with op.batch_alter_table("topologyservice", schema=None) as batch_op:
-        batch_op.add_column(sa.Column("manual", sa.Boolean(), nullable=True))
-    # ### end Alembic commands ###
+        # Handle MySQL explicitly
+        if op.get_bind().dialect.name == "mysql":
+            batch_op.add_column(sa.Column("manual", mysql.TINYINT(1), nullable=True))
+        else:
+            # Use standard Boolean() for PostgreSQL & others
+            batch_op.add_column(sa.Column("manual", sa.Boolean(), nullable=True))
 
 
 def downgrade() -> None:
     with op.batch_alter_table("topologyservice", schema=None) as batch_op:
         batch_op.drop_column("manual")
-
-    # ### end Alembic commands ###
