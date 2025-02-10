9e3a5c462c9681b78ee265f5f6a468d469ead0ce
Kirill Chernakov <yakiryous@gmail.com>
2025-02-10 19:26:26 +0400

    fix: keep it simple, leave the check only in entrypoint.sh

--- keep-ui/entrypoint.sh
+++ keep-ui/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "Starting Nextjs [${API_URL}]"
 echo "AUTH_TYPE: ${AUTH_TYPE}"
-sh ./scripts/check-required-env-vars.sh
+if [ -n "${NEXTAUTH_SECRET}" ]; 
+then echo "NEXTAUTH_SECRET is set"; 
+else echo "‼️ WARNING: NEXTAUTH_SECRET is not set"; fi
 exec node server.js
--- keep-ui/scripts/check-required-env-vars.sh
+++
@@ -1,3 +0,0 @@
-if [ -n "${NEXTAUTH_SECRET}" ]; 
-then echo "NEXTAUTH_SECRET is set"; 
-else echo "‼️ WARNING: NEXTAUTH_SECRET is not set"; fi
