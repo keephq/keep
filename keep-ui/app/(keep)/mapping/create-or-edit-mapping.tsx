4d05ed564cf4063fc8ba3949e7209a0f72311e68
Tal Borenstein <tal@keephq.dev>
2025-02-13 00:05:43 +0200

    fix: fix

--- keep-ui/app/(keep)/mapping/create-or-edit-mapping.tsx
+++ keep-ui/app/(keep)/mapping/create-or-edit-mapping.tsx
@@ -362,7 +362,12 @@ export default function CreateOrEditMapping({ editRule, editCallback }: Props) {
           Cancel
         </Button>
 
-        <Button color="orange" size="xs" type="submit">
+        <Button
+          color="orange"
+          size="xs"
+          type="submit"
+          disabled={!mapName || !attributeGroups.flat().length}
+        >
           {editMode ? "Update" : "Create"}
         </Button>
       </div>
