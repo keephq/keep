3eb561e4dfeb20693145c759045a7eb3f05a3c8a
Kirill Chernakov <yakiryous@gmail.com>
2025-02-13 13:07:48 +0400

    fix: opengraph image and description

--- keep-ui/app/(health)/health/page.tsx
+++ keep-ui/app/(health)/health/page.tsx
@@ -1,8 +1,10 @@
+import { Metadata } from "next";
 import ProviderHealthPage from "./check";
 
-export const metadata = {
-  title: "Keep",
-  description: "Easily check the configuration quality of your observability tools without the need to sign up.",
+export const metadata: Metadata = {
+  title: "Keep – Check your alerts quality",
+  description:
+    "Easily check the configuration quality of your observability tools such as Datadog, Grafana, Prometheus, and more without the need to sign up.",
 };
 
 export default ProviderHealthPage;
