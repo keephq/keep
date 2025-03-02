eb103c987b1e5731e6ddcc9a6ad6ba635853aa8e
shahargl <shaharglazner@gmail.com>
2025-03-02 13:02:25 +0200

    feat: wip

--- keep-ui/next.config.js
+++ keep-ui/next.config.js
@@ -102,7 +102,7 @@ const nextConfig = {
       : [
           {
             source: "/",
-            destination: "/incidents",
+            destination: "/overview/",
             permanent: process.env.ENV === "production",
           },
           ...workflowRawYamlRedirects,
