c58320ddf2655b4611e3dc71ab531a10c11ba5f5
shahargl <shaharglazner@gmail.com>
2025-03-05 14:16:54 +0200

    feat: mail and mapping improvements

--- keep/api/bl/enrichments_bl.py
+++ keep/api/bl/enrichments_bl.py
@@ -424,7 +424,9 @@ class EnrichmentsBl:
                             if not is_matcher:
                                 # If the key has . (dot) in it, it'll be added as is while it needs to be nested.
                                 # @tb: fix when somebody will be complaining about this.
-                                enrichments[key.strip()] = value.strip()
+                                if isinstance(value, str):
+                                    value = value.strip()
+                                enrichments[key.strip()] = value
                     break
 
         if enrichments:
@@ -432,7 +434,9 @@ class EnrichmentsBl:
             for key, value in enrichments.items():
                 # It's not relevant to enrich if the value if empty
                 if value is not None:
-                    setattr(alert, key.strip(), value.strip())
+                    if isinstance(value, str):
+                        value = value.strip()
+                    setattr(alert, key.strip(), value)
 
             # Save the enrichments to the database
             # SHAHAR: since when running this enrich_alert, the alert is not in elastic yet (its indexed after),
