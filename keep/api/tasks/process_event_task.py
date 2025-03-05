7ab4271d803883515842bcfb6574e1d4e41f6ff0
Shahar Glazner <shaharglazner@gmail.com>
2025-03-05 15:29:19 +0200

    fix(api): strip (#3873)

--- keep/api/tasks/process_event_task.py
+++ keep/api/tasks/process_event_task.py
@@ -247,7 +247,9 @@ def __save_to_db(
                 for enrichment in alert_enrichment.enrichments:
                     # set the enrichment
                     value = alert_enrichment.enrichments[enrichment]
-                    setattr(formatted_event, enrichment, value.strip())
+                    if isinstance(value, str):
+                        value = value.strip()
+                    setattr(formatted_event, enrichment, value)
             enriched_formatted_events.append(formatted_event)
         session.commit()
 
