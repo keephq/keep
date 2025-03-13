12e4a77de3aeea04ac1faf1126acdbfa8388139d
Shahar Glazner <shaharglazner@gmail.com>
2025-03-13 12:50:36 +0200

    feat: processing event error UX (#3896)

--- keep/identitymanager/identity_managers/noauth/noauth_identitymanager.py
+++ keep/identitymanager/identity_managers/noauth/noauth_identitymanager.py
@@ -1,3 +1,4 @@
+from keep.api.core.db import create_single_tenant_for_e2e
 from keep.api.models.user import User
 from keep.contextmanager.contextmanager import ContextManager
 from keep.identitymanager.authverifierbase import AuthVerifierBase
@@ -11,6 +12,20 @@ class NoAuthIdentityManager(BaseIdentityManager):
     def __init__(self, tenant_id, context_manager: ContextManager, **kwargs):
         super().__init__(tenant_id, context_manager, **kwargs)
         self.logger.info("DB Identity Manager initialized")
+
+    def on_start(self, app) -> None:
+        """
+        Initialize the identity manager.
+        """
+
+        # create tenant, for e2e tests
+        @app.post("/tenant")
+        def tenant(body: dict):
+            tenant_id = body.get("tenant_id")
+            create_single_tenant_for_e2e(tenant_id)
+            return {"message": "Tenant created"}
+
+        self.logger.info("Added tenant endpoint")
 
     def get_users(self) -> list[User]:
         return []
