8a08777ad89bd00f2ea1b65b8c01aecdf3ad12a0
Vladimir Filonov <vladimir@filonov.dev>
2025-03-13 13:14:03 +0400

    Remove unused imports in Alembic migration script
    Cleaned up unused imports from the migration file to improve code readability and reduce unnecessary dependencies. This change has no impact on functionality but simplifies the codebase.

--- keep/api/models/db/migrations/versions/2025-03-13-12-53_72611d1dadf2.py
+++ keep/api/models/db/migrations/versions/2025-03-13-12-53_72611d1dadf2.py
@@ -6,11 +6,6 @@ Create Date: 2025-03-13 12:53:16.705110
 
 """
 
-import sqlalchemy as sa
-import sqlalchemy_utils
-import sqlmodel
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision = "72611d1dadf2"
 down_revision = ("9f11356d8ed9", "aaec81b991bd")
