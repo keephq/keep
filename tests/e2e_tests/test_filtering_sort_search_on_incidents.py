0e846d9113e6519a0f93175ef6d1f75d049456a8
Ihor Panasiuk <igorskynet13@gmail.com>
2025-03-16 16:55:30 +0100

    Update test_filtering_sort_search_on_incidents.py

--- tests/e2e_tests/test_filtering_sort_search_on_incidents.py
+++ tests/e2e_tests/test_filtering_sort_search_on_incidents.py
@@ -1,14 +1,8 @@
 import re
-import time
-from datetime import datetime, timedelta
-
 import pytest
 from playwright.sync_api import expect, Page
 from tests.e2e_tests.incidents_alerts_setup import (
-    query_alerts,
     setup_incidents_alerts,
-    upload_alert,
-    create_fake_alert,
 )
 from tests.e2e_tests.utils import init_e2e_test
 
@@ -187,14 +181,17 @@ def test_adding_custom_facet_for_alert_field(browser, setup_test_data):
     current_incidents = setup_test_data["incidents"]
     incidents_alert = setup_test_data["incidents_alert"]
     init_test(browser, current_incidents)
+
+    # region Add custom facet
     browser.locator("button", has_text="Add Facet").click()
 
     browser.locator("input[placeholder='Enter facet name']").fill(facet_name)
     browser.locator("input[placeholder*='Search columns']").fill(facet_property_path)
     browser.locator("button", has_text=facet_property_path).click()
     browser.locator("button[data-testid='create-facet-btn']").click()
+    # endregion
 
-    ###
+    # region Verify that facet is displayed and has correct facet values with counters
     counters_dict = {}
     expect(
         browser.locator("[data-testid='facet']", has_text=facet_name)
@@ -225,8 +222,9 @@ def test_adding_custom_facet_for_alert_field(browser, setup_test_data):
         expect(
             facet_value_locator.locator("[data-testid='facet-value-count']")
         ).to_contain_text(str(count))
-    ###
+    # endregion
 
+    # region Select facet value and verify that only incidents with selected value are displayed
     option = facet_locator.locator("[data-testid='facet-value']", has_text=value)
     option.hover()
     option.locator("button", has_text="Only").click()
@@ -254,101 +252,4 @@ def test_adding_custom_facet_for_alert_field(browser, setup_test_data):
     expect(
         browser.locator("[data-testid='facet']", has_text=facet_name)
     ).not_to_be_visible()
-
-
-# search_by_cel_tescases = {
-#     "contains for nested property": {
-#         "cel_query": "labels.service.contains('java-otel')",
-#         "predicate": lambda alert: "java-otel"
-#         in alert.get("labels", {}).get("service", ""),
-#         "alert_property_name": "name",
-#     },
-#     "using enriched field": {
-#         "cel_query": "status == 'enriched status'",
-#         "predicate": lambda alert: "Enriched" in alert["name"],
-#         "alert_property_name": "name",
-#     },
-#     "date comparison greater than or equal": {
-#         "cel_query": f"dateForTests >= '{(datetime(2025, 2, 10, 10) + timedelta(days=-14)).isoformat()}'",
-#         "predicate": lambda alert: alert.get("dateForTests")
-#         and datetime.fromisoformat(alert.get("dateForTests"))
-#         >= (datetime(2025, 2, 10, 10) + timedelta(days=-14)),
-#         "alert_property_name": "name",
-#     },
-# }
-
-
-# @pytest.mark.parametrize("search_test_case", search_by_cel_tescases.keys())
-# def test_search_by_cel(browser, search_test_case, setup_test_data):
-#     test_case = search_by_cel_tescases[search_test_case]
-#     cel_query = test_case["cel_query"]
-#     predicate = test_case["predicate"]
-#     alert_property_name = test_case["alert_property_name"]
-#     current_alerts = setup_test_data
-#     init_test(browser, current_alerts)
-#     search_input = browser.locator(
-#         "textarea[placeholder*='Use CEL to filter your alerts']"
-#     )
-#     expect(search_input).to_be_visible()
-#     browser.wait_for_timeout(1000)
-#     search_input.fill(cel_query)
-#     search_input.press("Enter")
-
-#     assert_incidents_by_column(
-#         browser,
-#         current_alerts,
-#         predicate,
-#         alert_property_name,
-#         None,
-#     )
-
-
-# sort_tescases = {
-#     "sort by lastReceived asc/dsc": {
-#         "column_name": "Last Received",
-#         "column_id": "lastReceived",
-#         "sort_callback": lambda alert: alert["lastReceived"],
-#     },
-#     "sort by description asc/dsc": {
-#         "column_name": "description",
-#         "column_id": "description",
-#         "sort_callback": lambda alert: alert["description"],
-#     },
-# }
-
-
-# @pytest.mark.parametrize("sort_test_case", sort_tescases.keys())
-# def test_sort_asc_dsc(browser, sort_test_case, setup_test_data):
-#     test_case = sort_tescases[sort_test_case]
-#     coumn_name = test_case["column_name"]
-#     column_id = test_case["column_id"]
-#     sort_callback = test_case["sort_callback"]
-#     current_alerts = setup_test_data
-#     init_test(browser, current_alerts)
-#     filtered_alerts = [
-#         alert for alert in current_alerts if alert["providerType"] == "prometheus"
-#     ]
-#     select_one_facet_option(browser, "source", "prometheus")
-#     expect(
-#         browser.locator("table[data-testid='incidents-table'] tbody tr")
-#     ).to_have_count(len(filtered_alerts))
-
-#     for sort_direction_title in ["Sort ascending", "Sort descending"]:
-#         sorted_alerts = sorted(filtered_alerts, key=sort_callback)
-
-#         if sort_direction_title == "Sort descending":
-#             sorted_alerts = list(reversed(sorted_alerts))
-
-#         column_header_locator = browser.locator(
-#             f"table[data-testid='incidents-table'] thead th [data-testid='header-cell-{column_id}']",
-#             has_text=coumn_name,
-#         )
-#         expect(column_header_locator).to_be_visible()
-#         column_header_locator.click()
-#         rows = browser.locator("table[data-testid='incidents-table'] tbody tr")
-
-#         for index, alert in enumerate(sorted_alerts):
-#             row_locator = rows.nth(index)
-#             # 3 is index of "name" column
-#             column_locator = row_locator.locator("td").nth(3)
-#             expect(column_locator).to_have_text(alert["name"])
+    # endregion
