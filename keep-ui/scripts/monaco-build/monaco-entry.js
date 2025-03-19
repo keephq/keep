8e1757946202c9b71f51cf52033b80aa60150962
Kirill Chernakov <yakiryous@gmail.com>
2025-03-19 16:16:40 +0400

    prebuild monaco files with webpack in a separate folder

---
+++ keep-ui/scripts/monaco-build/monaco-entry.js
@@ -0,0 +1,5 @@
+// Export what you need from Monaco
+import * as monaco from "monaco-editor/esm/vs/editor/editor.main";
+
+// Export it to make it available to Next.js app
+window.monaco = monaco;
---
+++ keep-ui/scripts/monaco-build/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production", // Use production for smaller bundles
+  entry: {
+    "editor.worker": "monaco-editor/esm/vs/editor/editor.worker.js",
+    "json.worker": "monaco-editor/esm/vs/language/json/json.worker",
+    "css.worker": "monaco-editor/esm/vs/language/css/css.worker",
+    "html.worker": "monaco-editor/esm/vs/language/html/html.worker",
+    "ts.worker": "monaco-editor/esm/vs/language/typescript/ts.worker",
+    monaco: "./monaco-entry.js", // Main Monaco bundle
+  },
+  output: {
+    globalObject: "self",
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./monaco-editor"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ttf$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  cache: {
+    type: "filesystem",
+    cacheDirectory: path.resolve(__dirname, ".webpack_cache"),
+  },
+};
