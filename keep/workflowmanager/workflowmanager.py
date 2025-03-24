4a66dd232ee8423070ac8ac3e55aac368dfd1b79
shahargl <shaharglazner@gmail.com>
2025-03-24 16:50:37 +0200

    feat: opsgenie close-alert

--- keep/workflowmanager/workflowmanager.py
+++ keep/workflowmanager/workflowmanager.py
@@ -341,9 +341,9 @@ class WorkflowManager:
                     # Lastly, if the workflow should run, add it to the scheduler
                     self.logger.info("Adding workflow to run")
 
-                    # SHAHAR: this is
+                    # SHAHAR: TODO - finish redis implementation
                     # if REDIS is enabled, add the workflow to the queue
-                    # TODO: redis
+
                     """
                     if os.environ.get("REDIS", "false").lower() == "true":
                         try:
