d4e77de9e7d36b90e0cb8d0bfb9cf4dff02e2068
Kirill Chernakov <yakiryous@gmail.com>
2025-03-31 16:05:40 +0400

    chore: deprecation warning

--- keep-ui/entities/alerts/lib/getTabsFromPreset.ts
+++ keep-ui/entities/alerts/lib/getTabsFromPreset.ts
@@ -1,5 +1,10 @@
-import {Preset} from "@/entities/presets/model/types";
+import { Preset } from "@/entities/presets/model/types";
 
+/**
+ * @param preset
+ *
+ * @deprecated preset tabs are removed, this function shouldn't be used anymore
+ */
 export function getTabsFromPreset(preset: Preset): any[] {
   const tabsOption = preset.options.find(
     (option) => option.label.toLowerCase() === "tabs"
