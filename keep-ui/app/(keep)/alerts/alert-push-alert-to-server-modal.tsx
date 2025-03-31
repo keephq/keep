c61c0b31e0120223347afe32d0f6ce483439df84
Matvey Kukuy <motakuk@gmail.com>
2025-03-31 09:26:40 +0300

    fix: more tolerate docs checker

--- keep-ui/app/(keep)/alerts/alert-push-alert-to-server-modal.tsx
+++ keep-ui/app/(keep)/alerts/alert-push-alert-to-server-modal.tsx
@@ -83,11 +83,27 @@ const PushAlertToServerModal = ({
         data.alertJson = JSON.parse(data.alertJson);
       }
 
+      var random_fingerprint = Math.random().toString(36);
+
       const response = await api.post(
-        `/alerts/event/${data.source.type}`,
+        `/alerts/event/${data.source.type}?fingerprint=${random_fingerprint}`,
         data.alertJson
       );
 
+      await new Promise(() => {
+        setTimeout(async () => {
+          await api.post(
+            `/alerts/enrich/`,
+            {
+              fingerprint: random_fingerprint,
+              enrichments: {
+                "dont_show_in_a_linked_providers": true,
+              }
+            }
+          );
+        }, 5000);
+      });
+      
       mutateAlerts();
       presetsMutator();
       handleClose();
