d67ca15c57c253d653281aab8b05af73b749c8b7
Ihor Panasiuk <igorskynet13@gmail.com>
2025-04-03 11:16:34 +0200

    feat: integrate preset alerts count into PresetGridItem component

--- keep-ui/app/(keep)/dashboard/widget-types/preset/preset-grid-item.tsx
+++ keep-ui/app/(keep)/dashboard/widget-types/preset/preset-grid-item.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { WidgetData, WidgetType } from "../../types";
+import { usePresetAlertsCount } from "@/features/presets/custom-preset-links";
 
 interface GridItemProps {
   item: WidgetData;
 }
 
 const PresetGridItem: React.FC<GridItemProps> = ({ item }) => {
+  const { totalCount: presetAlertsCount } = usePresetAlertsCount(
+    item.preset?.options.find((option) => option.label === "CEL")?.value || "",
+    !!item.preset?.counter_shows_firing_only
+  );
+
   const getColor = () => {
     let color = "#000000";
     if (
@@ -14,22 +20,23 @@ const PresetGridItem: React.FC<GridItemProps> = ({ item }) => {
       item.preset
     ) {
       for (let i = item.thresholds.length - 1; i >= 0; i--) {
-        if (
-          item.preset &&
-          item.preset.alerts_count >= item.thresholds[i].value
-        ) {
+        if (item.preset && presetAlertsCount >= item.thresholds[i].value) {
           color = item.thresholds[i].color;
           break;
         }
       }
     }
+    console.log("Ihor", {
+      item,
+      presetAlertsCount,
+    });
     return color;
   };
 
   return (
     <div className="flex-1 h-4/5 flex items-center justify-center grid-item__widget">
       <div className="text-4xl font-bold" style={{ color: getColor() }}>
-        {item.preset?.alerts_count}
+        {presetAlertsCount}
       </div>
     </div>
   );
