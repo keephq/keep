8c2b41f7a29c11c34c5e3f507bf7092f074c77ff
Kirill Chernakov <yakiryous@gmail.com>
2025-04-10 09:27:51 +0400

    feat: validate mustache variable in workflow YAML (#4485)

--- keep-ui/entities/workflows/lib/validation.ts
+++ keep-ui/entities/workflows/lib/validation.ts
@@ -1,9 +1,10 @@
 import { Provider } from "@/shared/api/providers";
 import { Definition, V2Step } from "../model/types";
 import { getWithParams } from "./parser";
+import { YamlStepOrAction, YamlWorkflowDefinition } from "../model/yaml.types";
 
 export type ValidationResult = [string, string];
-export type ValidationError = [string, "error" | "warning"];
+export type ValidationError = [string, "error" | "warning" | "info"];
 /**
  * Extracts the trimmed value from mustache syntax by removing curly brackets.
  *
@@ -17,6 +18,141 @@ function extractMustacheValue(mustacheString: string): string {
   // Return the captured group if found, otherwise return empty string
   return match ? match[1] : "";
 }
+
+export const validateMustacheVariableNameForYAML = (
+  variableName: string,
+  currentStep: YamlStepOrAction,
+  currentStepType: "step" | "action",
+  definition: YamlWorkflowDefinition,
+  secrets: Record<string, string>,
+  providers: Provider[] | null,
+  installedProviders: Provider[] | null
+) => {
+  const cleanedVariableName = extractMustacheValue(variableName);
+  if (!cleanedVariableName) {
+    return ["Empty mustache variable.", "warning"];
+  }
+  const parts = cleanedVariableName.split(".");
+  if (!parts.every((part) => part.length > 0)) {
+    return [
+      `Variable: ${cleanedVariableName} - path parts cannot be empty.`,
+      "warning",
+    ];
+  }
+  if (parts[0] === "providers") {
+    const providerName = parts[1];
+    if (!providerName) {
+      return [
+        `Variable: ${cleanedVariableName} - To access a provider, you need to specify the provider name.`,
+        "warning",
+      ];
+    }
+    if (!providers || !installedProviders) {
+      // Skip validation if providers or installedProviders are not available
+      return null;
+    }
+    const isDefault = providerName.startsWith("default-");
+    if (isDefault) {
+      const providerType = isDefault ? providerName.split("-")[1] : null;
+      const provider = providers.find((p) => p.type === providerType);
+      if (!provider) {
+        return [
+          `Variable: ${cleanedVariableName} - Provider "${providerName}" not found.`,
+          "warning",
+        ];
+      }
+    } else {
+      const provider = installedProviders.find((p) => p.id === providerName);
+      if (!provider) {
+        return [
+          `Variable: ${cleanedVariableName} - Provider "${providerName}" is not installed.`,
+          "warning",
+        ];
+      }
+    }
+    return null;
+  }
+  if (parts[0] === "alert") {
+    // todo: validate alert properties
+    return null;
+  }
+  if (parts[0] === "incident") {
+    // todo: validate incident properties
+    return null;
+  }
+  if (parts[0] === "secrets") {
+    const secretName = parts[1];
+    if (!secretName) {
+      return [
+        `Variable: ${cleanedVariableName} - To access a secret, you need to specify the secret name.`,
+        "warning",
+      ];
+    }
+    if (!secrets[secretName]) {
+      return [
+        `Variable: ${cleanedVariableName} - Secret "${secretName}" not found.`,
+        "error",
+      ];
+    }
+    return null;
+  }
+  if (parts[0] === "steps") {
+    const stepName = parts[1];
+    if (!stepName) {
+      return [
+        `Variable: ${cleanedVariableName} - To access the results of a step, you need to specify the step name.`,
+        "warning",
+      ];
+    }
+    // todo: check if
+    // - the step exists
+    // - it's not the current step (can't access own results, only enrich_alert and enrich_incident can access their own results)
+    // - it's above the current step
+    // - if it's a step it cannot access actions since they run after steps
+    const step = definition.steps.find((s) => s.name === stepName);
+    const stepIndex = definition.steps.findIndex((s) => s.name === stepName);
+    const currentStepIndex =
+      currentStepType === "step"
+        ? definition.steps.findIndex((s) => s.name === currentStep.name)
+        : -1;
+    if (!step) {
+      return [
+        `Variable: ${cleanedVariableName} - a "${stepName}" step doesn't exist.`,
+        "error",
+      ];
+    }
+    const isCurrentStep = step.name === currentStep.name;
+    if (isCurrentStep) {
+      return [
+        `Variable: ${cleanedVariableName} - You can't access the results of the current step.`,
+        "error",
+      ];
+    }
+    if (currentStepIndex !== -1 && stepIndex > currentStepIndex) {
+      return [
+        `Variable: ${cleanedVariableName} - You can't access the results of a step that appears after the current step.`,
+        "error",
+      ];
+    }
+
+    if (!definition.steps?.some((step) => step.name === stepName)) {
+      return [
+        `Variable: ${cleanedVariableName} - a "${stepName}" step that doesn't exist.`,
+        "error",
+      ];
+    }
+    if (parts[2] === "results") {
+      // todo: validate results properties
+      return null;
+    } else {
+      return [
+        `Variable: ${cleanedVariableName} - To access the results of a step, use "results" as suffix.`,
+        "warning",
+      ];
+    }
+  }
+  return null;
+};
 
 export const validateMustacheVariableName = (
   variableName: string,
@@ -270,7 +406,7 @@ export function validateStepPure(
       secrets
     );
     variableErrors.forEach((error) => {
-      validationErrors.push([error, "warning"]);
+      validationErrors.push([error, "error"]);
     });
   }
   if (step.componentType === "task" && step.properties.with?.enrich_alert) {
@@ -282,7 +418,7 @@ export function validateStepPure(
       secrets
     );
     variableErrors.forEach((error) => {
-      validationErrors.push([error, "warning"]);
+      validationErrors.push([error, "error"]);
     });
   }
   if (step.componentType === "task" && step.properties.with?.enrich_incident) {
@@ -296,7 +432,7 @@ export function validateStepPure(
       secrets
     );
     variableErrors.forEach((error) => {
-      validationErrors.push([error, "warning"]);
+      validationErrors.push([error, "error"]);
     });
   }
   if (step.componentType === "switch") {
@@ -329,7 +465,7 @@ export function validateStepPure(
         secrets
       );
       variableErrorsCompareTo.forEach((error) => {
-        validationErrors.push([error, "warning"]);
+        validationErrors.push([error, "error"]);
       });
     }
     if (step.type === "condition-assert") {
@@ -343,7 +479,7 @@ export function validateStepPure(
         secrets
       );
       variableErrors.forEach((error) => {
-        validationErrors.push([error, "warning"]);
+        validationErrors.push([error, "error"]);
       });
     }
     const branches = step.branches || {
@@ -389,7 +525,7 @@ export function validateStepPure(
           secrets
         );
         variableErrors.forEach((error) => {
-          validationErrors.push([error, "warning"]);
+          validationErrors.push([error, "error"]);
         });
       }
     }
@@ -405,7 +541,7 @@ export function validateStepPure(
       secrets
     );
     variableErrors.forEach((error) => {
-      validationErrors.push([error, "warning"]);
+      validationErrors.push([error, "error"]);
     });
   }
   return validationErrors;
