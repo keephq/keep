0ef3f60269546f89aff3e94c46bb4b20d32a4a23
Ihor Panasiuk <igorskynet13@gmail.com>
2025-04-15 12:43:27 +0200

    revert

--- keep-ui/shared/lib/server/getConfig.ts
+++ keep-ui/shared/lib/server/getConfig.ts
@@ -67,14 +67,5 @@ export function getConfig(): InternalConfig {
       process.env.KEEP_CONTACT_US_URL || "https://slack.keephq.dev/",
     KEEP_HIDE_SENSITIVE_FIELDS:
       process.env.KEEP_HIDE_SENSITIVE_FIELDS === "true",
-    DISABLE_DEDUPLICATION: process.env.DISABLE_DEDUPLICATION === "true",
-    DISABLE_CORRELATION: process.env.DISABLE_CORRELATION === "true",
-    DISABLE_WORKFLOWS: process.env.DISABLE_WORKFLOWS === "true",
-    DISABLE_SERVICE_TOPOLOGY: process.env.DISABLE_SERVICE_TOPOLOGY === "true",
-    DISABLE_MAPPING: process.env.DISABLE_MAPPING === "true",
-    DISABLE_EXTRACTION: process.env.DISABLE_EXTRACTION === "true",
-    DISABLE_MAINTENANCE_WINDOW:
-      process.env.DISABLE_MAINTENANCE_WINDOW === "true",
-    DISABLE_AI_PLUGINS: process.env.DISABLE_AI_PLUGINS === "true",
   };
 }
