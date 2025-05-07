000764eb2c718dba268921cc827da14f91645769
Ihor Panasiuk <igorskynet13@gmail.com>
2025-05-07 20:11:41 +0200

    feat: refactor JSON array facet subquery for improved clarity and performance

--- keep/api/core/facets.py
+++ keep/api/core/facets.py
@@ -1,6 +1,15 @@
 import json
 import logging
-from sqlalchemy import String, func, literal, literal_column, select, text, union_all
+from sqlalchemy import (
+    Column,
+    String,
+    func,
+    literal,
+    literal_column,
+    select,
+    text,
+    union_all,
+)
 from sqlalchemy.exc import OperationalError
 from keep.api.core.cel_to_sql.ast_nodes import DataType
 from keep.api.core.cel_to_sql.properties_metadata import (
@@ -111,46 +120,19 @@ def build_facet_subquery_for_json_array(
             facet_filter,
         )
     elif engine.dialect.name == "mysql":
-        # Create a list of Select statements
-        select_statements = [select(literal(i).label("index")) for i in range(10)]
-
-        # Combine them using the standalone union_all function
-        iterator_query = union_all(*select_statements).alias("iterator_query")
-
-        stra = str(
-            iterator_query.compile(
-                compile_kwargs={"literal_binds": True}, dialect=engine.dialect
-            )
-        )
-
-        txtsss = facets_cel_to_sql_instance.json_extract_as_text(
-            column_name,
-            [f"CONCAT('$[', {iterator_query.c.index.name}, ']')"],
-        )
-
-        foo = (
+        base_query_cte = base_query.limit(1_000_000).cte(f"{column_name}_base_query")
+        json_table_join = func.json_table(
+            literal_column(column_name), Column("value", String(127))
+        ).table_valued("value")
+        return (
             select(
-                func.distinct(base_query.c.entity_id),
-                func.json_unquote(
-                    func.json_extract(
-                        literal_column(column_name),
-                        func.concat("$[", iterator_query.c.index, "]"),
-                    )
-                ).label("facet_value"),
+                func.distinct(base_query_cte.c.entity_id),
+                json_table_join.c.value.label("facet_value"),
             )
-            .select_from(base_query)
-            .join(
-                iterator_query,
-                iterator_query.c.index < func.json_length(literal_column(column_name)),
-            )
+            .select_from(base_query_cte)
+            .join(json_table_join, text("TRUE"))
             .filter(text(facets_cel_to_sql_instance.convert_to_sql_str(facet_filter)))
         )
-
-        stra = str(
-            foo.compile(compile_kwargs={"literal_binds": True}, dialect=engine.dialect)
-        )
-
-        return foo
 
 
 def build_facets_data_query(
@@ -185,7 +167,8 @@ def build_facets_data_query(
     new_fields_config = facet_selects_metadata["new_fields_config"]
     facets_properties_metadata = PropertiesMetadata(new_fields_config)
     facets_cel_to_sql_instance = get_cel_to_sql_provider(facets_properties_metadata)
-    base_query = base_query.cte("base_query")
+
+    base_query_common = base_query.cte("base_query")
 
     for facet in facets:
         metadata = facets_properties_metadata.get_property_metadata_for_str(
@@ -215,7 +198,7 @@ def build_facets_data_query(
             )
         else:
             facet_source_subquery = build_facet_subquery_for_column(
-                base_query,
+                base_query_common,
                 facets_cel_to_sql_instance,
                 facet,
                 column_name,
@@ -287,11 +270,11 @@ def get_facet_options(
                     properties_metadata=properties_metadata,
                     facet_options_query=facet_options_query,
                 )
-                strq = str(
-                    db_query.compile(
-                        compile_kwargs={"literal_binds": True}, dialect=engine.dialect
-                    )
-                )
+                # strq = str(
+                #     db_query.compile(
+                #         compile_kwargs={"literal_binds": True}, dialect=engine.dialect
+                #     )
+                # )
 
                 data = session.exec(db_query).all()
             except OperationalError as e:
