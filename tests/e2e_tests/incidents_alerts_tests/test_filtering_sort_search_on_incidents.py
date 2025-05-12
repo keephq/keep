df88bc793c08061a7c46ded318adb980020b4873
Ihor Panasiuk <igorskynet13@gmail.com>
2025-05-12 18:12:33 +0200

    test: enhance incident tests with additional status handling and initial loading verification

--- tests/e2e_tests/incidents_alerts_tests/test_filtering_sort_search_on_incidents.py
+++ tests/e2e_tests/incidents_alerts_tests/test_filtering_sort_search_on_incidents.py
@@ -31,9 +31,12 @@ def init_test(browser: Page, incidents, max_retries=3):
                 raise e
 
     browser.wait_for_selector("[data-testid='facet-value']")
+    browser.wait_for_selector("table[data-testid='incidents-table']")
 
+
+def display_all_available_incidents(browser: Page):
     # to select status filters that are initially not selected
-    for status in ["resolved", "deleted"]:
+    for status in ["resolved", "deleted", "acknowledged"]:
         facet_option = (
             browser.locator("[data-testid='facet']", has_text="Status")
             .locator("[data-testid='facet-value']", has_text=status)
@@ -126,6 +129,36 @@ def setup_test_data():
     yield setup_incidents_alerts()
 
 
+def test_initial_loading(browser, setup_test_data):
+    try:
+        incidents = setup_test_data["incidents"]
+        # verify intial loading of incidents page
+        init_test(browser, incidents)
+
+        assert_incidents_by_column(
+            browser,
+            incidents,
+            lambda alert: (alert["status"] in ["firing"]),
+            "status",
+            None,
+        )
+        # select all statuses
+        display_all_available_incidents(browser)
+
+        # verify what happens if "reset" in facets panel is clicked
+        browser.locator("#incidents-facets button", has_text="reset").click()
+        assert_incidents_by_column(
+            browser,
+            incidents,
+            lambda alert: (alert["status"] in ["firing"]),
+            "status",
+            None,
+        )
+    except Exception:
+        save_failure_artifacts(browser, log_entries=[])
+        raise
+
+
 facet_test_cases = {
     "severity": {
         "incident_property_name": "severity",
@@ -154,6 +187,7 @@ def test_filter_by_static_facet(browser, facet_test_case, setup_test_data):
         incidents = setup_test_data["incidents"]
 
         init_test(browser, incidents)
+        display_all_available_incidents(browser)
 
         assert_facet(browser, facet_name, incidents, incident_property_name)
 
@@ -188,6 +222,7 @@ def test_adding_custom_facet_for_alert_field(browser, setup_test_data):
         current_incidents = setup_test_data["incidents"]
         incidents_alert = setup_test_data["incidents_alert"]
         init_test(browser, current_incidents)
+        display_all_available_incidents(browser)
 
         # region Add custom facet
         browser.locator("button", has_text="Add Facet").click()
@@ -291,6 +326,8 @@ def test_sort_asc_dsc(
         current_incidents = setup_test_data["incidents"]
         name_column_index = 3
         init_test(browser, current_incidents)
+        display_all_available_incidents(browser)
+
         try:
             expect(
                 browser.locator("table[data-testid='incidents-table'] tbody tr")
