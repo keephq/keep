7ad6c4623143672a6f7d125f2d1a954c2a44130d
Kirill Chernakov <yakiryous@gmail.com>
2025-05-21 13:45:30 +0400

    fix: no workflows found for execution from alert or incident (#4858)

--- keep-ui/widgets/alerts-table/ui/alert-table-server-side.tsx
+++ keep-ui/widgets/alerts-table/ui/alert-table-server-side.tsx
@@ -533,7 +533,10 @@ export function AlertTableServerSide({
       }
     }
     return (
-      <Table className="[&>table]:table-fixed [&>table]:w-full">
+      <Table
+        className="[&>table]:table-fixed [&>table]:w-full"
+        data-testid="alerts-table"
+      >
         <AlertsTableHeaders
           columns={columns}
           table={table}
