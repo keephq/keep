5472551786d9e0b91154e8a25cf8bd855124ce00
Ihor Panasiuk <igorskynet13@gmail.com>
2025-05-22 20:51:30 +0200

    refactor: remove unnecessary database query string compilation in get_facet_options

--- keep/api/core/facets.py
+++ keep/api/core/facets.py
@@ -165,13 +165,6 @@ def get_facet_options(
                     facet_options_query=facet_options_query,
                 )
 
-                db_query_str = str(
-                    db_query.compile(
-                        compile_kwargs={"literal_binds": True},
-                        dialect=engine.dialect,
-                    ),
-                )
-
                 data = session.exec(db_query).all()
             except OperationalError as e:
                 logger.warning(
