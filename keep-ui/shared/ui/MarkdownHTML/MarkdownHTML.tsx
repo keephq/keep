f60606c79c13e467c5e528e9dd4e8b3317fe3bff
Kirill Chernakov <yakiryous@gmail.com>
2025-05-28 16:50:27 +0400

    tests: check if markdownhtml is to blame

--- keep-ui/shared/ui/MarkdownHTML/MarkdownHTML.tsx
+++ keep-ui/shared/ui/MarkdownHTML/MarkdownHTML.tsx
@@ -1,18 +1,23 @@
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import remarkRehype from "remark-rehype";
-import rehypeRaw from "rehype-raw";
-import rehypeSanitize from "rehype-sanitize";
+// import Markdown from "react-markdown";
+// import remarkGfm from "remark-gfm";
+// import remarkRehype from "remark-rehype";
+// import rehypeRaw from "rehype-raw";
+// import rehypeSanitize from "rehype-sanitize";
 
-// Only this component should be used to render markdown or HTML,
-// it sanitizes the HTML and allows for the use of raw HTML.
+// // Only this component should be used to render markdown or HTML,
+// // it sanitizes the HTML and allows for the use of raw HTML.
+// export const MarkdownHTML = ({ children }: { children: string }) => {
+//   return (
+//     <Markdown
+//       remarkPlugins={[remarkGfm, remarkRehype]}
+//       rehypePlugins={[rehypeRaw, rehypeSanitize]}
+//     >
+//       {children}
+//     </Markdown>
+//   );
+// };
+
+// mock implementation for now, testing the e2e tests
 export const MarkdownHTML = ({ children }: { children: string }) => {
-  return (
-    <Markdown
-      remarkPlugins={[remarkGfm, remarkRehype]}
-      rehypePlugins={[rehypeRaw, rehypeSanitize]}
-    >
-      {children}
-    </Markdown>
-  );
+  return <div>{children}</div>;
 };
