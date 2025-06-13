6d45118d92a44b779c52e66b57630750087e68ff
Ihor Panasiuk <igorskynet13@gmail.com>
2025-06-13 11:20:40 +0200

    refactor: make all fields in WorkflowExecutionDalModel optional

--- keep/workflowmanager/dal/models/workflowexecutiondalmodel.py
+++ keep/workflowmanager/dal/models/workflowexecutiondalmodel.py
@@ -6,18 +6,18 @@ from pydantic import BaseModel
 class WorkflowExecutionDalModel(BaseModel):
     id: str
     workflow_id: str
-    workflow_revision: int
     tenant_id: str
-    started: datetime
-    triggered_by: str
-    status: str
-    is_running: int
-    timeslot: int
-    execution_number: int
-    error: Optional[str]
-    execution_time: Optional[int]
-    results: dict
-    is_test_run: bool
+    workflow_revision: Optional[int] = None
+    started: Optional[datetime] = None
+    triggered_by: Optional[str] = None
+    status: Optional[str] = None
+    is_running: Optional[int] = None
+    timeslot: Optional[int] = None
+    execution_number: Optional[int] = None
+    error: Optional[str] = None
+    execution_time: Optional[int] = None
+    results: Optional[dict] = None
+    is_test_run: Optional[bool] = None
 
     class Config:
         use_enum_values = True
