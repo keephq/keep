9624c87e6c08d185b6f32ddcf0a62be57dd6b626
Ihor Panasiuk <igorskynet13@gmail.com>
2025-06-20 13:35:42 +0200

    feat: implement Elasticsearch workflow repository creation based on environment configuration

---
+++ keep/workflowmanager/dal/elasticsearch/create.py
@@ -0,0 +1,14 @@
+import os
+from keep.api.core.elastic import create_elastic_client
+from keep.workflowmanager.dal.elasticsearch.elasticsearchworkflowrepository import (
+    ElasticSearchWorkflowRepository,
+)
+
+elastic_index_suffix = os.environ.get("ELASTIC_INDEX_SUFFIX")
+
+
+def create_elasticsearch_workflow_repository():
+    elastic_search_client = create_elastic_client()
+    return ElasticSearchWorkflowRepository(
+        elastic_search_client=elastic_search_client, index_suffix=elastic_index_suffix
+    )
--- keep/workflowmanager/dal/factories.py
+++ keep/workflowmanager/dal/factories.py
@@ -1,15 +1,31 @@
+import os
 from keep.workflowmanager.dal.abstractworkflowrepository import WorkflowRepository
+from keep.workflowmanager.dal.elasticsearch.create import (
+    create_elasticsearch_workflow_repository,
+)
 from keep.workflowmanager.dal.sql.sqlworkflowrepository import (
     SqlWorkflowRepository,
 )
 
-db_or_elastic = "db"
+
+WORKFLOW_ENGINE_USE_ELASTICSEARCH = (
+    os.environ.get("WORKFLOW_ENGINE_USE_ELASTICSEARCH", "false").lower() == "true"
+)
+
+if WORKFLOW_ENGINE_USE_ELASTICSEARCH:
+    db_or_elastic = "elastic"
+else:
+    db_or_elastic = "db"
 
 
 def create_workflow_repository() -> WorkflowRepository:
     if db_or_elastic == "db":
         return SqlWorkflowRepository()
 
+    if db_or_elastic == "elastic":
+        return create_elasticsearch_workflow_repository()
+
     raise NotImplementedError(
-        "Only db workflow repository is implemented, but db_or_elastic is set to something else."
+        f"Unsupported workflow repository type: '{db_or_elastic}'. "
+        "Only 'db' and 'elastic' are supported."
     )
