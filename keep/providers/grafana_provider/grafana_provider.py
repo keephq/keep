76c396f983198158da30246a7e40e42bceebf46b
Shahar Glazner <shaharglazner@gmail.com>
2025-08-28 12:02:53 +0300

    feat: graf (#5286)

--- keep/providers/grafana_provider/grafana_provider.py
+++ keep/providers/grafana_provider/grafana_provider.py
@@ -241,10 +241,17 @@ class GrafanaProvider(BaseTopologyProvider, ProviderHealthMixin):
         """
         Calculate fingerprint for alert.
         """
+
+        # First, try to get fingerprint from alert
+        fingerprint = alert.get("fingerprint", "")
+        if fingerprint:
+            logger.debug("Fingerprint provided in alert")
+            return fingerprint
+        
         labels = alert.get("labels", {})
         fingerprint = labels.get("fingerprint", "")
         if fingerprint:
-            logger.debug("Fingerprint provided in alert")
+            logger.debug("Fingerprint provided in alert labels")
             return fingerprint
 
         fingerprint_string = None
