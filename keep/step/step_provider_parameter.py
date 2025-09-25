e72a38805ad6b78aed27d60cdac30c5855361a42
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2025-09-25 16:55:39 +0200

    Use extra config for Pydantic v1.

--- keep/step/step_provider_parameter.py
+++ keep/step/step_provider_parameter.py
@@ -1,8 +1,9 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, Extra
 
 
 class StepProviderParameter(BaseModel):
-    model_config = ConfigDict(extra="forbid")
+    class Config:
+        extra = Extra.forbid
     key: str  # the key to render
     safe: bool = False  # whether to validate this key or fail silently ("safe")
     default: str | int | bool = None  # default value if this key doesn't exist
