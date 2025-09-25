d417950fdbfc0998c67c47502ffef0b198859ec6
Manuel Luis Sanmartín Rozada <manuel.luis@gmail.com>
2025-09-25 16:17:33 +0200

    Forbid extra parameters in StepProviderParameter class.
    Allow the use of the key "key" in parameters in step workflows,
    for example a POST body in WebHook provider with the key: "key".

--- keep/step/step_provider_parameter.py
+++ keep/step/step_provider_parameter.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class StepProviderParameter(BaseModel):
+    model_config = ConfigDict(extra="forbid")
     key: str  # the key to render
     safe: bool = False  # whether to validate this key or fail silently ("safe")
     default: str | int | bool = None  # default value if this key doesn't exist
