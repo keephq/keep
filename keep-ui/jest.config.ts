4b8f6d2aadf85800dbc44ff7bedacf326aca89bd
DolevNe <dolev.nezer@gmail.com>
2025-11-04 09:38:07 +0000

    chore: trigger workflow run

--- keep-ui/jest.config.ts
+++ keep-ui/jest.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "jest";
 import nextJest from "next/jest.js";
+// Trigger workflow
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
