0579ba89f7f01a918a6a7174e4c85ff1c8761e97
Zeke <zekebawt@gmail.com>
2026-02-09 15:20:38 -0500

    fix: lint - explicit re-export and remove unused requests import

--- keep/providers/nagios_provider/nagios_provider.py
+++ keep/providers/nagios_provider/nagios_provider.py
@@ -3,7 +3,6 @@ Nagios Provider is a class that provides a way to receive alerts from Nagios usi
 """
 import dataclasses
 import pydantic
-import requests
 from keep.api.models.alert import AlertDto, AlertSeverity, AlertStatus
 from keep.contextmanager.contextmanager import ContextManager
 from keep.providers.base.base_provider import BaseProvider
